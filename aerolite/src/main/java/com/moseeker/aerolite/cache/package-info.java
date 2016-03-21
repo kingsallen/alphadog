7ac1f3a8422286f76dd278b4cb5dcaa634bae9ca
wjf <123456>
2016-03-21 19:44:00 +0800

    init package

---
+++ aerolite/src/main/java/com/moseeker/aerolite/cache/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * package-info.java
+ *
+ * 1.0
+ *
+ * Mar 21, 2016
+ *
+ * Copyright notice
+ */
+/**
+ * 
+ * cache包提供对外提供缓存相关功能。目前主要提供redis集群缓存客户端调用方法和二级缓存功能。
+ * 
+ * @date Mar 21, 2016
+ * @company 大岂仟寻
+ * @author wjf
+ * @email wjf2255@gmail.com
+ */
+package com.moseeker.aerolite.cache;
---
+++ aerolite/src/main/java/com/moseeker/aerolite/session/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * package-info.java
+ *
+ * Version information
+ *
+ * Mar 21, 2016
+ *
+ * Copyright notice
+ */
+/**
+ * TODO 
+ * @date Mar 21, 2016
+ * @company 大岂仟寻
+ * @author wjf
+ * @email wjf2255@gmail.com
+ */
+package com.moseeker.aerolite.session;
