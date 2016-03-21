2157af4ccf40201257ed234a71d13c5542893811
wangyaofeng <wangyaofeng@moseeker.com>
2016-03-21 19:10:35 +0800

    restructure service-manager

--- service-manager/src/main/java/com/moseeker/thrift/position/service/PositionService.java
+++
@@ -1,7 +0,0 @@
-package com.moseeker.thrift.position.service;
-
-/**
- * Created by zzh on 16/3/11.
- */
-public class PositionService {
-}
--- service-manager/src/main/java/com/moseeker/thrift/position/struct/Position.java
+++
@@ -1,7 +0,0 @@
-package com.moseeker.thrift.position.struct;
-
-/**
- * Created by zzh on 16/3/11.
- */
-public class Position {
-}
