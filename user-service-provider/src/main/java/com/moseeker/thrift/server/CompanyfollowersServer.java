40a513026f6fa35f9895fad717a06c06f1b00c69
wangyaofeng <wangyaofeng@moseeker.com>
2016-03-21 17:54:14 +0800

    user-service-provider thrift 服务端

---
+++ user-service-provider/src/main/java/com/moseeker/thrift/server/CompanyfollowersServer.java
@@ -0,0 +1,71 @@
+package com.moseeker.thrift.server;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TServer.Args;
+import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TServerTransport;
+
+import com.moseeker.thrift.gen.companyfollowers.CompanyfollowerServices;
+import com.moseeker.thrift.gen.companyfollowers.CompanyfollowerServices.Processor;
+import com.moseeker.thrift.service.impl.CompanyfollowerServicesImpl;
+
+public class CompanyfollowersServer {
+
+	public static CompanyfollowerServicesImpl handler;
+
+	public static CompanyfollowerServices.Processor<CompanyfollowerServicesImpl> processor;
+
+	public static void main(String[] args) {
+		try {
+			handler = new CompanyfollowerServicesImpl();
+			processor = new Processor<CompanyfollowerServicesImpl>(handler);
+
+			new Thread(new Runnable() {
+				public void run() {
+					startSimpleServer(processor, 9090);
+				}
+			}).start();
+
+		} catch (Exception x) {
+			x.printStackTrace();
+		}
+	}
+
+	public static void startSimpleServer(Processor processor,
+			int port) {
+		try {
+			TServerTransport serverTransport = new TServerSocket(port);
+			TServer server = new TSimpleServer(
+					new Args(serverTransport).processor(processor));
+
+			// Use this for a multithreaded server
+			// TServer server = new TThreadPoolServer(new
+			// TThreadPoolServer.Args(serverTransport).processor(processor));
+
+			// System.out.println("Starting the simple server...");
+			server.serve();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
