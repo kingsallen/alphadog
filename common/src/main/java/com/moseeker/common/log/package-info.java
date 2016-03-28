3dcec48be97d6b161bf8555167cae9fbea802bb9
chendi <joker_chendi@126.com>
2016-03-28 14:19:40 +0800

    refactor project structure

---
+++ common/src/main/java/com/moseeker/common/log/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * package-info.java
+ *
+ * Version information
+ *
+ * Mar 21, 2016
+ *
+ * Copyright notice
+ */
+/**
+ * 
+ * 提供日记服务的客户端。
+ * 
+ * @date Mar 21, 2016
+ * @company 大岂仟寻
+ * @author wjf
+ * @email wjf2255@gmail.com
+ */
+package com.moseeker.aerolite.log;
