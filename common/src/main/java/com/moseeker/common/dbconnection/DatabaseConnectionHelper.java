bdbcf12cf79c651e3d548adeeae7bdb429c9db56
chendi <joker_chendi@126.com>
2016-03-30 14:28:14 +0800

    read .properties file when connecting mycat

--- common/src/main/java/com/moseeker/common/dbconnection/DatabaseConnectionHelper.java
+++ common/src/main/java/com/moseeker/common/dbconnection/DatabaseConnectionHelper.java
@@ -1,15 +1,18 @@
 package com.moseeker.common.dbconnection;
+
 import java.util.Properties;
+
 import com.moseeker.common.util.ConfigPropertiesUtil;
 import org.jooq.*;
 import org.jooq.impl.*;
+
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Connection;
 
 import com.jolbox.bonecp.BoneCP;
 import com.jolbox.bonecp.BoneCPConfig;
-    
+
 public class DatabaseConnectionHelper {
 
     private static ConfigPropertiesUtil dbPropertiesReader = null;
@@ -23,22 +26,18 @@ public class DatabaseConnectionHelper {
 
     static {
         try {
-            // dbPropertiesReader = DBPropertiesReader.getDBPropertiesReader();
-            // url = dbPropertiesReader.get("mycat.url", String.class); // read from .properties file
-            // userName = dbPropertiesReader.get("mycat.userName", String.class);
-            // password = dbPropertiesReader.get("mycat.password", String.class);
-            url = "jdbc:mysql://192.168.4.64:8066/userDB";
-            userName = "userdev";
-            password = "userdev";
-            minConnections = 5;
-            maxConnections = 20;
-
+            dbPropertiesReader = DBPropertiesReader.getDBPropertiesReader();
+            url = dbPropertiesReader.get("mycat.url", String.class); // read from .properties file
+            userName = dbPropertiesReader.get("mycat.userName", String.class);
+            password = dbPropertiesReader.get("mycat.password", String.class);
+            minConnections = dbPropertiesReader.get("mycat.minConnections", Integer.class);
+            maxConnections = dbPropertiesReader.get("mycat.maxConnections", Integer.class);
             System.out.println(url);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
+
     static {
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
@@ -56,15 +55,15 @@ public class DatabaseConnectionHelper {
             config.setMinConnectionsPerPartition(minConnections);
             config.setMaxConnectionsPerPartition(maxConnections);
             long currentTime = System.currentTimeMillis();
-            connectionPool = new BoneCP(config); 
+            connectionPool = new BoneCP(config);
             System.out.println("first connection: " + (System.currentTimeMillis() - currentTime));
-        }  catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     private static DSLContext getCreate(Connection conn) throws SQLException {
-        return DSL.using(conn, SQLDialect.MYSQL); 
+        return DSL.using(conn, SQLDialect.MYSQL);
     }
 
     public static DSLContext getCreate() throws SQLException {
@@ -80,15 +79,15 @@ public class DatabaseConnectionHelper {
         // System.out.println(password);
         return connectionPool.getConnection();
         // return DriverManager.getConnection(url, userName, password);
-    } 
+    }
 
 }
 
 class DBPropertiesReader {
 
-    public static ConfigPropertiesUtil getDBPropertiesReader () throws Exception {
+    public static ConfigPropertiesUtil getDBPropertiesReader() throws Exception {
         ConfigPropertiesUtil cpu = ConfigPropertiesUtil.getInstance();
-        ConfigPropertiesUtil.loadResource("mycatConfig.properties");
+        cpu.loadResource("mycatConfig.properties");
         return cpu;
     }
 
--- common/src/main/java/com/moseeker/common/dbconnection/Test.java
+++ common/src/main/java/com/moseeker/common/dbconnection/Test.java
@@ -9,7 +9,7 @@ public class Test {
 
     public static void main(String[] args) {
 
-        for(int i = 0; i < 2000; i++) {
+        for(int i = 0; i < 5; i++) {
             
             new Thread(() -> {
 
