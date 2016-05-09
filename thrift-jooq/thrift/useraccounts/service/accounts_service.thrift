857ec1f0d2933de7b464db2b1e58ed37296a364c
wangyaofeng <wangyaofeng@moseeker.com>
2016-05-09 17:26:36 +0800

    common struct

--- thrift-jooq/thrift/useraccounts/service/accounts_service.thrift
+++ thrift-jooq/thrift/useraccounts/service/accounts_service.thrift
@@ -1,19 +1,20 @@
 # file: useraccounts.thrift
 
 include "../struct/useraccounts_struct.thrift"
+include "../../common/struct/common_struct.thrift"
 
 namespace java com.moseeker.thrift.gen.useraccounts.service
 
 service UseraccountsServices {
-    useraccounts_struct.userloginresp postuserlogin(1: useraccounts_struct.userloginreq userloginreq);
-    i32 postuserlogout(1: i32 userid);
-    i32 postsendsignupcode(1: string mobile);
-    i32 postusermobilesignup(1: string mobile, 2: string code, 3: string password);
-    i32 postuserwxsignup(1: string unionid);
-    i32 postuserwxbindmobile(1: string unionid, 2: string code,3: string mobile);
-    i32 postusermobilebindwx(1: string mobile, 2: string code,3: string unionid);
-    i32 postuserchangepassword(1: i32 user_id, 2: string old_password,  3: string password);
-    i32 postusersendpasswordforgotcode(1: string mobile);
-    i32 postuserresetpassword(1: string mobile, 2: string code, 3: string password);
-    i32 postusermergebymobile(1: string mobile);
+    common_struct.Response postuserlogin(1: useraccounts_struct.userloginreq userloginreq);
+    common_struct.Response postuserlogout(1: i32 userid);
+    common_struct.Response postsendsignupcode(1: string mobile);
+    common_struct.Response postusermobilesignup(1: string mobile, 2: string code, 3: string password);
+    common_struct.Response postuserwxsignup(1: string unionid);
+    common_struct.Response postuserwxbindmobile(1: string unionid, 2: string code,3: string mobile);
+    common_struct.Response postusermobilebindwx(1: string mobile, 2: string code,3: string unionid);
+    common_struct.Response postuserchangepassword(1: i32 user_id, 2: string old_password,  3: string password);
+    common_struct.Response postusersendpasswordforgotcode(1: string mobile);
+    common_struct.Response postuserresetpassword(1: string mobile, 2: string code, 3: string password);
+    common_struct.Response postusermergebymobile(1: string mobile);
 }
