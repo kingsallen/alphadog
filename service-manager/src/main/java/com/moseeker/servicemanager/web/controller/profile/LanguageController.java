5f9163a2171eb317ce8528e9d2439070814afe60
wangyaofeng <wangyaofeng@moseeker.com>
2016-05-13 12:04:52 +0800

    revert deleted  profile controllers.

---
+++ service-manager/src/main/java/com/moseeker/servicemanager/web/controller/profile/LanguageController.java
@@ -0,0 +1,84 @@
+package com.moseeker.servicemanager.web.controller.profile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.moseeker.rpccenter.common.ServiceUtil;
+import com.moseeker.servicemanager.common.ParamUtils;
+import com.moseeker.servicemanager.common.ResponseLogNotification;
+import com.moseeker.thrift.gen.common.struct.CommonQuery;
+import com.moseeker.thrift.gen.common.struct.Response;
+import com.moseeker.thrift.gen.profile.service.LanguageServices;
+import com.moseeker.thrift.gen.profile.struct.Language;
+
+@Controller
+public class LanguageController {
+
+	Logger logger = LoggerFactory.getLogger(LanguageController.class);
+
+	LanguageServices.Iface languageService = ServiceUtil.getService(LanguageServices.Iface.class);
+	
+	@RequestMapping(value = "/profile/language", method = RequestMethod.GET)
+	@ResponseBody
+	public String get(HttpServletRequest request, HttpServletResponse response) {
+		//PrintWriter writer = null;
+		try {
+			// GET方法 通用参数解析并赋值
+			CommonQuery query = ParamUtils.initCommonQuery(request, CommonQuery.class);
+
+			Response result = languageService.getResources(query);
+			
+			return ResponseLogNotification.success(request, result);
+		} catch (Exception e) {	
+			return ResponseLogNotification.fail(request, e.getMessage());
+		}
+	}
+
+	@RequestMapping(value = "/profile/language", method = RequestMethod.POST)
+	@ResponseBody
+	public String post(HttpServletRequest request, HttpServletResponse response) {
+		//PrintWriter writer = null;
+		try {
+			Language language = ParamUtils.initModelForm(request, Language.class);
+			Response result = languageService.postResource(language);
+			
+			return ResponseLogNotification.success(request, result);
+		} catch (Exception e) {	
+			e.printStackTrace();
+			return ResponseLogNotification.fail(request, e.getMessage());
+		}
+	}
+
+	@RequestMapping(value = "/profile/language", method = RequestMethod.PUT)
+	@ResponseBody
+	public String put(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			Language language = ParamUtils.initModelForm(request, Language.class);
+			Response result = languageService.putResource(language);
+			
+			return ResponseLogNotification.success(request, result);
+		} catch (Exception e) {	
+			return ResponseLogNotification.fail(request, e.getMessage());
+		}
+	}
+
+	@RequestMapping(value = "/profile/language", method = RequestMethod.DELETE)
+	@ResponseBody
+	public String delete(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			Language language = ParamUtils.initModelForm(request, Language.class);
+			Response result = languageService.delResource(language);
+			
+			return ResponseLogNotification.success(request, result);
+		} catch (Exception e) {	
+			return ResponseLogNotification.fail(request, e.getMessage());
+		}
+	}
+}
---
+++ service-manager/src/main/java/com/moseeker/servicemanager/web/controller/profile/RewardController.java
@@ -0,0 +1,84 @@
+package com.moseeker.servicemanager.web.controller.profile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.moseeker.rpccenter.common.ServiceUtil;
+import com.moseeker.servicemanager.common.ParamUtils;
+import com.moseeker.servicemanager.common.ResponseLogNotification;
+import com.moseeker.thrift.gen.common.struct.CommonQuery;
+import com.moseeker.thrift.gen.common.struct.Response;
+import com.moseeker.thrift.gen.profile.service.RewardServices;
+import com.moseeker.thrift.gen.profile.struct.Reward;
+
+@Controller
+public class RewardController {
+
+	Logger logger = LoggerFactory.getLogger(RewardController.class);
+
+	RewardServices.Iface rewardService = ServiceUtil.getService(RewardServices.Iface.class);
+	
+	@RequestMapping(value = "/profile/reward", method = RequestMethod.GET)
+	@ResponseBody
+	public String get(HttpServletRequest request, HttpServletResponse response) {
+		//PrintWriter writer = null;
+		try {
+			// GET方法 通用参数解析并赋值
+			CommonQuery query = ParamUtils.initCommonQuery(request, CommonQuery.class);
+
+			Response result = rewardService.getResources(query);
+			
+			return ResponseLogNotification.success(request, result);
+		} catch (Exception e) {	
+			return ResponseLogNotification.fail(request, e.getMessage());
+		}
+	}
+
+	@RequestMapping(value = "/profile/reward", method = RequestMethod.POST)
+	@ResponseBody
+	public String post(HttpServletRequest request, HttpServletResponse response) {
+		//PrintWriter writer = null;
+		try {
+			Reward reward = ParamUtils.initModelForm(request, Reward.class);
+			Response result = rewardService.postResource(reward);
+			
+			return ResponseLogNotification.success(request, result);
+		} catch (Exception e) {	
+			e.printStackTrace();
+			return ResponseLogNotification.fail(request, e.getMessage());
+		}
+	}
+
+	@RequestMapping(value = "/profile/reward", method = RequestMethod.PUT)
+	@ResponseBody
+	public String put(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			Reward reward = ParamUtils.initModelForm(request, Reward.class);
+			Response result = rewardService.putResource(reward);
+			
+			return ResponseLogNotification.success(request, result);
+		} catch (Exception e) {	
+			return ResponseLogNotification.fail(request, e.getMessage());
+		}
+	}
+
+	@RequestMapping(value = "/profile/reward", method = RequestMethod.DELETE)
+	@ResponseBody
+	public String delete(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			Reward reward = ParamUtils.initModelForm(request, Reward.class);
+			Response result = rewardService.delResource(reward);
+			
+			return ResponseLogNotification.success(request, result);
+		} catch (Exception e) {	
+			return ResponseLogNotification.fail(request, e.getMessage());
+		}
+	}
+}
