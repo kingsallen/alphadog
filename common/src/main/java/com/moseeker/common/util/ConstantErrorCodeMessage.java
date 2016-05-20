9cd58b4a3d028e6cea41502f75db965d9343fe86
wangyaofeng <wangyaofeng@moseeker.com>
2016-05-20 15:36:25 +0800

    修改手机号

--- common/src/main/java/com/moseeker/common/util/ConstantErrorCodeMessage.java
+++ common/src/main/java/com/moseeker/common/util/ConstantErrorCodeMessage.java
@@ -11,13 +11,14 @@ public final class ConstantErrorCodeMessage {
 	public static final String PROGRAM_DEL_FAILED = "{'status':20013,'message':'删除失败!'}";
 
 	
-	public static final String LOGIN_ACCOUNT_UNLEGAL = "{'status':10010,'message':'用户名密码不匹配!'}";
-	public static final String LOGIN_VALIDATION_CODE_UNLEGAL = "{'status':10011,'message':'无效验证码！'}";
+	public static final String LOGIN_ACCOUNT_ILLEAGUE = "{'status':10010,'message':'用户名密码不匹配!'}";
+	public static final String INVALID_SMS_CODE = "{'status':10011,'message':'无效验证码！'}";
 	public static final String LOGIN_PASSWORD_UNLEGAL = "{'status':10012,'message':'修改密码失败：原密码错误!'}";
 	public static final String LOGIN_UPDATE_PASSWORD_FAILED = "{'status':10013,'message':'修改密码失败！'}";
 	public static final String LOGIN_MOBILE_NOTEXIST = "{'status':10014,'message':'手机号不存在！'}";
 	public static final String USERACCOUNT_BIND_NONEED = "{'status':10015,'message':'手机号已经注册'}";;
 	public static final String USERACCOUNT_EXIST = "{'status':10016,'message':'帐号已存在!'}";;
+	public static final String USERACCOUNT_NOTEXIST = "{'status':10017,'message':'帐号不存在!'}";;
 	
 	
 
