86a93182b9aa735180a7702f13b9ab4afe4bdcfd
wangyaofeng <wangyaofeng@moseeker.com>
2016-05-21 14:30:30 +0800

    remove ;;

--- common/src/main/java/com/moseeker/common/util/ConstantErrorCodeMessage.java
+++ common/src/main/java/com/moseeker/common/util/ConstantErrorCodeMessage.java
@@ -16,9 +16,9 @@ public final class ConstantErrorCodeMessage {
 	public static final String LOGIN_PASSWORD_UNLEGAL = "{'status':10012,'message':'修改密码失败：原密码错误!'}";
 	public static final String LOGIN_UPDATE_PASSWORD_FAILED = "{'status':10013,'message':'修改密码失败！'}";
 	public static final String LOGIN_MOBILE_NOTEXIST = "{'status':10014,'message':'手机号不存在！'}";
-	public static final String USERACCOUNT_BIND_NONEED = "{'status':10015,'message':'手机号已经注册'}";;
-	public static final String USERACCOUNT_EXIST = "{'status':10016,'message':'帐号已存在!'}";;
-	public static final String USERACCOUNT_NOTEXIST = "{'status':10017,'message':'帐号不存在!'}";;
+	public static final String USERACCOUNT_BIND_NONEED = "{'status':10015,'message':'手机号已经注册'}";
+	public static final String USERACCOUNT_EXIST = "{'status':10016,'message':'帐号已存在!'}";
+	public static final String USERACCOUNT_NOTEXIST = "{'status':10017,'message':'帐号不存在!'}";
 	
 	
 
