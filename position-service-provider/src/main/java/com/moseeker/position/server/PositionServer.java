dd2e0d6e6479771008581392765d375e6feb3aaf
chendi <joker_chendi@126.com>
2016-05-25 20:25:51 +0800

    jd基本可用版本

--- position-service-provider/src/main/java/com/moseeker/position/server/PositionServer.java
+++ position-service-provider/src/main/java/com/moseeker/position/server/PositionServer.java
@@ -1,7 +1,48 @@
 package com.moseeker.position.server;
 
-/**
- * Created by chendi on 5/25/16.
- */
+import com.moseeker.position.service.impl.PositionServicesImpl;
+import com.moseeker.rpccenter.common.ServerNodeUtils;
+import com.moseeker.rpccenter.main.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class PositionServer {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(PositionServer.class);
+
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext acac = initSpring();
+        try {
+            Server server = new Server(
+                    PositionServer.class,
+                    ServerNodeUtils.getPort(args),
+                    acac.getBean(PositionServicesImpl.class)
+            );
+            server.start();
+
+            synchronized (PositionServer.class) {
+                while (true) {
+                    try {
+                        PositionServer.class.wait();
+                    } catch (InterruptedException e) {
+                        LOGGER.error("error", e);
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.error("error", e);
+            e.printStackTrace();
+        }
+
+    }
+
+    private static AnnotationConfigApplicationContext initSpring() {
+        AnnotationConfigApplicationContext acac = new AnnotationConfigApplicationContext();
+        acac.scan("com.moseeker.position");
+        acac.refresh();
+        return acac;
+    }
 }
