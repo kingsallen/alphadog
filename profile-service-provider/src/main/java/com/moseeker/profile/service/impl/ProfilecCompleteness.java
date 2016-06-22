7845f98e6b487e9989d1d7ef66ff9c4e6abf0da3
wengjianfei <wengjianfei@moseeker.com>
2016-06-22 11:46:44 +0800

    简历完成度

---
+++ profile-service-provider/src/main/java/com/moseeker/profile/service/impl/ProfilecCompleteness.java
@@ -0,0 +1,18 @@
+package com.moseeker.profile.service.impl;
+
+public class ProfilecCompleteness {
+
+	private int useruser;
+	private int usersettings;
+	private int profilebasic;
+	private int profileworkexp;
+	private int profileeducation;
+	private int profileprojectexp;
+	private int profilelanguage;
+	private int profileskill;
+	private int profilecredentials;
+	private int profileawards;
+	private int profileworks;
+	private int profileintentions;
+	private int profileId;
+}
