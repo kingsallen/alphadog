3edb0e455a82fc477e327f3815619c9bd5577d9a
wengjianfei <wengjianfei@moseeker.com>
2016-07-24 08:46:18 +0800

    继续添加日志

--- profile-service-provider/src/main/java/com/moseeker/profile/service/impl/ProfileIntentionServicesImpl.java
+++ profile-service-provider/src/main/java/com/moseeker/profile/service/impl/ProfileIntentionServicesImpl.java
@@ -361,6 +361,7 @@ public class ProfileIntentionServicesImpl extends JOOQBaseServiceImpl<Intention,
 					while(it.hasNext()) {
 						ProfileIntentionPositionRecord selectedPositionRecord = it.next();
 						System.out.println("position_code:"+selectedPositionRecord.getPositionCode().intValue()+" position_name:"+selectedPositionRecord.getPositionName());
+						logger.debug("position_code:"+selectedPositionRecord.getPositionCode().intValue()+" position_name:"+selectedPositionRecord.getPositionName());
 						if (isSameOfPositionRecord(entry, selectedPositionRecord)) {
 							exist = true;
 							it.remove();
@@ -437,6 +438,7 @@ public class ProfileIntentionServicesImpl extends JOOQBaseServiceImpl<Intention,
 					while(it.hasNext()) {
 						ProfileIntentionIndustryRecord selectedIndustryRecord = it.next();
 						System.out.println("industry_code:"+selectedIndustryRecord.getIndustryCode().intValue()+" industry_name:"+selectedIndustryRecord.getIndustryName());
+						logger.debug("industry_code:"+selectedIndustryRecord.getIndustryCode().intValue()+" industry_name:"+selectedIndustryRecord.getIndustryName());
 						if (isSameOfIndustryRecord(entry, selectedIndustryRecord)) {
 							exist = true;
 							it.remove();
@@ -519,6 +521,7 @@ public class ProfileIntentionServicesImpl extends JOOQBaseServiceImpl<Intention,
 					while(it.hasNext()) {
 						ProfileIntentionCityRecord selectedCityRecord = it.next();
 						System.out.println("city_code:"+selectedCityRecord.getCityCode().intValue()+" city_name:"+selectedCityRecord.getCityName());
+						logger.debug("city_code:"+selectedCityRecord.getCityCode().intValue()+" city_name:"+selectedCityRecord.getCityName());
 						if (isSameOfCityRecord(entry, selectedCityRecord)) {
 							exist = true;
 							it.remove();
