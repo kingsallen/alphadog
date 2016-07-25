139ead8855983c0c2f845137c283637cf959459d
wangyaofeng <wangyaofeng@moseeker.com>
2016-07-25 19:49:32 +0800

    user_id remove

--- thrift-jooq/thrift/position/service/position_service.thrift
+++ thrift-jooq/thrift/position/service/position_service.thrift
@@ -6,7 +6,7 @@ namespace java com.moseeker.thrift.gen.position.service
 service PositionServices {
     common_struct.Response getResources(1:common_struct.CommonQuery query);
     common_struct.Response getRecommendedPositions(1:i32 pid);
-    common_struct.Response verifyCustomize(1:i32 userId, 2:i32 positionId);
+    common_struct.Response verifyCustomize(1:i32 positionId);
     // 根据职位Id获取当前职位
     common_struct.Response getPositionById(1:i32 positionId);
 }
