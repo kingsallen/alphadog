3802f6510a42be071b1ad9d42ce0066dfd24b13d
qianqian <qianqian@moseeker.com>
2016-08-19 11:55:58 +0800

    change query boosting score

--- searchengine-service-provider/src/main/java/com/moseeker/searchengine/service/impl/SearchengineServiceImpl.java
+++ searchengine-service-provider/src/main/java/com/moseeker/searchengine/service/impl/SearchengineServiceImpl.java
@@ -84,9 +84,9 @@ public class SearchengineServiceImpl implements Iface {
                 for (int i = 0; i < keyword_list.length; i++) {
                     String keyword = keyword_list[i];
                     QueryBuilder keyfilter = QueryBuilders.simpleQueryStringQuery(keyword)
-                            .field("title",0.5f)
-                            .field("city",0.2f)
-                            .field("company_name",0.19f);
+                            .field("title",20.5f)
+                            .field("city",10.2f)
+                            .field("company_name",5.19f);
                     ((BoolQueryBuilder) keyand).should(keyfilter);
                 }
                 ((BoolQueryBuilder) query).must(keyand);
