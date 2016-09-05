72d04a579ebff96271d5aa7d24670bed48e38ae8
翁剑飞 <wengjianfei@moseeker.com>
2016-09-05 18:17:51 +0800

    将download服务器地址移到配置文件中

--- service-manager/src/main/java/com/moseeker/servicemanager/web/controller/profile/form/OutPutResumeUtil.java
+++ service-manager/src/main/java/com/moseeker/servicemanager/web/controller/profile/form/OutPutResumeUtil.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.moseeker.common.providerutils.ResponseUtils;
+import com.moseeker.common.util.ConfigPropertiesUtil;
 import com.moseeker.common.util.ConstantErrorCodeMessage;
 import com.moseeker.servicemanager.common.UrlUtil;
 import com.moseeker.thrift.gen.common.struct.Response;
@@ -16,19 +17,22 @@ public class OutPutResumeUtil {
 	
 	Logger logger = LoggerFactory.getLogger(OutPutResumeUtil.class);
 	
-	private static String GENERATE_USER_ID = "http://download1.dqprism.com/generatebyuserid";
-	private static String GENERATE_APP_ID = "http://download1.dqprism.com/generatebyappid";
-
 	public Response outPutResume(OutPutResumeForm form) {
 		String url = null;
 		String result = null;
 		JSONObject params = new JSONObject();
+		ConfigPropertiesUtil propertiesUtils = ConfigPropertiesUtil.getInstance();
+		try {
+			propertiesUtils.loadResource("setting.properties");
+		} catch (Exception e1) {
+			logger.error(e1.getMessage(), e1);
+		}
 		if(form.getUser_id() > 0) {
-			url = GENERATE_USER_ID;
+			url = propertiesUtils.get("GENERATE_USER_ID", String.class);
 			params.put("user_id", form.getUser_id());
 			params.put("password", form.getPassword());
 		} else if(form.getApp_id() > 0) {
-			url = GENERATE_APP_ID;
+			url = propertiesUtils.get("GENERATE_APP_ID", String.class);;
 			params.put("app_id", form.getApp_id());
 			params.put("password", form.getPassword());
 		}
