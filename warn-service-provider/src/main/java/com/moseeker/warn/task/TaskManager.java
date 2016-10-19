80086cc83588809c76df9cabf59fb9d5ba692841
lucky8987 <lucky8987@lucky8987deMac-mini.local>
2016-10-19 16:41:42 +0800

    mkdir task

---
+++ warn-service-provider/src/main/java/com/moseeker/warn/task/TaskManager.java
@@ -0,0 +1,33 @@
+package com.moseeker.warn.task;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import com.moseeker.warn.service.EventConfigService;
+
+/**
+ * @author lucky8987
+ * spring 任务调度管理 <br/>
+ * EnableScheduling: 启用task任务调度
+ */
+@Component
+@EnableScheduling
+public class TaskManager {
+	
+	Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private EventConfigService eventConfigService;
+	
+	/**
+	 * 加载event配置信息（每天23:00执行）
+	 */
+	@Scheduled(cron="0 0 23 * * ?")
+	public void loadEventConfig() {
+		eventConfigService.init();
+	}
+
+}
