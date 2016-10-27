e6fd739554eb6a33dfafeb9161a05abd1d73b17e
翁剑飞 <wengjianfei@moseeker.com>
2016-10-27 19:12:14 +0800

    删除test

--- service-manager/src/main/java/com/moseeker/servicemanager/web/controller/mq/MqController.java
+++ service-manager/src/main/java/com/moseeker/servicemanager/web/controller/mq/MqController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
