e84419d463d05c93dd82cf6b1ba08986d50a80d6
翁剑飞 <wengjianfei@moseeker.com>
2016-11-02 10:37:29 +0800

    添加获取公众号信息接口

---
+++ company-service-provider/src/main/java/com/moseeker/company/dao/WechatDao.java
@@ -0,0 +1,8 @@
+package com.moseeker.company.dao;
+
+import com.moseeker.common.providerutils.daoutils.BaseDao;
+import com.moseeker.db.hrdb.tables.records.HrWxWechatRecord;
+
+public interface WechatDao extends BaseDao<HrWxWechatRecord> {
+
+}
---
+++ company-service-provider/src/main/java/com/moseeker/company/thrift/ThriftCompanyService.java
@@ -0,0 +1,45 @@
+package com.moseeker.company.thrift;
+
+import org.apache.thrift.TException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.moseeker.company.service.impl.CompanyServicesImpl;
+import com.moseeker.thrift.gen.common.struct.CommonQuery;
+import com.moseeker.thrift.gen.common.struct.Response;
+import com.moseeker.thrift.gen.company.service.CompanyServices.Iface;
+import com.moseeker.thrift.gen.company.struct.Hrcompany;
+
+@Service
+public class ThriftCompanyService implements Iface {
+	
+	@Autowired
+    protected CompanyServicesImpl companyService;
+
+	@Override
+	public Response getResource(CommonQuery query) throws TException {
+		return companyService.getResource(query);
+	}
+
+	@Override
+	public Response getResources(CommonQuery query) throws TException {
+		return companyService.getResources(query);
+	}
+
+	@Override
+	public Response getAllCompanies(CommonQuery query) throws TException {
+		return companyService.getAllCompanies(query);
+	}
+
+	@Override
+	public Response add(Hrcompany company) throws TException {
+		return companyService.add(company);
+	}
+
+	@Override
+	public Response getWechat(long companyId, long wechatId) throws TException {
+		// TODO Auto-generated method stub
+		return companyService.getWechat(companyId, wechatId);
+	}
+
+}
