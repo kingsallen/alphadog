61b6e1865569bdbeca18995728a241cec9fff354
翁剑飞 <wengjianfei@moseeker.com>
2016-11-07 10:08:26 +0800

    hr3.2

---
+++ function-service-provider/src/main/java/com/moseeker/function/service/ChaosService.java
@@ -0,0 +1,57 @@
+package com.moseeker.function.service;
+
+import com.moseeker.function.service.choas.ChannelType;
+import com.moseeker.function.service.choas.ThirdPartyAccountPojo;
+import com.moseeker.thrift.gen.common.struct.Response;
+
+/**
+ * 
+ * 封装数据的chaos系统。
+ * chaos系统主要提供将符合第三方渠道的职位数据发布到第三方平台，模拟登录第三方平台，获取发布职位数等功能。
+ * <p>Company: MoSeeker</P>  
+ * <p>date: Nov 7, 2016</p>  
+ * <p>Email: wjf2255@gmail.com</p>
+ * @author wjf
+ * @version
+ */
+public interface ChaosService {
+
+	/**
+	 * 绑定第三方帐号
+	 * @param account 第三方渠道帐号
+	 * @return
+	 */
+	public Response bind(ThirdPartyAccountPojo account);
+	
+	/**
+	 * 查询最新职位数
+	 * @param account 第三方渠道帐号
+	 * @return
+	 */
+	public Response fetchRemainNum(ThirdPartyAccountPojo account);
+	
+	/**
+	 * 同步职位数据
+	 * @param account 第三方渠道帐号
+	 * @param positionJson 
+	 * @return
+	 */
+	public Response synchronizePosition(ThirdPartyAccountPojo account, String positionJson);
+	
+	/**
+	 * 刷新职位数据
+	 * @param account 第三方渠道帐号
+	 * @param positionId 职位编号
+	 * @param jobNumber 第三方渠道职位编号
+	 * @return
+	 */
+	public Response refreshPosition(ThirdPartyAccountPojo account, int positionId, String jobNumber);
+	
+	/**
+	 * 仟寻职位转第三方职位
+	 * @param positionId 职位编号
+	 * @param channel 渠道编号
+	 * @return
+	 */
+	public Response distortPosition(int positionId, ChannelType channel);
+}
---
+++ function-service-provider/src/main/java/com/moseeker/function/service/impl/ChaosServiceImpl.java
@@ -0,0 +1,49 @@
+package com.moseeker.function.service.impl;
+
+import com.moseeker.function.service.ChaosService;
+import com.moseeker.function.service.choas.ChannelType;
+import com.moseeker.function.service.choas.ThirdPartyAccountPojo;
+import com.moseeker.thrift.gen.common.struct.Response;
+
+/**
+ * 
+ * 第三方渠道（比如51，智联）服务 
+ * <p>Company: MoSeeker</P>  
+ * <p>date: Nov 6, 2016</p>  
+ * <p>Email: wjf2255@gmail.com</p>
+ * @author wjf
+ * @version
+ */
+public class ChaosServiceImpl implements ChaosService {
+
+	@Override
+	public Response bind(ThirdPartyAccountPojo account) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response fetchRemainNum(ThirdPartyAccountPojo account) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response synchronizePosition(ThirdPartyAccountPojo account, String positionJson) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response refreshPosition(ThirdPartyAccountPojo account, int positionId, String jobNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response distortPosition(int positionId, ChannelType channel) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
