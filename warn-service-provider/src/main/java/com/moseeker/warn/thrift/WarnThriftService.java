15aeec595cade4b9560a1d4bc844353584e9e52f
zztaiwll <zztaiwll@zztaiwlldeMac-mini.local>
2016-11-08 13:59:40 +0800

    张泽腾 预警服务

--- warn-service-provider/src/main/java/com/moseeker/warn/thrift/WarnThriftService.java
+++ warn-service-provider/src/main/java/com/moseeker/warn/thrift/WarnThriftService.java
@@ -1,4 +1,4 @@
-package com.moseeker.warn.service.impl;
+package com.moseeker.warn.thrift;
 
 import org.apache.thrift.TException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +8,14 @@ import com.moseeker.thrift.gen.warn.service.WarnSetService.Iface;
 import com.moseeker.thrift.gen.warn.struct.WarnBean;
 import com.moseeker.warn.service.validate.ValidationService;
 @Service
-public class WarnServiceImpl implements Iface{
-@Autowired
-private ValidationService validService;
+public class WarnThriftService implements Iface{
+	
+	@Autowired
+	private ValidationService validService;
+	
 	//验证预警信息，并且置于redis队列
 	@Override
 	public void sendOperator(WarnBean bean) throws TException {
-		// TODO Auto-generated method stub
 		validService.valid(bean);
 	}
 
