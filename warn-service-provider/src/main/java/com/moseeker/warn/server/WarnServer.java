e372f8977bf79c7d9e0df40be02f17c147ccf640
zztaiwll <zztaiwll@zztaiwlldeMac-mini.local>
2016-11-08 18:10:35 +0800

    张泽腾 预警服务

--- warn-service-provider/src/main/java/com/moseeker/warn/server/WarnServer.java
+++ warn-service-provider/src/main/java/com/moseeker/warn/server/WarnServer.java
@@ -2,7 +2,10 @@ package com.moseeker.warn.server;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.moseeker.rpccenter.common.ServerNodeUtils;
+import com.moseeker.rpccenter.main.Server;
 import com.moseeker.warn.service.ManageService;
+import com.moseeker.warn.thrift.WarnThriftService;
 
 /**
  * @author ltf
@@ -12,6 +15,8 @@ public class WarnServer {
 	
 	public static void main(String[] args) throws Exception {
 		AnnotationConfigApplicationContext context = initSpring();
+		Server server=new Server(WarnServer.class,ServerNodeUtils.getPort(args),context.getBean(WarnThriftService.class));
+		server.start();
 		Thread sendMsgThread = new Thread(() -> {
 			context.getBean(ManageService.class).sendMessage();
 		});
