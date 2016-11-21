a7be44483759d30aec349b395c0dbddbf11b4941
zztaiwll <zztaiwll@zztaiwlldeMac-mini.local>
2016-11-21 17:02:37 +0800

    张泽腾 hr3.2

--- baseorm-service-provider/src/test/java/moseeker/baseorm/dao/dictoccupation/JobCustomTest.java
+++ baseorm-service-provider/src/test/java/moseeker/baseorm/dao/dictoccupation/JobCustomTest.java
@@ -12,22 +12,22 @@ import com.moseeker.thrift.gen.common.struct.Response;
 import com.moseeker.thrift.gen.position.service.PositionDao;
 
 public class JobCustomTest {
-	private PositionServiceImpl position;
-	public void init() {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-		context.scan("com.moseeker.baseorm");
-		context.refresh();
-		position= context.getBean(PositionServiceImpl.class);
-	}
-	@Test
-	public void getCustomField() throws Exception{
-		PositionDao.Iface position = ServiceManager.SERVICEMANAGER
-				.getService(PositionDao.Iface.class);
-		CommonQuery query=new CommonQuery();
-		HashMap<String,String> map=new HashMap<String,String>();
-		map.put("company_id", "1");
-		query.setEqualFilter(map);
-		Response result=position.getJobCustoms(query);
-		System.out.println(result);
-	}
+//	private PositionServiceImpl position;
+//	public void init() {
+//		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+//		context.scan("com.moseeker.baseorm");
+//		context.refresh();
+//		position= context.getBean(PositionServiceImpl.class);
+//	}
+//	@Test
+//	public void getCustomField() throws Exception{
+//		PositionDao.Iface position = ServiceManager.SERVICEMANAGER
+//				.getService(PositionDao.Iface.class);
+//		CommonQuery query=new CommonQuery();
+//		HashMap<String,String> map=new HashMap<String,String>();
+//		map.put("company_id", "1");
+//		query.setEqualFilter(map);
+//		Response result=position.getJobCustoms(query);
+//		System.out.println(result);
+//	}
 }
