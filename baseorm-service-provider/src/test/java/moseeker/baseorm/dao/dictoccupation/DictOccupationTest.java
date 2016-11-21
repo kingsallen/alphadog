6a8440a40781cbd549061b10e4e2b070b0d340f9
zztaiwll <zztaiwll@zztaiwlldeMac-mini.local>
2016-11-21 16:57:50 +0800

    张泽腾 hr3.2 职位职能

--- baseorm-service-provider/src/test/java/moseeker/baseorm/dao/dictoccupation/DictOccupationTest.java
+++ baseorm-service-provider/src/test/java/moseeker/baseorm/dao/dictoccupation/DictOccupationTest.java
@@ -11,40 +11,40 @@ import com.moseeker.thrift.gen.common.struct.Response;
 import com.moseeker.thrift.gen.dict.service.DictOccupationDao;
 
 public class DictOccupationTest {
-	private DictDaoServiceImpl service;
-	private PositionServiceImpl position;
-	public void init() {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-		context.scan("com.moseeker.baseorm");
-		context.refresh();
-		service = context.getBean(DictDaoServiceImpl.class);
-		position= context.getBean(PositionServiceImpl.class);
-	}
-	@Test
-	public void testGetAllOccupation(){
-		init();
-		//Response result=service.queryAll51Occupation();
-		Response result=service.occupations51();
-	}
-	@Test
-	public void getOccupation() throws Exception{
-		DictOccupationDao.Iface dictOccupationDao = ServiceManager.SERVICEMANAGER
-				.getService(DictOccupationDao.Iface.class);
-//		Map<String,Object> map=new HashMap<String,Object>();
-//		map.put("single_layer", false);
-//		map.put("channel", 1);
-//		String param=JSONObject.toJSONString(map);
-		Response result=dictOccupationDao.getOccupations51();
-		System.out.println(result);
-	}
-	@Test
-	public void testJobOccupation(){
-		init();
-		CommonQuery query=new CommonQuery();
-		HashMap<String,String> map=new HashMap<String,String>();
-		map.put("company_id", "1");
-		query.setEqualFilter(map);
-		Response result=position.getJobOccupation(query);
-		System.out.println(result.getData());
-	}
+//	private DictDaoServiceImpl service;
+//	private PositionServiceImpl position;
+//	public void init() {
+//		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+//		context.scan("com.moseeker.baseorm");
+//		context.refresh();
+//		service = context.getBean(DictDaoServiceImpl.class);
+//		position= context.getBean(PositionServiceImpl.class);
+//	}
+//	@Test
+//	public void testGetAllOccupation(){
+//		init();
+//		//Response result=service.queryAll51Occupation();
+//		Response result=service.occupations51();
+//	}
+//	@Test
+//	public void getOccupation() throws Exception{
+//		DictOccupationDao.Iface dictOccupationDao = ServiceManager.SERVICEMANAGER
+//				.getService(DictOccupationDao.Iface.class);
+////		Map<String,Object> map=new HashMap<String,Object>();
+////		map.put("single_layer", false);
+////		map.put("channel", 1);
+////		String param=JSONObject.toJSONString(map);
+//		Response result=dictOccupationDao.getOccupations51();
+//		System.out.println(result);
+//	}
+//	@Test
+//	public void testJobOccupation(){
+//		init();
+//		CommonQuery query=new CommonQuery();
+//		HashMap<String,String> map=new HashMap<String,String>();
+//		map.put("company_id", "1");
+//		query.setEqualFilter(map);
+//		Response result=position.getJobOccupation(query);
+//		System.out.println(result.getData());
+//	}
 }
