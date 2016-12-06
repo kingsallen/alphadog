75ae0acaa48c3382f0a8a1e3ebc29c8c4460ad88
翁剑飞 <wengjianfei@moseeker.com>
2016-12-06 11:35:41 +0800

    解决冲突

--- warn-service-provider/src/main/java/com/moseeker/warn/service/ManageService.java
+++ warn-service-provider/src/main/java/com/moseeker/warn/service/ManageService.java
@@ -9,14 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-<<<<<<< HEAD
-import com.alibaba.fastjson.JSONObject;
-import com.moseeker.common.constants.AppId;
-import com.moseeker.common.constants.KeyIdentifier;
-import com.moseeker.common.redis.RedisClient;
-import com.moseeker.common.redis.RedisClientFactory;
-=======
->>>>>>> f3d910d733004ccf89ae29a23631cd0a3fb2f2df
 import com.moseeker.warn.dto.Event;
 import com.moseeker.warn.dto.WarnMsg;
 import com.moseeker.warn.utils.SendChannel;
@@ -26,15 +18,6 @@ public class ManageService {
 	
 	private Logger log = LoggerFactory.getLogger(getClass());
 	
-<<<<<<< HEAD
-	/**
-	 * 从redis发送队列中获取信息
-	 * @return
-	 */
-	private String fetchConstantlyMessage() {
-		RedisClient redisClient = RedisClientFactory.getCacheClient();
-		return redisClient.brpop(AppId.APPID_ALPHADOG.getValue(), KeyIdentifier.NEW_WARNING_REDIS_KEY.toString()).get(1);
-=======
 	public String environment;
 	
 	@PostConstruct
@@ -46,7 +29,6 @@ public class ManageService {
 		} catch (IOException e) {
 			log.error("load warn.properties error:", e);
 		}
->>>>>>> f3d910d733004ccf89ae29a23631cd0a3fb2f2df
 	}
 	
 	/**
