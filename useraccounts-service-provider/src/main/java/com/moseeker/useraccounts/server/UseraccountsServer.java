c4a66b910c2ce7da1ac0aa0aa760ef720105e599
jack frame weng <jack@wjfs-MacBook-Pro.local>
2017-03-03 10:17:21 +0800

    删除无效代码

--- useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/server/UseraccountsServer.java
+++ useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/server/UseraccountsServer.java
@@ -28,14 +28,6 @@ public class UseraccountsServer {
 
         try {
             AnnotationConfigApplicationContext acac = initSpring();
-//            MultiRegServer server = new MultiRegServer(UseraccountsServer.class,
-//                    ServerNodeUtils.getPort(args),
-//                    acac.getBean(UserHrAccountServiceImpl.class),
-//                    acac.getBean(UsersettingsServicesImpl.class),
-//                    acac.getBean(UserCommonThriftService.class),
-//                    acac.getBean(UserCenterThriftService.class),
-//                    acac.getBean(UseraccountsServiceImpl.class));
-//            server.start(); // 阻塞式IO + 多线程处理
             MoServer server = new MoServer(acac,"",
             		acac.getBean(UserHrAccountServiceImpl.class),
                     acac.getBean(UsersettingsServicesImpl.class),
