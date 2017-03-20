251ff3457627aed64e67dee16d584da19aa9f200
zhangdi <zhangdi@moseeker.com>
2017-03-20 12:15:59 +0800

    Profile下载api配置到setting.properties

--- baseorm-service-provider/src/main/java/com/moseeker/baseorm/Thriftservice/ProfileProfileDaoThriftService.java
+++ baseorm-service-provider/src/main/java/com/moseeker/baseorm/Thriftservice/ProfileProfileDaoThriftService.java
@@ -17,12 +17,13 @@ public class ProfileProfileDaoThriftService implements ProfileProfileDao.Iface {
 
     @Autowired
     ProfileDao profileDao;
+
     @Override
-    public Response getResourceByApplication(int companyId, int sourceId, int atsStatus, boolean recommender, boolean dl_url_required) throws TException {
+    public Response getResourceByApplication(String downloadApi, int companyId, int sourceId, int atsStatus, boolean recommender, boolean dl_url_required) throws TException {
         try {
-            return profileDao.getResourceByApplication(companyId, sourceId, atsStatus, recommender,dl_url_required);
+            return profileDao.getResourceByApplication(downloadApi, companyId, sourceId, atsStatus, recommender, dl_url_required);
         } catch (Exception e) {
-            throw new TException(MessageFormat.format("getResourceByApplication查询错误:companyId={1}&sourceId={2}&atsStatus={3}&recommender={4}", companyId,sourceId,atsStatus,recommender));
+            throw new TException(MessageFormat.format("getResourceByApplication查询错误:companyId={1}&sourceId={2}&atsStatus={3}&recommender={4}", companyId, sourceId, atsStatus, recommender));
         }
     }
 
--- thrift-jooq/thrift/dao/service/profiledb_service.thrift
+++ thrift-jooq/thrift/dao/service/profiledb_service.thrift
@@ -4,7 +4,7 @@ include "../../common/struct/common_struct.thrift"
 include "../struct/profiledb_struct.thrift"
 
 service ProfileProfileDao{
-    common_struct.Response getResourceByApplication(1:i32 companyId,2:i32 sourceId,3:i32 atsStatus,4:bool recommender,5:bool dl_url_required);
+    common_struct.Response getResourceByApplication(1:string downloadApi,2:i32 companyId,3:i32 sourceId,4:i32 atsStatus,5:bool recommender,6:bool dl_url_required);
 }
 
 
