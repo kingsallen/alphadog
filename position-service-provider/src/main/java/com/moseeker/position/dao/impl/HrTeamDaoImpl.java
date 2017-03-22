89b38d85984a829fb7e63097b626c535a7f8f546
yuyunfeng <yuyunfeng@ceair.com>
2017-03-22 09:26:10 +0800

    去掉position依赖baseorm

--- position-service-provider/src/main/java/com/moseeker/position/dao/impl/HrTeamDaoImpl.java
+++ position-service-provider/src/main/java/com/moseeker/position/dao/impl/HrTeamDaoImpl.java
@@ -1,13 +1,15 @@
 package com.moseeker.position.dao.impl;
 
-import com.moseeker.baseorm.db.hrdb.tables.HrTeam;
-import com.moseeker.baseorm.db.hrdb.tables.records.HrTeamRecord;
-import com.moseeker.baseorm.util.BaseDaoImpl;
+
+import com.moseeker.common.providerutils.daoutils.BaseDaoImpl;
+import com.moseeker.db.hrdb.tables.HrTeam;
+import com.moseeker.db.hrdb.tables.records.HrTeamRecord;
+import com.moseeker.position.dao.HrTeamDao;
 
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class HrTeamDaoImpl extends BaseDaoImpl<HrTeamRecord, HrTeam> {
+public class HrTeamDaoImpl extends BaseDaoImpl<HrTeamRecord, HrTeam> implements HrTeamDao {
 
     @Override
     protected void initJOOQEntity() {
