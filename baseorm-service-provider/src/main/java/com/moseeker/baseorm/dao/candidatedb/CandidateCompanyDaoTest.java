b7890565762466710d4cd51768bf921cb86ad860
jack frame weng <jack@wjfs-MacBook-Pro.local>
2017-03-27 11:27:13 +0800

    删除无效引用

--- baseorm-service-provider/src/main/java/com/moseeker/baseorm/dao/candidatedb/CandidateCompanyDaoTest.java
+++ baseorm-service-provider/src/main/java/com/moseeker/baseorm/dao/candidatedb/CandidateCompanyDaoTest.java
@@ -1,10 +1,8 @@
 package com.moseeker.baseorm.dao.candidatedb;
 
 import com.moseeker.baseorm.db.candidatedb.tables.records.CandidateCompanyRecord;
-import org.jooq.DSLContext;
 import org.jooq.impl.DefaultDSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -15,9 +13,6 @@ public class CandidateCompanyDaoTest {
 
     @Autowired
     DefaultDSLContext dsl;
-
-    @Autowired
-    DataSourceTransactionManager txMgr;
 
     public CandidateCompanyRecord addCandidateCompany(CandidateCompanyRecord candidateCompanyRecord) {
 
--- baseorm-service-provider/src/main/java/com/moseeker/baseorm/dao/candidatedb/CandidatePositionDaoTest.java
+++ baseorm-service-provider/src/main/java/com/moseeker/baseorm/dao/candidatedb/CandidatePositionDaoTest.java
@@ -1,10 +1,8 @@
 package com.moseeker.baseorm.dao.candidatedb;
 
 import com.moseeker.baseorm.db.candidatedb.tables.records.CandidatePositionRecord;
-import org.jooq.DSLContext;
 import org.jooq.impl.DefaultDSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -14,9 +12,6 @@ import org.springframework.stereotype.Repository;
 public class CandidatePositionDaoTest {
     @Autowired
     DefaultDSLContext dsl;
-
-    @Autowired
-    DataSourceTransactionManager txMgr;
 
     public CandidatePositionRecord addCandidatePosition(CandidatePositionRecord candidatePositionRecord) {
         dsl.attach(candidatePositionRecord);
