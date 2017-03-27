8d6493f92947470cf5301456bd3b8d863661cf53
yuyunfeng <yuyunfeng@ceair.com>
2017-03-27 14:46:08 +0800

    修改UserHRaccountDao冲突 Please enter the commit message for your changes. Lines starting

--- baseorm-service-provider/src/main/java/com/moseeker/baseorm/Thriftservice/HRAccountDaoThriftService.java
+++ baseorm-service-provider/src/main/java/com/moseeker/baseorm/Thriftservice/HRAccountDaoThriftService.java
@@ -7,17 +7,12 @@ import com.moseeker.baseorm.db.userdb.tables.records.UserHrAccountRecord;
 import com.moseeker.common.constants.ChannelType;
 import com.moseeker.common.constants.ConstantErrorCodeMessage;
 import com.moseeker.common.providerutils.ResponseUtils;
-<<<<<<< HEAD
-=======
-import com.moseeker.common.providerutils.bzutils.JOOQBaseServiceImpl;
 import com.moseeker.common.util.BeanUtils;
-import com.moseeker.db.userdb.tables.records.UserHrAccountRecord;
->>>>>>> refactor/v4.1.2
 import com.moseeker.thrift.gen.common.struct.CommonQuery;
 import com.moseeker.thrift.gen.common.struct.Response;
 import com.moseeker.thrift.gen.dao.service.UserHrAccountDao.Iface;
-import com.moseeker.thrift.gen.useraccounts.struct.UserHrAccount;
 import com.moseeker.thrift.gen.useraccounts.struct.BindAccountStruct;
+import com.moseeker.thrift.gen.useraccounts.struct.UserHrAccount;
 
 import org.apache.thrift.TException;
 import org.joda.time.DateTime;
@@ -28,10 +23,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
-import java.text.ParseException;
 
 /**
  * 提供hr帐号表的单表操作
@@ -42,122 +36,12 @@ import java.text.ParseException;
  * @author wjf
  */
 @Service
-<<<<<<< HEAD
 public class HRAccountDaoThriftService implements Iface {
-	
-	private Logger logger = LoggerFactory.getLogger(ChannelType.class);
-	
-	@Autowired
-	private UserHRAccountDao hraccountDao;
-	
-	@Autowired
-	private HRThirdPartyAccountDao hrThirdPartyAccountDao;
-
-	@Override
-	public Response getAccount(CommonQuery query) throws TException {
-		try {
-			UserHrAccountRecord record = hraccountDao.getResource(query);
-			if(record != null) {
-				return ResponseUtils.success(record.intoMap());
-			} else {
-				return ResponseUtils.fail(ConstantErrorCodeMessage.PROGRAM_DATA_EMPTY);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			logger.error(e.getMessage(), e);
-			return ResponseUtils.fail(ConstantErrorCodeMessage.PROGRAM_DATA_EMPTY);
-		} finally {
-			//do nothing
-		}
-	}
-
-	@Override
-	public Response getThirdPartyAccount(CommonQuery query) throws TException {
-		try {
-			HrThirdPartyAccountRecord record = hrThirdPartyAccountDao.getResource(query);
-			if(record != null) {
-				return ResponseUtils.success(record.intoMap());
-			} else {
-				return ResponseUtils.fail(ConstantErrorCodeMessage.PROGRAM_DATA_EMPTY);
-			}
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-			return ResponseUtils.fail(ConstantErrorCodeMessage.PROGRAM_DATA_EMPTY);
-		} finally {
-			//do nothing
-		}
-	}
-
-	@Override
-	public Response createThirdPartyAccount(BindAccountStruct account) throws TException {
-		
-		try {
-			HrThirdPartyAccountRecord record = new HrThirdPartyAccountRecord();
-			record.setBinding((short)account.getBinding());
-			record.setChannel((short)account.getChannel());
-			record.setCompanyId(UInteger.valueOf(account.getCompany_id()));
-			Timestamp now = new Timestamp(System.currentTimeMillis());
-			record.setCreateTime(now);
-			record.setMembername(account.getMember_name());
-			record.setPassword(account.getPassword());
-			record.setRemainNum(UInteger.valueOf(account.getRemainNum()));
-			record.setSyncTime(now);
-			record.setUsername(account.getUsername());
-			hrThirdPartyAccountDao.postResource(record);
-			HashMap<String, Object> map = new HashMap<>();
-			map.put("remain_num", account.getRemainNum());
-			DateTime dt = new DateTime(now.getTime());
-			map.put("sync_time", dt.toString("yyyy-MM-dd HH:mm:ss"));
-			return ResponseUtils.success(map);
-		} catch (Exception e) {
-			e.printStackTrace();
-			logger.error(e.getMessage(), e);
-			return ResponseUtils.fail(ConstantErrorCodeMessage.PROGRAM_EXCEPTION);
-		} finally {
-			
-		}
-	}
-
-	@Override
-	public Response upsertThirdPartyAccount(BindAccountStruct account) throws TException {
-		try {
-			HrThirdPartyAccountRecord record = new HrThirdPartyAccountRecord();
-			record.setBinding((short)account.getBinding());
-			record.setChannel((short)account.getChannel());
-			record.setCompanyId(UInteger.valueOf(account.getCompany_id()));
-			Timestamp now = new Timestamp(System.currentTimeMillis());
-			record.setCreateTime(now);
-			record.setMembername(account.getMember_name());
-			record.setPassword(account.getPassword());
-			record.setRemainNum(UInteger.valueOf(account.getRemainNum()));
-			record.setSyncTime(now);
-			record.setBinding((short)1);
-			record.setUsername(account.getUsername());
-			int count = hrThirdPartyAccountDao.upsertResource(record);
-			if(count == 0) {
-				return ResponseUtils.fail(ConstantErrorCodeMessage.PROGRAM_POST_FAILED);
-			}
-			HashMap<String, Object> map = new HashMap<>();
-			map.put("remain_num", account.getRemainNum());
-			DateTime dt = new DateTime(now.getTime());
-			map.put("sync_time", dt.toString("yyyy-MM-dd HH:mm:ss"));
-			return ResponseUtils.success(map);
-		} catch (Exception e) {
-			e.printStackTrace();
-			logger.error(e.getMessage(), e);
-			return ResponseUtils.fail(ConstantErrorCodeMessage.PROGRAM_EXCEPTION);
-		} finally {
-			
-		}
-	}
-}
-=======
-public class HRAccountDaoThriftService extends JOOQBaseServiceImpl<UserHrAccount, UserHrAccountRecord> implements Iface {
 
     private Logger logger = LoggerFactory.getLogger(ChannelType.class);
 
     @Autowired
-    private HRAccountDao hraccountDao;
+    private UserHRAccountDao hraccountDao;
 
     @Autowired
     private HRThirdPartyAccountDao hrThirdPartyAccountDao;
@@ -178,15 +62,6 @@ public class HRAccountDaoThriftService extends JOOQBaseServiceImpl<UserHrAccount
         } finally {
             //do nothing
         }
-    }
-
-
-    public HRAccountDao getHraccountDao() {
-        return hraccountDao;
-    }
-
-    public void setHraccountDao(HRAccountDao hraccountDao) {
-        this.hraccountDao = hraccountDao;
     }
 
     @Override
@@ -273,9 +148,13 @@ public class HRAccountDaoThriftService extends JOOQBaseServiceImpl<UserHrAccount
     public Response getAccounts(CommonQuery query) throws TException {
         try {
             List<UserHrAccountRecord> records = hraccountDao.getResources(query);
+            List<UserHrAccount> datas = new ArrayList<>();
             if (records != null && records.size() > 0) {
-                List<UserHrAccount> structs = DBsToStructs(records);
-                return ResponseUtils.success(structs);
+                records.forEach(record -> {
+                    UserHrAccount data = (UserHrAccount) BeanUtils.DBToStruct(UserHrAccount.class, record);
+                    datas.add(data);
+                });
+                return ResponseUtils.success(datas);
             } else {
                 return ResponseUtils.fail(ConstantErrorCodeMessage.PROGRAM_DATA_EMPTY);
             }
@@ -284,25 +163,6 @@ public class HRAccountDaoThriftService extends JOOQBaseServiceImpl<UserHrAccount
             logger.error(e.getMessage(), e);
             return ResponseUtils.fail(ConstantErrorCodeMessage.PROGRAM_DATA_EMPTY);
         } finally {
-            //do nothing
-
         }
     }
-
-    @Override
-    protected void initDao() {
-        this.dao = hraccountDao;
-    }
-
-    @Override
-    protected UserHrAccountRecord structToDB(UserHrAccount userHrAccount) throws ParseException {
-        return (UserHrAccountRecord) BeanUtils.structToDB(userHrAccount, UserHrAccountRecord.class);
-    }
-
-    @Override
-    protected UserHrAccount DBToStruct(UserHrAccountRecord userHrAccountRecord) {
-        return (UserHrAccount) BeanUtils.DBToStruct(UserHrAccount.class, userHrAccountRecord);
-
-    }
 }
->>>>>>> refactor/v4.1.2
