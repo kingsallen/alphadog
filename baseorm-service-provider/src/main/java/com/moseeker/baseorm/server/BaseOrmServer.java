be5ae028f7857c7572cfb1059a470aaffca87d9b
zhangdi <zhangdi@moseeker.com>
2017-05-03 13:49:01 +0800

    Fix error

--- baseorm-service-provider/src/main/java/com/moseeker/baseorm/server/BaseOrmServer.java
+++ baseorm-service-provider/src/main/java/com/moseeker/baseorm/server/BaseOrmServer.java
@@ -43,7 +43,6 @@ public class BaseOrmServer {
                     acac.getBean(LogDBDaoThriftService.class),
                     acac.getBean(ThirdPartyUserDaoThriftService.class),
                     acac.getBean(UserEmployeeDaoThriftService.class),
-                    acac.getBean(ProfileProfileDaoThriftService.class),
                     acac.getBean(TalentpoolDaoThriftService.class));
             server.startServer();
             synchronized (BaseOrmServer.class) {
