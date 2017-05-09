1910c93311e0747dd6a51bb5cca9ac9d66fed4db
jack frame weng <jack@wjfs-MacBook-Pro.local>
2017-05-09 11:32:42 +0800

    修改JooqCrudImpl类，修复部分逻辑问题

--- baseorm-service-provider/src/main/java/com/moseeker/baseorm/crud/LocalQuery.java
+++ baseorm-service-provider/src/main/java/com/moseeker/baseorm/crud/LocalQuery.java
@@ -5,14 +5,16 @@ import com.moseeker.common.util.query.Query;
 import org.jooq.*;
 import org.jooq.impl.TableImpl;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
 /**
  * Created by zhangdi on 2017/3/17.
  * CommonQuery包装类，实现CommonQuery和jooq语法对接
  */
-public class LocalQuery<R extends Record> {
+class LocalQuery<R extends Record> {
     DSLContext create;
     TableImpl table;
     Query query;
@@ -44,54 +46,62 @@ public class LocalQuery<R extends Record> {
     }
 
     public Collection<? extends SelectField<?>> buildSelect() {
-        return query.getAttributes().stream()
-                .map(select -> {
-                    Field<?> field = table.field(select.getField());
-                    if (field == null) {
-                        throw new OrmException("field '" + select.getField() + "' not found in table " + table.getName());
-                    } else {
-                        switch (select.getSelectOp()) {
-                            case AVG:
-                                return field.avg().as(select.getField() + "_avg");
-                            case COUNT:
-                                return field.count().as(select.getField() + "_count");
-                            case COUNT_DISTINCT:
-                                return field.countDistinct().as(select.getField() + "_count_distinct");
-                            case TRIM:
-                                return field.trim().as(select.getField() + "_trim");
-                            case LCASE:
-                                return field.lower().as(select.getField() + "_lower");
-                            case LEN:
-                                return field.length().as(select.getField() + "_length");
-                            case MAX:
-                                return field.max().as(select.getField() + "_max");
-                            case MIN:
-                                return field.min().as(select.getField() + "_min");
-                            case ROUND:
-                                return field.round().as(select.getField() + "_round");
-                            case SUM:
-                                return field.sum().as(select.getField() + "_sum");
-                            case UCASE:
-                                return field.upper().as(select.getField() + "_ucase");
-                            default:
-                                return field;
+        if (query != null && query.getAttributes() != null) {
+            return query.getAttributes().stream()
+                    .map(select -> {
+                        Field<?> field = table.field(select.getField());
+                        if (field == null) {
+                            throw new OrmException("field '" + select.getField() + "' not found in table " + table.getName());
+                        } else {
+                            switch (select.getSelectOp()) {
+                                case AVG:
+                                    return field.avg().as(select.getField() + "_avg");
+                                case COUNT:
+                                    return field.count().as(select.getField() + "_count");
+                                case COUNT_DISTINCT:
+                                    return field.countDistinct().as(select.getField() + "_count_distinct");
+                                case TRIM:
+                                    return field.trim().as(select.getField() + "_trim");
+                                case LCASE:
+                                    return field.lower().as(select.getField() + "_lower");
+                                case LEN:
+                                    return field.length().as(select.getField() + "_length");
+                                case MAX:
+                                    return field.max().as(select.getField() + "_max");
+                                case MIN:
+                                    return field.min().as(select.getField() + "_min");
+                                case ROUND:
+                                    return field.round().as(select.getField() + "_round");
+                                case SUM:
+                                    return field.sum().as(select.getField() + "_sum");
+                                case UCASE:
+                                    return field.upper().as(select.getField() + "_ucase");
+                                default:
+                                    return field;
+                            }
                         }
-                    }
-                })
-                .collect(Collectors.toList());
+                    })
+                    .collect(Collectors.toList());
+        } else {
+            return null;
+        }
     }
 
     public Collection<? extends Field<?>> buildGroup() {
-        return query.getGroups().stream()
-                .map(groupField -> {
-                    Field<?> field = table.field(groupField);
-                    if (field == null) {
-                        throw new OrmException("field '" + groupField + "' not found in table " + table.getName());
-                    } else {
-                        return field;
-                    }
-                })
-                .collect(Collectors.toList());
+        if (query != null && query.getGroups() != null) {
+            return query.getGroups().stream()
+                    .map(groupField -> {
+                        Field<?> field = table.field(groupField);
+                        if (field == null) {
+                            throw new OrmException("field '" + groupField + "' not found in table " + table.getName());
+                        } else {
+                            return field;
+                        }
+                    })
+                    .collect(Collectors.toList());
+        } else {
+            return null;
+        }
     }
 
     /**
@@ -109,34 +119,63 @@ public class LocalQuery<R extends Record> {
      * @return
      */
     public Collection<? extends SortField<?>> buildOrder() {
-        return query.getOrders().stream()
-                .map(orderBy -> {
-                    Field<?> field = table.field(orderBy.getField());
-                    if (field == null) {
-                        throw new OrmException("field '" + orderBy.getField() + "' not found in table " + table.getName());
-                    } else {
-                        switch (orderBy.getOrder()) {
-                            case DESC:
-                                return field.desc();
-                            default:
-                                return field.asc();
+        if (query != null && query.getOrders() != null) {
+            return query.getOrders().stream()
+                    .map(orderBy -> {
+                        Field<?> field = table.field(orderBy.getField());
+                        if (field == null) {
+                            throw new OrmException("field '" + orderBy.getField() + "' not found in table " + table.getName());
+                        } else {
+                            switch (orderBy.getOrder()) {
+                                case DESC:
+                                    return field.desc();
+                                default:
+                                    return field.asc();
+                            }
                         }
-                    }
-                })
-                .collect(Collectors.toList());
+                    })
+                    .collect(Collectors.toList());
+        } else {
+            return null;
+        }
     }
 
-    public SelectConditionStep convertToSelect() {
-        return create
-                .select(buildSelect())
-                .from(table)
-                .where(buildConditions());
+    public SelectJoinStep<Record> convertForCount() {
+        SelectJoinStep<Record> table = null;
+        Collection<? extends SelectField<?>> selectFields = buildSelect();
+        if (selectFields != null && selectFields.size() > 0) {
+            table = create.select(selectFields).from(table);
+        } else {
+            table = create.select().from(table);
+        }
+        Collection<? extends Field<?>> groups = buildGroup();
+        if (groups != null && groups.size() > 0) {
+            table.groupBy(groups);
+        }
+
+        return table;
     }
 
-    public ResultQuery<R> convertToResultQuery() {
-        return convertToSelect()
-                .groupBy(buildGroup())
-                .orderBy(buildOrder())
-                .limit((getPage() - 1) * getPageSize(), getPageSize());
+    public SelectJoinStep<Record> convertToResultQuery() {
+        SelectJoinStep<Record> table = null;
+        Collection<? extends SelectField<?>> selectFields = buildSelect();
+        if (selectFields != null && selectFields.size() > 0) {
+            table = create.select(selectFields).from(table);
+        } else {
+            table = create.select().from(table);
+        }
+        org.jooq.Condition condition = buildConditions();
+        if (condition != null) {
+            table.where(condition);
+        }
+        Collection<? extends Field<?>> groups = buildGroup();
+        if (groups != null && groups.size() > 0) {
+            table.groupBy(groups);
+        }
+        Collection<? extends SortField<?>> orders = buildOrder();
+        if (orders != null && orders.size() > 0) {
+            table.orderBy(orders);
+        }
+        return table;
     }
 }
