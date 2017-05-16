fcc9ab51037b5b77c8789040739ac2595e60f7f3
lucky8987 <lucky8987@lucky8987deMac-mini.local>
2017-05-16 18:50:40 +0800

    红包职位列表

--- position-service-provider/src/main/java/com/moseeker/position/service/fundationbs/PositionService.java
+++ position-service-provider/src/main/java/com/moseeker/position/service/fundationbs/PositionService.java
@@ -1259,6 +1259,7 @@ public class PositionService extends JOOQBaseServiceImpl<Position, JobPositionRe
             qu.addEqualFilter("status", "3"); //正在运行
             qu.addEqualFilter("company_id", String.valueOf(company_id));
             qu.addEqualFilter("type", "[2,3]"); //转发类职位
+            qu.setPer_page(Integer.MAX_VALUE);
             List<HrHbConfigDO> hbConfigs = hrDao.getHbConfigs(qu);
             List<Integer> hbConfgIds = hbConfigs.stream().map(HrHbConfigDO::getId).collect(Collectors.toList());
             String allHbConfigIdsFilterString = "[" + org.apache.commons.lang.StringUtils.join(hbConfgIds.toArray(), ",") + "]";
