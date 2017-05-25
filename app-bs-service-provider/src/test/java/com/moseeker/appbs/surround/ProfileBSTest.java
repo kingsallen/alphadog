4e6eed9669d034d750ca4367e8f97856c31cbfcb
zztaiwll <zztaiwll@zztaiwlldeMac-mini.local>
2017-05-25 11:24:14 +0800

    修改测试类

--- app-bs-service-provider/src/test/java/com/moseeker/appbs/surround/ProfileBSTest.java
+++ app-bs-service-provider/src/test/java/com/moseeker/appbs/surround/ProfileBSTest.java
@@ -3,65 +3,62 @@ package com.moseeker.appbs.surround;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.alibaba.fastjson.JSONObject;
+import com.moseeker.apps.config.AppConfig;
 import com.moseeker.apps.service.ProfileBS;
 import com.moseeker.common.providerutils.ResponseUtils;
 import com.moseeker.thrift.gen.application.service.JobApplicationServices;
 import com.moseeker.thrift.gen.common.struct.Response;
 import com.moseeker.thrift.gen.profile.service.WholeProfileServices;
 import com.moseeker.thrift.gen.useraccounts.service.UseraccountsServices;
-
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes =AppConfig.class)
+//@Transactional
 public class ProfileBSTest {
-	@Mock
-	UseraccountsServices.Iface useraccountsServices;
-	@Mock
-	WholeProfileServices.Iface wholeProfileService;
-	@Mock
-	JobApplicationServices.Iface applicationService;
-	@Rule 
-	public MockitoRule mockitoRule = MockitoJUnit.rule();
-	
-	private ProfileBS profileBS;
-	
-	public AnnotationConfigApplicationContext initSpring(){
-		AnnotationConfigApplicationContext acac = new AnnotationConfigApplicationContext();
-		acac.scan("com.moseeker.apps");
-		acac.scan("com.moseeker.common.aop.iface"); //开启接口统计
-		acac.scan("com.moseeker.common.aop.notify");
-		acac.scan("com.moseeker.baseorm");
-        acac.refresh();
-        return acac;
-	}
-	@Before
-	public void init() throws Exception{
-		AnnotationConfigApplicationContext acac=initSpring();
-		profileBS=acac.getBean(ProfileBS.class);
-		Mockito.when(useraccountsServices.ifExistProfile(Mockito.anyString())).thenReturn(false);
-		Mockito.when(applicationService.getApplicationByUserIdAndPositionId(Mockito.anyLong(),
-				Mockito.anyLong(),Mockito.anyLong())).thenReturn(ResponseUtils.success(true));
-		Mockito.when(applicationService.postApplication(Mockito.any())).thenReturn(ResponseUtils.success(null));
-		Mockito.when(wholeProfileService.createProfile(Mockito.anyString())).thenReturn(ResponseUtils.success(null));
-		Mockito.when(wholeProfileService.improveProfile(Mockito.anyString())).thenReturn(ResponseUtils.success(true));
-		Mockito.when(useraccountsServices.createRetrieveProfileUser(Mockito.any())).thenReturn(1000);
-		profileBS.setApplicationService(applicationService);
-		profileBS.setUseraccountsServices(useraccountsServices);
-		profileBS.setWholeProfileService(wholeProfileService);
-        
-	}
-	@Test
-	public void retrieveProfileTest(){
-		JSONObject json=new JSONObject();
-		JSONObject user=new JSONObject();
-		user.put("mobile", "111");
-		json.put("user", user);
-		String profile=json.toJSONString();
-		Response result=profileBS.retrieveProfile(124576,profile,3);
-		System.out.println(result);
-	}
-
+//	@Mock
+//	UseraccountsServices.Iface useraccountsServices;
+//	@Mock
+//	WholeProfileServices.Iface wholeProfileService;
+//	@Mock
+//	JobApplicationServices.Iface applicationService;
+//	@Rule 
+//	public MockitoRule mockitoRule = MockitoJUnit.rule();
+//	@Autowired
+//	private ProfileBS profileBS;
+//	
+//	@Before
+//	public void init() throws Exception{
+//		Mockito.when(useraccountsServices.ifExistProfile(Mockito.anyString())).thenReturn(false);
+//		Mockito.when(applicationService.getApplicationByUserIdAndPositionId(Mockito.anyLong(),
+//				Mockito.anyLong(),Mockito.anyLong())).thenReturn(ResponseUtils.success(true));
+//		Mockito.when(applicationService.postApplication(Mockito.any())).thenReturn(ResponseUtils.success(null));
+//		Mockito.when(wholeProfileService.createProfile(Mockito.anyString())).thenReturn(ResponseUtils.success(null));
+//		Mockito.when(wholeProfileService.improveProfile(Mockito.anyString())).thenReturn(ResponseUtils.success(true));
+//		Mockito.when(useraccountsServices.createRetrieveProfileUser(Mockito.any())).thenReturn(1000);
+//		profileBS.setApplicationService(applicationService);
+//		profileBS.setUseraccountsServices(useraccountsServices);
+//		profileBS.setWholeProfileService(wholeProfileService);
+//        
+//	}
+//	@Test
+//	public void retrieveProfileTest(){
+//		JSONObject json=new JSONObject();
+//		JSONObject user=new JSONObject();
+//		user.put("mobile", "111");
+//		json.put("user", user);
+//		String profile=json.toJSONString();
+//		Response result=profileBS.retrieveProfile(124576,profile,3);
+//		System.out.println(result);
+//	}
 }
--- app-bs-service-provider/src/test/java/com/moseeker/appbs/surround/ProfileProcessBSTest.java
+++ app-bs-service-provider/src/test/java/com/moseeker/appbs/surround/ProfileProcessBSTest.java
@@ -1,10 +1,19 @@
 package com.moseeker.appbs.surround;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.moseeker.apps.config.AppConfig;
 import com.moseeker.apps.service.ProfileProcessBS;
 import com.moseeker.baseorm.db.hrdb.tables.records.HrWxWechatRecord;
 import com.moseeker.common.providerutils.ResponseUtils;
@@ -12,42 +21,36 @@ import com.moseeker.thrift.gen.common.struct.Response;
 import com.moseeker.thrift.gen.company.service.CompanyServices;
 import com.moseeker.thrift.gen.mq.service.MqService;
 
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes =AppConfig.class)
+//@Transactional
 public class ProfileProcessBSTest {
-	@Mock
-	MqService.Iface mqService;
-	@Mock
-	CompanyServices.Iface companyService;
-	
-	private ProfileProcessBS profileProcessBS;
-	
-	public AnnotationConfigApplicationContext initSpring(){
-		AnnotationConfigApplicationContext acac = new AnnotationConfigApplicationContext();
-		acac.scan("com.moseeker.apps");
-		acac.scan("com.moseeker.common.aop.iface"); //开启接口统计
-		acac.scan("com.moseeker.common.aop.notify");
-		acac.scan("com.moseeker.baseorm");
-        acac.refresh();
-        return acac;
-	}
-	@Before
-	public void init() throws Exception{
-		AnnotationConfigApplicationContext acac=initSpring();
-		profileProcessBS=acac.getBean(ProfileProcessBS.class);
-		HrWxWechatRecord record=new HrWxWechatRecord();
-		record.setId(278);
-		record.setCompanyId(40104);
-		record.setType((byte)1);
-		record.setSignature("NjM2YjY3OWEzZjIxY2ZiM2JkOTJmOWZiZTY3YmVlYmY5NGEwZDBlOA==");
-		record.setName("乐山路4Helper");
-		record.setAppid("wx4eb438db0b7a28cf");
+//	@Mock
+//	MqService.Iface mqService;
+//	@Mock
+//	CompanyServices.Iface companyService;
+//	@Rule 
+//	public MockitoRule mockitoRule = MockitoJUnit.rule();
+//	@Autowired
+//	private ProfileProcessBS profileProcessBS;
+//	
+//	@Before
+//	public void init() throws Exception{
+//		HrWxWechatRecord record=new HrWxWechatRecord();
+//		record.setId(278);
+//		record.setCompanyId(40104);
+//		record.setType((byte)1);
+//		record.setSignature("NjM2YjY3OWEzZjIxY2ZiM2JkOTJmOWZiZTY3YmVlYmY5NGEwZDBlOA==");
+//		record.setName("乐山路4Helper");
+//		record.setAppid("wx4eb438db0b7a28cf");
 //		Mockito.when(companyService.getWechat(Mockito.anyLong(),Mockito.anyLong())).thenReturn(ResponseUtils.success(record.intoMap()));
-		Mockito.when(mqService.messageTemplateNotice(Mockito.any())).thenReturn(ResponseUtils.success(null));
-		profileProcessBS.setCompanyService(companyService);
-		profileProcessBS.setMqService(mqService);
-	}
-	@Test
-	public void processProfileAtsTest(){
-		Response result=profileProcessBS.processProfileAts(12, "200");
-		System.out.println(result);
-	}
+//		Mockito.when(mqService.messageTemplateNotice(Mockito.any())).thenReturn(ResponseUtils.success(null));
+//		profileProcessBS.setCompanyService(companyService);
+//		profileProcessBS.setMqService(mqService);
+//	}
+//	@Test
+//	public void processProfileAtsTest(){
+//		Response result=profileProcessBS.processProfileAts(10, "627216844");
+//		System.out.println(result);
+//	}
 }
