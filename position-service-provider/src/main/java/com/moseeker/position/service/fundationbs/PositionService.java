280c3d6d7e887d160e6758cae419f832a39ba43a
jack frame weng <jack@wjfs-MacBook-Pro.local>
2017-06-21 12:01:19 +0800

    修复 获取城市 条件为空时的空指针异常以及添加日志查看 获取职位时，条件的输出

--- position-service-provider/src/main/java/com/moseeker/position/service/fundationbs/PositionService.java
+++ position-service-provider/src/main/java/com/moseeker/position/service/fundationbs/PositionService.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.ValueFilter;
+import com.moseeker.baseorm.crud.JooqCrudImpl;
 import com.moseeker.baseorm.dao.dictdb.DictCityDao;
 import com.moseeker.baseorm.dao.dictdb.DictCityPostcodeDao;
 import com.moseeker.baseorm.dao.dictdb.DictConstantDao;
@@ -11,6 +12,7 @@ import com.moseeker.baseorm.dao.hrdb.*;
 import com.moseeker.baseorm.dao.jobdb.*;
 import com.moseeker.baseorm.db.dictdb.tables.records.DictCityPostcodeRecord;
 import com.moseeker.baseorm.db.dictdb.tables.records.DictCityRecord;
+import com.moseeker.baseorm.db.hrdb.tables.HrHbConfig;
 import com.moseeker.baseorm.db.hrdb.tables.records.HrCompanyAccountRecord;
 import com.moseeker.baseorm.db.hrdb.tables.records.HrTeamRecord;
 import com.moseeker.baseorm.db.hrdb.tables.records.HrThirdPartyPositionRecord;
@@ -46,6 +48,7 @@ import com.moseeker.thrift.gen.dao.struct.hrdb.*;
 import com.moseeker.thrift.gen.position.struct.*;
 import org.apache.thrift.TException;
 import org.jooq.Field;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -1307,6 +1310,7 @@ public class PositionService {
                 .and("company_id", company_id)
                 .and(condition)
                 .buildQuery();
+        logger.info("qu:",qu.toString());
         List<HrHbConfigDO> hbConfigs = hrHbConfigDao.getDatas(qu,HrHbConfigDO.class);
         List<Integer> hbConfgIds = hbConfigs.stream().map(HrHbConfigDO::getId).collect(Collectors.toList());
 
