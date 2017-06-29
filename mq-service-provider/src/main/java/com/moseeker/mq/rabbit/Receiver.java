0bd7550e7eb5d10d79615559f2ab03764f5176fb
lucky8987 <lucky8987@lucky8987deMac-mini.local>
2017-06-29 09:45:07 +0800

    集成rabbit-mq

---
+++ mq-service-provider/src/main/java/com/moseeker/mq/rabbit/Receiver.java
@@ -0,0 +1,18 @@
+package com.moseeker.mq.rabbit;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by lucky8987 on 17/6/28.
+ */
+@Component
+@RabbitListener(queues = "hello")
+public class Receiver {
+
+    @RabbitHandler
+    public void process(String hello) {
+        System.out.println("Receiver: " + hello);
+    }
+}
