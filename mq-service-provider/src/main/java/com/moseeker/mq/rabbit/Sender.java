3f4ce42c5dff94c2d2e943e9387bc32afed0c6bd
lucky8987 <lucky8987@lucky8987deMac-mini.local>
2017-07-11 15:24:28 +0800

    rabbit sandbox 配置

--- mq-service-provider/src/main/java/com/moseeker/mq/rabbit/Sender.java
+++ mq-service-provider/src/main/java/com/moseeker/mq/rabbit/Sender.java
@@ -1,7 +1,6 @@
 package com.moseeker.mq.rabbit;
 
 import java.time.LocalDateTime;
-import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,17 +16,13 @@ public class Sender {
     private RabbitTemplate amqpTemplate;
 
     public void  send(String name) {
-        String str = name.concat(",").concat(LocalDateTime.now().withNano(0).toString());
+        String str = "send: " + name.concat(",").concat(LocalDateTime.now().withNano(0).toString());
         MessageProperties msp = new MessageProperties();
-        msp.setDelay(10000);
-        amqpTemplate.send("topic-exchange", "hello.test", MessageBuilder.withBody(str.getBytes()).andProperties(msp).build());
-//        amqpTemplate.convertAndSend("topic-exchange", "hello.test", str, new MessagePostProcessor() {
-//            @Override
-//            public Message postProcessMessage(Message message) throws AmqpException {
-//                message.getMessageProperties().setDelay(15000);
-//                return message;
-//            }
-//        });
+        msp.setDelay(10000); // 延迟10s发送
         System.out.println("send success...");
+    }
+
+    public void sendAndReceive(String msg) {
+        String str = "";
     }
 }
