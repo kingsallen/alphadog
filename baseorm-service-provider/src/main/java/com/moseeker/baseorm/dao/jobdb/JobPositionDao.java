4613e3e03c894357150cde0e1ee38feaef17ff78
yuyunfeng <yuyunfeng@moseeker.com>
2017-07-11 17:03:27 +0800

    修改BUG3771错误

--- baseorm-service-provider/src/main/java/com/moseeker/baseorm/dao/jobdb/JobPositionDao.java
+++ baseorm-service-provider/src/main/java/com/moseeker/baseorm/dao/jobdb/JobPositionDao.java
@@ -536,8 +536,9 @@ public class JobPositionDao extends JooqCrudImpl<JobPositionDO, JobPositionRecor
     }
 
     public List<Integer> getPositionIds(int companyId) {
-        Result<Record1<Integer>> result = create.select(JobPosition.JOB_POSITION.ID).
-                where(JobPosition.JOB_POSITION.COMPANY_ID.eq(companyId))
+        Result<Record1<Integer>> result = create.select(JobPosition.JOB_POSITION.ID)
+                .from(JobPosition.JOB_POSITION)
+                .where(JobPosition.JOB_POSITION.COMPANY_ID.eq(companyId))
                 .fetch();
         if (result != null && result.size() > 0) {
             return result.stream().filter(record1 -> record1.value1() != null && record1.value1().intValue() > 0)
