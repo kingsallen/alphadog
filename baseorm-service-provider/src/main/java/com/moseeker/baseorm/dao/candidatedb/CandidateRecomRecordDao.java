98380259cdeaa6a2c33d4b0287878a0ec08e756a
jack frame weng <jack@wjfs-MacBook-Pro.local>
2017-07-13 17:43:41 +0800

    condition 增加条件是，需要 condition = cndition.add()

--- baseorm-service-provider/src/main/java/com/moseeker/baseorm/dao/candidatedb/CandidateRecomRecordDao.java
+++ baseorm-service-provider/src/main/java/com/moseeker/baseorm/dao/candidatedb/CandidateRecomRecordDao.java
@@ -164,8 +164,9 @@ public class CandidateRecomRecordDao extends JooqCrudImpl<CandidateRecomRecordDO
                 .and(CandidateRecomRecord.CANDIDATE_RECOM_RECORD.CLICK_TIME.greaterOrEqual(new Timestamp(dateTime.getMillis())))
                 .and(CandidateRecomRecord.CANDIDATE_RECOM_RECORD.CLICK_TIME.lessThan(new Timestamp(dateTime.plusDays(1).getMillis())))
                 .and(CandidateRecomRecord.CANDIDATE_RECOM_RECORD.POSITION_ID.in(positionIdList));
+
         if (recoms != null && recoms.size() > 0) {
-            condition.and(CandidateRecomRecord.CANDIDATE_RECOM_RECORD.IS_RECOM.in(recoms));
+            condition = condition.and(CandidateRecomRecord.CANDIDATE_RECOM_RECORD.IS_RECOM.in(recoms));
         }
 
         Result<CandidateRecomRecordRecord> result = create.selectFrom(CandidateRecomRecord.CANDIDATE_RECOM_RECORD)
@@ -197,7 +198,7 @@ public class CandidateRecomRecordDao extends JooqCrudImpl<CandidateRecomRecordDO
                 .and(CandidateRecomRecord.CANDIDATE_RECOM_RECORD.CLICK_TIME.lessThan(new Timestamp(dateTime.plusDays(1).getMillis())))
                 .and(CandidateRecomRecord.CANDIDATE_RECOM_RECORD.POSITION_ID.in(positionIdList));
         if (recoms != null && recoms.size() > 0) {
-            condition.and(CandidateRecomRecord.CANDIDATE_RECOM_RECORD.IS_RECOM.in(recoms));
+            condition = condition.and(CandidateRecomRecord.CANDIDATE_RECOM_RECORD.IS_RECOM.in(recoms));
         }
         SelectOffsetStep selectOffsetStep = create.selectFrom(CandidateRecomRecord.CANDIDATE_RECOM_RECORD)
                 .where(condition)
