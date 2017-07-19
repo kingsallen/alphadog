3383f0c8d468cc4a3040542287d4def16f8110f3
jack frame weng <jack@wjfs-MacBook-Pro.local>
2017-07-19 11:03:23 +0800

    添加user_ali_user表，重新生成jooq

---
+++ baseorm-service-provider/src/main/java/com/moseeker/baseorm/dao/userdb/UserAliUserDao.java
@@ -0,0 +1,7 @@
+package com.moseeker.baseorm.dao.userdb;
+
+/**
+ * Created by jack on 19/07/2017.
+ */
+public class UserAliUserDao {
+}
---
+++ profile-service-provider/src/main/java/com/moseeker/profile/service/impl/retriveprofile/tasks/AliUserTask.java
@@ -0,0 +1,7 @@
+package com.moseeker.profile.service.impl.retriveprofile.tasks;
+
+/**
+ * Created by jack on 19/07/2017.
+ */
+public class AliUserTask {
+}
