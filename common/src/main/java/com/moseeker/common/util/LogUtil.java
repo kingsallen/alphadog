16614c4de4989062d19d3f2037210c0db6febf1c
zhangdi <eddieeezhang@outlook.com>
2017-07-28 13:07:29 +0800

    关键字密码等日志过滤

---
+++ common/src/main/java/com/moseeker/common/util/LogUtil.java
@@ -0,0 +1,30 @@
+package com.moseeker.common.util;
+
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by zhangdi on 2017/7/28.
+ */
+public class LogUtil {
+
+    public static String filterSenstiveLog(String log, String... senstiveKeys) {
+        if (StringUtils.isNullOrEmpty(log)) {
+            return log;
+        }
+        if (senstiveKeys == null || senstiveKeys.length == 0) {
+            return log;
+        }
+
+        StringBuilder regexBuilder = new StringBuilder();
+        for (String senstiveKey : senstiveKeys) {
+            regexBuilder.append('|').append("(\"").append(senstiveKey).append("\\s*\":\\s*\"[^\"]+\",?)");
+        }
+
+        regexBuilder.delete(0, 1);
+        return log.replaceAll(regexBuilder.toString(), "");
+    }
+
+    public static String filterPwd(String log) {
+        return filterSenstiveLog(log, "password", "pwd");
+    }
+}
