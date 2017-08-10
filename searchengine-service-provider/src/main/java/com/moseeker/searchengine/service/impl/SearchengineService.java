976f930cec6150051410dff3cfdd95989049ac19
yuyunfeng <yuyunfeng@moseeker.com>
2017-08-10 13:30:46 +0800

    员工列表按照认证排序

--- searchengine-service-provider/src/main/java/com/moseeker/searchengine/service/impl/SearchengineService.java
+++ searchengine-service-provider/src/main/java/com/moseeker/searchengine/service/impl/SearchengineService.java
@@ -340,6 +340,7 @@ public class SearchengineService {
         SearchRequestBuilder searchRequestBuilder = searchClient.prepareSearch("awards").setTypes("award").setQuery(query)
                 .addSort(buildSortScript("awards." + timespan + ".award", SortOrder.DESC))
                 .addSort(buildSortScript("awards." + timespan + ".last_update_time", SortOrder.ASC))
+                .addSort(buildSortScript("activation", SortOrder.ASC))
                 .setFetchSource(new String[]{"id", "awards." + timespan + ".award", "awards." + timespan + ".last_update_time"}, null);
         if (pageNum > 0 && pageSize > 0) {
             searchRequestBuilder.setSize(pageSize).setFrom((pageNum - 1) * pageSize);
