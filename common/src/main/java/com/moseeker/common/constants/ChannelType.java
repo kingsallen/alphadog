fe2f63005ec0c57091d1582d81d5b3b9dcae3805
jack frame weng <jack@wjfs-MacBook-Pro.local>
2017-08-10 16:33:04 +0800

    修复 51简历回流和日期startDate endDate的支持

--- common/src/main/java/com/moseeker/common/constants/ChannelType.java
+++ common/src/main/java/com/moseeker/common/constants/ChannelType.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 
+ *
  * 渠道 
  * <p>Company: MoSeeker</P>  
  * <p>date: Nov 7, 2016</p>  
@@ -15,7 +15,7 @@ import java.util.Map;
  * @version
  */
 public enum ChannelType {
-	
+
 	JOB51(1, "51job","51job") {
 		@Override
 		public String getOrigin(String origin) {
@@ -76,7 +76,7 @@ public enum ChannelType {
 	}, LINKEDIN(4, "linkedin","领英") {
 		@Override
 		public String getOrigin(String origin) {
-			
+
 			return null;
 		}
 	}, ALIPAY(5, "alipay","支付宝") {
@@ -84,56 +84,56 @@ public enum ChannelType {
 		public String getOrigin(String origin) {
 			String result;
 			if(StringUtils.isNullOrEmpty(origin)) {
-				result = String.valueOf(10000000000000000l);
+				result = String.valueOf(1000000000000000000l);
 			} else {
-				if(origin.length() >= 18) {
-					if(origin.charAt(origin.length()-18) == '0') {
-						result = String.valueOf(Long.valueOf(origin)+100000000000000000l);
+				if(origin.length() >= 19) {
+					if(origin.charAt(origin.length()-19) == '0') {
+						result = String.valueOf(Long.valueOf(origin)+1000000000000000000l);
 					} else {
 						result = origin;
 					}
 				} else {
-					result = String.valueOf(Long.valueOf(origin)+100000000000000000l);
+					result = String.valueOf(Long.valueOf(origin)+1000000000000000000l);
 				}
 			}
 			return result;
 		}
 	};
-	
+
 	private ChannelType(int value, String name,String alias) {
 		this.value = value;
 		this.name = name;
 		this.alias = alias;
 	}
-	
+
 	private int value = 0;				//渠道值
 	private String name = null;			//渠道名称
 	private String alias = null;        //渠道别名
-	
+
 	public abstract String getOrigin(String origin);
-	
+
 	private static final String BINDING = "position/binding"; //帐号绑定请求名称
 	private static final String REMAIN_NUM = "position/remain"; //帐号绑定请求名称
-	
+
 	private static final Map<Integer, ChannelType> intToEnum
-    	= new HashMap<Integer, ChannelType>();
-	
+			= new HashMap<Integer, ChannelType>();
+
 	static { // Initialize map from constant name to enum constant
-	    for (ChannelType op : values())
-	    	intToEnum.put(op.getValue(), op);
+		for (ChannelType op : values())
+			intToEnum.put(op.getValue(), op);
 	}
-	
+
 	public static ChannelType instaceFromInteger(int value) {
 		return intToEnum.get(value);
 	}
-	
+
 	public int getValue() {
 		return this.value;
 	}
-	
+
 	/**
 	 * 返回该渠道的绑定请求地址
-	 * @param domain 
+	 * @param domain
 	 * @param domain chaos域名
 	 * @return
 	 */
@@ -149,7 +149,7 @@ public enum ChannelType {
 	public String getRemain(String domain) {
 		return domain+"/"+name+"/"+REMAIN_NUM;
 	}
-	
+
 	/**
 	 * 返回该渠道的可发布职位数的请求地址
 	 * @param domain chaos域名
