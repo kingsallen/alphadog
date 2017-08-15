998af8c2c938ade76fd72726e55d2eea9e5a9c2f
zztaiwll <zztaiwll@zztaiwlldeMac-mini.local>
2017-08-15 11:14:18 +0800

    pc2.0 company

--- company-service-provider/src/main/java/com/moseeker/company/service/impl/CompanyPcService.java
+++ company-service-provider/src/main/java/com/moseeker/company/service/impl/CompanyPcService.java
@@ -3,17 +3,19 @@ package com.moseeker.company.service.impl;
 import com.moseeker.baseorm.dao.hrdb.HrCompanyAccountDao;
 import com.moseeker.baseorm.dao.hrdb.HrCompanyConfDao;
 import com.moseeker.baseorm.dao.hrdb.HrCompanyDao;
+import com.moseeker.baseorm.dao.hrdb.HrTeamDao;
+import com.moseeker.baseorm.dao.jobdb.JobPositionDao;
 import com.moseeker.common.util.StringUtils;
-import com.moseeker.common.util.query.Condition;
-import com.moseeker.common.util.query.Query;
-import com.moseeker.common.util.query.ValueOp;
+import com.moseeker.common.util.query.*;
 import com.moseeker.thrift.gen.dao.struct.hrdb.HrCompanyAccountDO;
 import com.moseeker.thrift.gen.dao.struct.hrdb.HrCompanyConfDO;
 import com.moseeker.thrift.gen.dao.struct.hrdb.HrCompanyDO;
+import com.moseeker.thrift.gen.dao.struct.hrdb.HrTeamDO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -29,6 +31,10 @@ public class CompanyPcService {
     private HrCompanyConfDao hrCompanyConfDao;
     @Autowired
     private HrCompanyAccountDao hrCompanyAccountDao;
+    @Autowired
+    private JobPositionDao jobPositionDao;
+    @Autowired
+    private HrTeamDao hrTeamDao;
 
     public Map<String,Object> getCompanyInfo(int companyId){
 
@@ -67,24 +73,47 @@ public class CompanyPcService {
         return result;
     }
     /*
-        获取子公司下的team-id
+        获取子公司下的team_id
      */
     public List<Integer> getSubCompanyTeam(List<Integer> list){
-        Query query=new Query.QueryBuilder().where(new Condition("publisher",list.toArray(), ValueOp.IN)).and("status",0).buildQuery();
-        return null;
+        Query query=new Query.QueryBuilder().select(new Select("team_id", SelectOp.DISTINCT)).where(new Condition("publisher",list.toArray(), ValueOp.IN)).and("status",0).buildQuery();
+        List<Map<String,Object>> result=jobPositionDao.getMaps(query);
+        List<Integer> teamList=new ArrayList<Integer>();
+        if(StringUtils.isEmptyList(result)){
+            return new ArrayList<Integer>();
+        }
+        for(Map<String,Object> map:result){
+            int teamId= (int) map.get("team_id");
+            teamList.add(teamId);
+        }
+        return teamList;
     }
     /*
           获取母公司下team的id
      */
-    public List<Integer> getCompanyTeamId(){
-
-        return null;
+    public List<HrTeamDO> getCompanyTeamId(int companyId){
+        Query query=new Query.QueryBuilder().where("company_id",companyId).and("is_show",1).and("disable",0).orderBy("show_order").buildQuery();
+        List<HrTeamDO> list=hrTeamDao.getDatas(query);
+        return list;
     }
     /*
         获取公司下团队职位数量
      */
     public Map<Integer,Integer> getTeamPositionNum(List<Integer> list){
-        return null;
+        Query query=new Query.QueryBuilder().select(new Select("id", SelectOp.COUNT)).select("team_id").where(new Condition("team_id",list.toArray(), ValueOp.IN)).and("status",0).groupBy("team_id").buildQuery();
+        List<Map<String,Object>> result=jobPositionDao.getMaps(query);
+        if(StringUtils.isEmptyList(result)){
+            return null;
+        }
+        Map<Integer,Integer> teamPosition=new HashMap<>();
+        for(Map<String,Object> map:result){
+            int teamId= (int) map.get("team_id");
+            int num= (int) map.get("id_field");
+            if(teamPosition.get(teamId)==null){
+                teamPosition.put(teamId,num);
+            }
+        }
+        return teamPosition;
     }
 
     /*
@@ -94,4 +123,13 @@ public class CompanyPcService {
 
         return null;
     }
+    /*
+       获取子公司的发布职位position，id
+     */
+    public List<Integer> getCompanyPositions(List<Integer> publisherList){
+
+        return null;
+    }
+
+
 }
