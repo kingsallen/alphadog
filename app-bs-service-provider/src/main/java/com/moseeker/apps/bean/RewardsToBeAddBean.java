9d3903e8b2b200c562c29d50b1ec27545cffc242
lucky8987 <lucky8987@lucky8987deMac-mini.local>
2017-08-16 14:30:14 +0800

    员工积分相关

--- app-bs-service-provider/src/main/java/com/moseeker/apps/bean/RewardsToBeAddBean.java
+++ app-bs-service-provider/src/main/java/com/moseeker/apps/bean/RewardsToBeAddBean.java
@@ -11,6 +11,15 @@ public class RewardsToBeAddBean {
 	private long recommender_id;
 	private int points_conf_id;
     private int position_id;
+    private int applier_id ;
+
+    public int getApplier_id() {
+        return applier_id;
+    }
+
+    public void setApplier_id(int applier_id) {
+        this.applier_id = applier_id;
+    }
 
     public int getPosition_id() {
         return position_id;
