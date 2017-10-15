0a6e9972e65ad59dd5372a0436b395457d6235db
panyongbing <panyongbing@moseeker.com>
2017-10-15 16:05:47 +0800

    加上RabbitHandler注解

--- function-service-provider/src/main/java/com/moseeker/function/service/ScheduledTask.java
+++ function-service-provider/src/main/java/com/moseeker/function/service/ScheduledTask.java
@@ -6,8 +6,11 @@ import com.moseeker.baseorm.redis.RedisClient;
 import com.moseeker.function.config.AppConfig;
 import com.moseeker.function.constants.BindThridPart;
 import com.moseeker.function.service.chaos.ChaosServiceImpl;
+import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
@@ -24,11 +27,19 @@ public class ScheduledTask {
     private RedisClient redisClient;
 
     @RabbitListener(queues = {BindThridPart.BIND_GET_QUEUE_NAME,BindThridPart.BIND_CONFIRM_GET_QUEUE_NAME,BindThridPart.BIND_CODE_GET_QUEUE_NAME}, containerFactory = "rabbitListenerContainerFactoryAutoAck")
-    public void scheduler(String data) {
-        logger.info("获取到数据:"+data);
-        JSONObject msg= JSON.parseObject(data);
-        logger.info("推送数据到redis中");
-        redisClient.set(BindThridPart.APP_ID,BindThridPart.KEY_IDENTIFIER,msg.getString(BindThridPart.CHAOS_ACCOUNTID),data);
-        logger.info("推送数据成功");
+    @RabbitHandler
+    public void scheduler(Message message, Channel channel) {
+        try{
+            String data=new String(message.getBody(), "UTF-8");
+            logger.info("获取到数据:"+data);
+            JSONObject msg= JSON.parseObject(data);
+            logger.info("推送数据到redis中");
+            redisClient.set(BindThridPart.APP_ID,BindThridPart.KEY_IDENTIFIER,msg.getString(BindThridPart.CHAOS_ACCOUNTID),data);
+            logger.info("推送数据成功");
+        } catch (Exception e) {
+            // 错误日志记录到数据库 的 log_dead_letter 表中
+            e.printStackTrace();
+        }
+
     }
 }
