49434a11a2fb182d2866f93205d8480e7f2e8350
zztaiwll <zztaiwll@zztaiwlldeMac-mini.local>
2017-10-19 10:39:06 +0800

    alipay city

--- position-service-provider/src/main/java/com/moseeker/position/service/fundationbs/PositionThridService.java
+++ position-service-provider/src/main/java/com/moseeker/position/service/fundationbs/PositionThridService.java
@@ -237,6 +237,30 @@ public class PositionThridService {
         List<JobPositionCityDO> list=jobPositionCityDao.getDatas(query);
         return list;
     }
+    //处理直辖市的特殊情况的数据
+    public List<JobPositionCityDO> handlepecialCityCodeS(List<JobPositionCityDO> list){
+        if(StringUtils.isEmptyList(list)){
+            return null;
+        }
+        List<JobPositionCityDO> result=new ArrayList<>();
+        for(JobPositionCityDO DO:list){
+            int code=DO.getCode();
+            if(code==310000){
+                DO.setCode(310000);
+            }
+            if(code==110000){
+                DO.setCode(110100);
+            }
+            if(code==120000){
+                DO.setCode(120100);
+            }
+            if(code==500000){
+                DO.setCode(500100);
+            }
+            result.add(DO);
+        }
+        return result;
+    }
     //组装数据，仅仅获取<pid,code>的格式
     private Map<Integer,Integer> handleJobPositionCityData(List<JobPositionCityDO> list,List<Integer> pidList){
         if(StringUtils.isEmptyList(list)){
