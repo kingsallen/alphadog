3c69c765766aeb70c87581916824d8b22f88bae6
zhangdi <zhangdi@moseeker.com>
2017-10-20 11:35:55 +0800

    熟悉git命令

--- position-service-provider/src/main/java/com/moseeker/position/PositionServer.java
+++ position-service-provider/src/main/java/com/moseeker/position/PositionServer.java
@@ -13,7 +13,6 @@ public class PositionServer {
 
     public static void main(String[] args) {
 
-
         AnnotationConfigApplicationContext acac = initSpring();
         try {
         	MoServer server = new MoServer(
