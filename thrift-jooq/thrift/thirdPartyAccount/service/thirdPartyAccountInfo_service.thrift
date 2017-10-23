7509e3c8916d6267d6b53f35859ef43c44e87e2f
panyongbing <panyongbing@moseeker.com>
2017-10-23 17:49:12 +0800

    获取第三方绑定账号相关信息thrift配置

--- thrift-jooq/thrift/thirdPartyAccount/service/thirdPartyAccountInfo_service.thrift
+++ thrift-jooq/thrift/thirdPartyAccount/service/thirdPartyAccountInfo_service.thrift
@@ -2,15 +2,12 @@ namespace java com.moseeker.thrift.gen.thirdpart.service
 
 include "../struct/thirdPartyAccountInfo_struct.thrift"
 include "../struct/thirdPartyAccountInfoParam_struct.thrift"
-include "../../dao/struct/thirdpartydb/thirdparty_account_city_struct.thrift"
-include "../../dao/struct/thirdpartydb/thirdparty_account_company_address_struct.thrift"
-include "../../dao/struct/thirdpartydb/thirdparty_account_company_struct.thrift"
-include "../../dao/struct/thirdpartydb/thirdparty_account_department_struct.thrift"
+
 
 service ThirdPartyAccountInfoService{
     thirdPartyAccountInfo_struct.ThirdPartyAccountInfo  getAllInfo(1:thirdPartyAccountInfoParam_struct.ThirdPartyAccountInfoParam param);
-    list<thirdparty_account_city_struct.ThirdpartyAccountCityDO>  getCity(1:thirdPartyAccountInfoParam_struct.ThirdPartyAccountInfoParam param);
-    list<thirdparty_account_company_address_struct.ThirdpartyAccountCompanyAddressDO>  getCompanyAddress(1:thirdPartyAccountInfoParam_struct.ThirdPartyAccountInfoParam param);
-    list<thirdparty_account_company_struct.ThirdpartyAccountCompanyDO>  getCompany(1:thirdPartyAccountInfoParam_struct.ThirdPartyAccountInfoParam param);
-    list<thirdparty_account_department_struct.ThirdpartyAccountDepartmentDO>  getDepartment(1:thirdPartyAccountInfoParam_struct.ThirdPartyAccountInfoParam param);
+    list<thirdPartyAccountInfo_struct.ThirdPartyAccountInfoCity>  getCity(1:thirdPartyAccountInfoParam_struct.ThirdPartyAccountInfoParam param);
+    list<thirdPartyAccountInfo_struct.ThirdPartyAccountInfoAddress>  getCompanyAddress(1:thirdPartyAccountInfoParam_struct.ThirdPartyAccountInfoParam param);
+    list<thirdPartyAccountInfo_struct.ThirdPartyAccountInfoCompany>  getCompany(1:thirdPartyAccountInfoParam_struct.ThirdPartyAccountInfoParam param);
+    list<thirdPartyAccountInfo_struct.ThirdPartyAccountInfoDepartment>  getDepartment(1:thirdPartyAccountInfoParam_struct.ThirdPartyAccountInfoParam param);
 }
--- thrift-jooq/thrift/thirdPartyAccount/struct/thirdPartyAccountInfo_struct.thrift
+++ thrift-jooq/thrift/thirdPartyAccount/struct/thirdPartyAccountInfo_struct.thrift
@@ -1,16 +1,34 @@
 namespace java com.moseeker.thrift.gen.thirdpart.struct
 
-include "../../dao/struct/thirdpartydb/thirdparty_account_city_struct.thrift"
-include "../../dao/struct/thirdpartydb/thirdparty_account_company_address_struct.thrift"
-include "../../dao/struct/thirdpartydb/thirdparty_account_company_struct.thrift"
-include "../../dao/struct/thirdpartydb/thirdparty_account_department_struct.thrift"
-
 
 struct ThirdPartyAccountInfo{
-    1:i32 status,
-    2:string message,
-    3:optional list<thirdparty_account_city_struct.ThirdpartyAccountCityDO> city,
-    4:optional list<thirdparty_account_company_address_struct.ThirdpartyAccountCompanyAddressDO> address,
-    5:optional list<thirdparty_account_company_struct.ThirdpartyAccountCompanyDO> company,
-    6:optional list<thirdparty_account_department_struct.ThirdpartyAccountDepartmentDO> department
+    3:optional list<ThirdPartyAccountInfoCity> city,
+    4:optional list<ThirdPartyAccountInfoAddress> address,
+    5:optional list<ThirdPartyAccountInfoCompany> company,
+    6:optional list<ThirdPartyAccountInfoDepartment> department
+}
+
+
+struct ThirdPartyAccountInfoCity{
+    1: optional i32 id,
+    2: optional i32 code,
+    3: optional string name,
+    4: optional i8 jobType,	//1 社招， 2 校招
+    5: optional i32 remainNum,	//可发布数
+}
+
+struct ThirdPartyAccountInfoAddress{
+    1: optional i32 id,	//主键
+    3: optional string city,	//上班城市
+    4: optional string name,	//上班地址
+}
+
+struct ThirdPartyAccountInfoCompany{
+    1: optional i32 id,	//主键
+	3: optional string name,	//公司名称
+}
+
+struct ThirdPartyAccountInfoDepartment{
+	1: optional i32 id,	//主键
+	3: optional string name,	//部门名称
 }
