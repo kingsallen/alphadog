0e7a95335bcebd7230bb04c4bb94f2096ad439d9
panyongbing <panyongbing@moseeker.com>
2017-10-23 17:47:34 +0800

    获取第三方绑定账号相关信息服务层

--- position-service-provider/src/main/java/com/moseeker/position/thrift/ThirdPartyAccountInfoServiceImpl.java
+++ position-service-provider/src/main/java/com/moseeker/position/thrift/ThirdPartyAccountInfoServiceImpl.java
@@ -1,19 +1,26 @@
 package com.moseeker.position.thrift;
 
+import com.moseeker.common.constants.ConstantErrorCodeMessage;
 import com.moseeker.position.service.third.ThirdPartyAccountInfoService;
+import com.moseeker.thrift.gen.dao.struct.hrdb.HrThirdPartyAccountHrDO;
 import com.moseeker.thrift.gen.dao.struct.thirdpartydb.ThirdpartyAccountCityDO;
 import com.moseeker.thrift.gen.dao.struct.thirdpartydb.ThirdpartyAccountCompanyAddressDO;
 import com.moseeker.thrift.gen.dao.struct.thirdpartydb.ThirdpartyAccountCompanyDO;
 import com.moseeker.thrift.gen.dao.struct.thirdpartydb.ThirdpartyAccountDepartmentDO;
-import com.moseeker.thrift.gen.thirdpart.struct.ThirdPartyAccountInfo;
-import com.moseeker.thrift.gen.thirdpart.struct.ThirdPartyAccountInfoParam;
+import com.moseeker.thrift.gen.thirdpart.struct.*;
 import org.apache.thrift.TException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.moseeker.thrift.gen.thirdpart.service.ThirdPartyAccountInfoService.Iface;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ThirdPartyAccountInfoServiceImpl implements Iface {
+
+    Logger logger= LoggerFactory.getLogger(ThirdPartyAccountInfoServiceImpl.class);
 
     @Autowired
     ThirdPartyAccountInfoService service;
@@ -21,27 +28,68 @@ public class ThirdPartyAccountInfoServiceImpl implements Iface {
 
     @Override
     public ThirdPartyAccountInfo getAllInfo(ThirdPartyAccountInfoParam param) throws TException {
-        return service.getAllInfo(param);
+        try {
+            ThirdPartyAccountInfo info=service.getAllInfo(param);
+            return info;
+        }catch (TException e){
+            throw e;
+        }catch (Exception e){
+            logger.error(e.getMessage(),e);
+            throw new TException(ConstantErrorCodeMessage.PROGRAM_EXCEPTION,e);
+        }
+
     }
 
     @Override
-    public List<ThirdpartyAccountCityDO> getCity(ThirdPartyAccountInfoParam param) throws TException {
-        return service.getCity(service.getThirdPartyAccount(param).getThirdPartyAccountId());
+    public List<ThirdPartyAccountInfoCity> getCity(ThirdPartyAccountInfoParam param) throws TException {
+        try{
+            HrThirdPartyAccountHrDO hrAccountDoHr=service.getThirdPartyAccount(param);
+            return service.getInfoCity(hrAccountDoHr.getThirdPartyAccountId());
+        }catch (TException e){
+            throw e;
+        }catch (Exception e){
+            logger.error(e.getMessage(),e);
+            throw new TException(ConstantErrorCodeMessage.PROGRAM_EXCEPTION,e);
+        }
     }
 
     @Override
-    public List<ThirdpartyAccountCompanyAddressDO> getCompanyAddress(ThirdPartyAccountInfoParam param) throws TException {
-        return service.getCompanyAddress(service.getThirdPartyAccount(param).getThirdPartyAccountId());
+    public List<ThirdPartyAccountInfoAddress> getCompanyAddress(ThirdPartyAccountInfoParam param) throws TException {
+        try{
+            HrThirdPartyAccountHrDO hrAccountDoHr=service.getThirdPartyAccount(param);
+            return service.getInfoCompanyAddress(hrAccountDoHr.getThirdPartyAccountId());
+        }catch (TException e){
+            throw e;
+        }catch (Exception e){
+            logger.error(e.getMessage(),e);
+            throw new TException(ConstantErrorCodeMessage.PROGRAM_EXCEPTION,e);
+        }
     }
 
     @Override
-    public List<ThirdpartyAccountCompanyDO> getCompany(ThirdPartyAccountInfoParam param) throws TException {
-        return service.getCompany(service.getThirdPartyAccount(param).getThirdPartyAccountId());
+    public List<ThirdPartyAccountInfoCompany> getCompany(ThirdPartyAccountInfoParam param) throws TException {
+        try{
+            HrThirdPartyAccountHrDO hrAccountDoHr=service.getThirdPartyAccount(param);
+            return service.getInfoCompany(hrAccountDoHr.getThirdPartyAccountId());
+        }catch (TException e){
+            throw e;
+        }catch (Exception e){
+            logger.error(e.getMessage(),e);
+            throw new TException(ConstantErrorCodeMessage.PROGRAM_EXCEPTION,e);
+        }
     }
 
     @Override
-    public List<ThirdpartyAccountDepartmentDO> getDepartment(ThirdPartyAccountInfoParam param) throws TException {
-        return service.getDepartment(service.getThirdPartyAccount(param).getThirdPartyAccountId());
+    public List<ThirdPartyAccountInfoDepartment> getDepartment(ThirdPartyAccountInfoParam param) throws TException {
+        try{
+            HrThirdPartyAccountHrDO hrAccountDoHr=service.getThirdPartyAccount(param);
+            return service.getInfoDepartment(hrAccountDoHr.getThirdPartyAccountId());
+        }catch (TException e){
+            throw e;
+        }catch (Exception e){
+            logger.error(e.getMessage(),e);
+            throw new TException(ConstantErrorCodeMessage.PROGRAM_EXCEPTION,e);
+        }
     }
 
 
