f224a002ef8c431016f3639e60b3314a1a20c7ba
panyongbing <panyongbing@moseeker.com>
2017-11-16 13:49:40 +0800

    加上 同步第三方发布职位需要的信息 用到的rabbitMQ相关配置

--- position-service-provider/src/main/java/com/moseeker/position/config/AppConfig.java
+++ position-service-provider/src/main/java/com/moseeker/position/config/AppConfig.java
@@ -1,11 +1,17 @@
 package com.moseeker.position.config;
 
+import com.moseeker.baseorm.config.RabbitMQConfig;
 import com.moseeker.baseorm.redis.cache.CacheClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import com.moseeker.position.constants.VeryEastConstant;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.*;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by lucky8987 on 17/5/11.
@@ -13,12 +19,27 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Configuration
 @EnableScheduling
 @ComponentScan({"com.moseeker.position", "com.moseeker.common.aop.iface", "com.moseeker.entity"})
-@Import(com.moseeker.baseorm.config.AppConfig.class)
+@Import({com.moseeker.baseorm.config.AppConfig.class, RabbitMQConfig.class})
+@PropertySource("classpath:common.properties")
 public class AppConfig {
-
+    //设置rabbitMQ的Exchange
     @Bean
-    public CacheClient cacheClient(){
-        return null;
+    public TopicExchange exchange() {
+        TopicExchange exchange = new TopicExchange(VeryEastConstant.EXCHANGE, true, false);
+        return exchange;
     }
 
+
+    //绑定第一次推送第三方账号队列(即推送需要绑定的账号和密码)
+    @Bean
+    public Queue paramQueue() {
+        Queue queue = new Queue(VeryEastConstant.PARAM_GET_QUEUE, true, false, false);
+        return queue;
+    }
+    @Bean
+    public List<Binding> bindingParamQueue() {
+        return new ArrayList<Binding>(){{
+            add(BindingBuilder.bind(paramQueue()).to(exchange()).with(VeryEastConstant.PARAM_GET_ROUTING_KEY));
+        }};
+    }
 }
