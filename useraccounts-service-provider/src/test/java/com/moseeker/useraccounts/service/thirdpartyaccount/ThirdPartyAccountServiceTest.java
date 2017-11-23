fd998ab030599fcc4e2105999ebe162b9b4bed8e
panyongbing <panyongbing@moseeker.com>
2017-11-23 11:34:48 +0800

    测试文件

--- useraccounts-service-provider/src/test/java/com/moseeker/useraccounts/service/thirdpartyaccount/ThirdPartyAccountServiceTest.java
+++ useraccounts-service-provider/src/test/java/com/moseeker/useraccounts/service/thirdpartyaccount/ThirdPartyAccountServiceTest.java
@@ -3,18 +3,27 @@ package com.moseeker.useraccounts.service.thirdpartyaccount;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.moseeker.baseorm.dao.hrdb.HRThirdPartyAccountDao;
 import com.moseeker.common.util.AopTargetUtils;
+import com.moseeker.entity.ThridPartyAcountEntity;
+import com.moseeker.entity.pojos.Data;
+import com.moseeker.entity.pojos.ThirdPartyAccountExt;
 import com.moseeker.thrift.gen.dao.struct.hrdb.HrThirdPartyAccountDO;
 import com.moseeker.useraccounts.config.AppConfig;
+import com.moseeker.useraccounts.service.thirdpartyaccount.base.ChaosHandler;
+import com.moseeker.useraccounts.service.thirdpartyaccount.operation.BindOperation;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.HashMap;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
@@ -22,34 +31,109 @@ public class ThirdPartyAccountServiceTest {
     @Autowired
     ThirdPartyAccountService service;
 
-    HrThirdPartyAccountDO thirdPartyAccount;
+    @Autowired
+    HRThirdPartyAccountDao thirdPartyAccountDao;
+
+    @Autowired
+    ThridPartyAcountEntity thridPartyAcountEntity;
 
     @Mock
-    ThirdPartyAccountSynctor synctor;
+    ChaosHandler chaosHandler;
+
+    @Autowired
+    BindOperation bindOperation;
 
     @Before
     public void init() throws Exception {
-        synctor= Mockito.mock(ThirdPartyAccountSynctor.class);
+        MockitoAnnotations.initMocks(this);
+        chaosHandler= Mockito.mock(ChaosHandler.class);
 
-        ReflectionTestUtils.setField(AopTargetUtils.getTarget(service), "thirdPartyAccountSynctor", synctor);
+        ReflectionTestUtils.setField(AopTargetUtils.getTarget(bindOperation), "chaosHandler", chaosHandler);
 
-        Mockito.when(synctor.bindThirdPartyAccount(thirdPartyAccount,null,false)).thenReturn(thirdPartyAccount);
+        HrThirdPartyAccountDO thirdPartyAccount=new HrThirdPartyAccountDO();
+        thirdPartyAccount.setBinding((short)10);
+        Mockito.when(chaosHandler.bind(thirdPartyAccount,new HashMap<>())).thenReturn(thirdPartyAccount);
     }
 
     @Test
     public void test() throws Exception {
         String str="{" +
-                "  \"username\": \"18917124545\"," +
+                "  \"username\": \"peter62277\"," +
                 "  \"appid\": 4," +
                 "  \"user_id\": 82690," +
-                "  \"member_name\": null," +
-                "  \"channel\": 3" +
+                "  \"channel\": 6," +
+                " \"password\": \"123\""+
                 "}";
         JSONObject obj=JSON.parseObject(str);
-        thirdPartyAccount= JSON.toJavaObject(obj,HrThirdPartyAccountDO.class);
+        HrThirdPartyAccountDO thirdPartyAccount= JSON.toJavaObject(obj,HrThirdPartyAccountDO.class);
 
-        service.bindThirdAccount(obj.getIntValue("user_id"),thirdPartyAccount,false);
+        //正常流程
+        try {
+            print(service.bindThirdAccount(obj.getIntValue("user_id"),thirdPartyAccount,false));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        /*//测试已存在
+        thirdPartyAccount.setUsername("dqwl805");
+        thirdPartyAccount.setChannel((short)1);
+        try {
+            print(service.bindThirdAccount(obj.getIntValue("user_id"),thirdPartyAccount,false));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //测试同渠道
+        thirdPartyAccount.setCompanyId(4);
+        thirdPartyAccount.setUsername("18917124545");
+        thirdPartyAccount.setChannel((short)2);
+        try {
+            print(service.bindThirdAccount(82690,thirdPartyAccount,false));
+        }catch (Exception e){
+            e.printStackTrace();
+        }*/
+
+        //测试正常流程
+        /*new Thread(){
+            @Override
+            public void run() {
+                try {
+                    print(service.bindThirdAccount(obj.getIntValue("user_id"),thirdPartyAccount,false));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();*/
+
+        //测试redis
+        /*new Thread(){
+            @Override
+            public void run() {
+                try {
+                    print(service.bindThirdAccount(obj.getIntValue("user_id"),thirdPartyAccount,false));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();*/
 
 
+        /*new Thread(){
+            @Override
+            public void run() {
+                try {
+                    print(service.bindThirdAccount(obj.getIntValue("user_id"),thirdPartyAccount,false));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();*/
+
+
+
+    }
+
+    public void print(Object obj){
+        System.out.println(JSON.toJSONString(obj));
     }
 }
