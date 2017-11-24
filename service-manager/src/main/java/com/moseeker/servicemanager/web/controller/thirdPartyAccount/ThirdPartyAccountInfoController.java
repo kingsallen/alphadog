e47b4f0f0f879aa445d5fc401fe0ec596831476a
panyongbing <panyongbing@moseeker.com>
2017-11-24 16:03:30 +0800

    传回的json转换成JsonObject

--- service-manager/src/main/java/com/moseeker/servicemanager/web/controller/thirdPartyAccount/ThirdPartyAccountInfoController.java
+++ service-manager/src/main/java/com/moseeker/servicemanager/web/controller/thirdPartyAccount/ThirdPartyAccountInfoController.java
@@ -1,6 +1,7 @@
 package com.moseeker.servicemanager.web.controller.thirdPartyAccount;
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.moseeker.common.annotation.iface.CounterIface;
 import com.moseeker.rpccenter.client.ServiceManager;
 import com.moseeker.servicemanager.common.ParamUtils;
@@ -37,7 +38,7 @@ public class ThirdPartyAccountInfoController {
             ThirdPartyAccountInfoParam param=ParamUtils.initModelForm(request, ThirdPartyAccountInfoParam.class);
             ThirdPartyAccountInfo info=thirdPartyAccountInfoServices.getAllInfo(param);
             logger.info("getAllInfo data:{}",info);
-            return ResponseLogNotification.successJson(request,info.getJson());
+            return ResponseLogNotification.successJson(request, JSONObject.parseObject(info.getJson()));
         }catch (Exception e){
             logger.error(e.getMessage(), e);
             return ResponseLogNotification.failJson(request, e);
