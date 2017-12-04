cb29c6e836026642a554765c4a5556b66882223e
jack frame weng <jack@wjfs-MacBook-Pro.local>
2017-12-04 15:13:21 +0800

    生成talentpooldb

--- ralentpool-service-provider/src/main/java/com/moseeker/talentpool/domain/TalentPool.java
+++ ralentpool-service-provider/src/main/java/com/moseeker/talentpool/domain/TalentPool.java
@@ -1,15 +1,19 @@
 package com.moseeker.talentpool.domain;
 
+import com.moseeker.baseorm.db.talentpooldb.tables.pojos.TalentpoolTag;
 import com.moseeker.common.exception.CommonException;
 import com.moseeker.talentpool.domain.pojo.Conditions;
 import com.moseeker.talentpool.domain.pojo.Pagination;
 import com.moseeker.talentpool.domain.pojo.TalentVO;
+import org.springframework.stereotype.Service;
 
 /**
- * 人才库
+ * 人才库 业务聚合根
  * Created by jack on 27/11/2017.
  */
+@Service
 public class TalentPool {
+
 
     /**
      *
