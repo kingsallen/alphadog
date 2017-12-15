780ba73ccbe18a5cb814a977ac9833104e64b270
panyongbing <panyongbing@moseeker.com>
2017-12-15 16:24:26 +0800

    加上注解

--- position-service-provider/src/main/java/com/moseeker/position/service/position/job1001/Job021ParamRefresher.java
+++ position-service-provider/src/main/java/com/moseeker/position/service/position/job1001/Job021ParamRefresher.java
@@ -2,7 +2,9 @@ package com.moseeker.position.service.position.job1001;
 
 import com.alibaba.fastjson.JSONObject;
 import com.moseeker.position.service.position.job1001.refresh.handler.YLParamRefresher;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Job021ParamRefresher extends YLParamRefresher {
     @Override
     public void addUserParam(JSONObject jsonSend) {
