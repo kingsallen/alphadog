f9e2e83f35302cf85a6005fb604c5e7b55e35bfe
zztaiwll <zztaiwll@zztaiwlldeMac-mini.local>
2017-12-15 15:21:37 +0800

    es

--- common/src/main/java/com/moseeker/common/util/EsClientInstance.java
+++ common/src/main/java/com/moseeker/common/util/EsClientInstance.java
@@ -1,10 +1,7 @@
 package com.moseeker.common.util;
 
-/**
- * Created by zztaiwll on 17/12/6.
- */
 import com.moseeker.common.util.ConfigPropertiesUtil;
-import org.apache.commons.lang.*;
+import org.apache.log4j.Logger;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
@@ -14,9 +11,9 @@ import java.net.InetAddress;
  * Created by zztaiwll on 17/12/5.
  */
 public class EsClientInstance {
-
+    private static Logger logger=Logger.getLogger(EsClientInstance.class);
     private static TransportClient client=null;
-    public  static  TransportClient getClient() {
+    public  static  TransportClient getClient(){
         if(client!=null){
             return client;
         }else{
@@ -24,32 +21,53 @@ public class EsClientInstance {
                 if(client!=null){
                     return client;
                 }else {
-                    try {
-                        ConfigPropertiesUtil propertiesReader = ConfigPropertiesUtil.getInstance();
-                        propertiesReader.loadResource("es.properties");
-                        String cluster_name = propertiesReader.get("es.cluster.name", String.class);
-                        String es_connection = propertiesReader.get("es.connection", String.class);
-                        Integer es_port = propertiesReader.get("es.port", Integer.class);
-                        Settings settings = Settings.settingsBuilder().put("cluster.name", cluster_name)
-                                .put("client.transport.sniff", true)
-                                .build();
-                        String es_alternate = propertiesReader.get("es.alternate", String.class);
-                        if(org.apache.commons.lang.StringUtils.isNotBlank(es_alternate)){
-                            client = TransportClient.builder().settings(settings).build()
-                                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(es_connection), es_port))
-                                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(es_alternate), es_port));
-                        }else{
-                            client = TransportClient.builder().settings(settings).build()
-                                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(es_connection), es_port));
+                    while(true){
+                        client=createClient();
+                        logger.info("链接es客户端=========");
+                        if(client!=null){
+                            break;
                         }
-                    } catch (Exception e) {
-                        client.close();
-                        client = null;
+                        try {
+                            logger.info("链接es客户端失败，等待1.5秒重新连接=========");
+                            Thread.sleep(1500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
             }
         }
 
+        return client;
+    }
+    /*
+     创建客户端
+     */
+    private static TransportClient createClient(){
+        try {
+            ConfigPropertiesUtil propertiesReader = ConfigPropertiesUtil.getInstance();
+            propertiesReader.loadResource("es.properties");
+            String cluster_name = propertiesReader.get("es.cluster.name", String.class);
+            String es_connection = propertiesReader.get("es.connection", String.class);
+            Integer es_port = propertiesReader.get("es.port", Integer.class);
+            Settings settings = Settings.settingsBuilder().put("cluster.name", cluster_name)
+                    .put("client.transport.sniff", true)
+                    .build();
+            String es_alternate = propertiesReader.get("es.alternate", String.class);
+            if(org.apache.commons.lang.StringUtils.isNotBlank(es_alternate)){
+                client = TransportClient.builder().settings(settings).build()
+                        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(es_connection), es_port))
+                        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(es_alternate), es_port));
+            }else{
+                client = TransportClient.builder().settings(settings).build()
+                        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(es_connection), es_port));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            client.close();
+            client = null;
+        }
         return client;
     }
     public synchronized static void closeEsClient(){
