f35570df1148132cd4c7daf00a79be3badb77da4
panyongbing <panyongbing@moseeker.com>
2017-12-25 17:46:33 +0800

    DegreeSource加上一个研究生，和硕士一个code 相关的测试方法

--- profile-service-provider/src/test/java/com/moseeker/profile/refactor/ProfileServicesImplTest.java
+++ profile-service-provider/src/test/java/com/moseeker/profile/refactor/ProfileServicesImplTest.java
@@ -1,33 +1,55 @@
 package com.moseeker.profile.refactor;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.moseeker.baseorm.db.logdb.tables.records.LogResumeRecordRecord;
+import com.moseeker.baseorm.db.profiledb.tables.records.ProfileEducationRecord;
 import com.moseeker.baseorm.tool.QueryConvert;
 import com.moseeker.common.constants.ConstantErrorCodeMessage;
+import com.moseeker.common.util.DateUtils;
+import com.moseeker.common.util.StringUtils;
+import com.moseeker.entity.ProfileEntity;
+import com.moseeker.entity.biz.ProfileUtils;
+import com.moseeker.entity.pojo.profile.Education;
+import com.moseeker.entity.pojo.resume.EducationObj;
+import com.moseeker.entity.pojo.resume.ResumeObj;
 import com.moseeker.profile.config.AppConfig;
 import com.moseeker.profile.service.impl.ProfileService;
 import com.moseeker.profile.thrift.ProfileOtherThriftServiceImpl;
 import com.moseeker.profile.thrift.ProfileProjectExpServicesImpl;
 import com.moseeker.profile.thrift.ProfileServicesImpl;
+import com.moseeker.profile.utils.DegreeSource;
 import com.moseeker.thrift.gen.common.struct.BIZException;
 import com.moseeker.thrift.gen.common.struct.CommonQuery;
 import com.moseeker.thrift.gen.common.struct.Response;
 import com.moseeker.thrift.gen.profile.service.ProfileServices.Iface;
 import com.moseeker.thrift.gen.profile.struct.Profile;
 
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.Consts;
 import org.apache.thrift.TException;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.*;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
 public class ProfileServicesImplTest {
+    Logger logger= LoggerFactory.getLogger(ProfileServicesImplTest.class);
 
     @Autowired
     ProfileServicesImpl service;
@@ -35,11 +57,103 @@ public class ProfileServicesImplTest {
     @Autowired
     ProfileService profileService;
 
+    @Autowired
+    private ProfileEntity profileEntity;
+
+    @Autowired
+    ProfileUtils profileUtils;
+
     Object response;
 
     @After
     public void printResponse() {
         System.out.println(JSON.toJSONString(response));
+    }
+
+    @Test
+    public void getResumeObj() throws TException, IOException {
+        String file="C:\\Users\\xym-moseeker\\Desktop\\ResumeObj.txt";
+        try(BufferedReader br=new BufferedReader(new FileReader(file))){
+
+            StringBuilder sb=new StringBuilder();
+            String temp=null;
+            while( (temp=br.readLine()) !=null){
+                sb.append(temp);
+            }
+
+            ParserConfig.getGlobalInstance().setAsmEnable(false);
+            ResumeObj resumeObj = JSON.parseObject(sb.toString(),ResumeObj.class);
+
+            int uid=1;
+
+            String fileName="";
+
+            logger.info("profileParser resumeObj.getResult().getEducation_objs():{}", JSON.toJSONString(resumeObj.getResult().getEducation_objs()));
+            // 教育经历
+            List<Education> educationList = new ArrayList<>();
+            List<Map<String,Object>> mapList=new ArrayList<>();
+            if (resumeObj.getResult().getEducation_objs() != null && resumeObj.getResult().getEducation_objs().size() > 0) {
+                for (EducationObj educationObj : resumeObj.getResult().getEducation_objs()) {
+                    Education education = new Education();
+                    if (educationObj.getEdu_degree() != null) {
+                        if (DegreeSource.intToEnum.get(educationObj.getEdu_degree()) != null) {
+                            education.setDegree(DegreeSource.intToEnum.get(educationObj.getEdu_degree()));
+                        } else {
+                            education.setDegree(0);
+                        }
+                    }
+                    try {
+                        education.setStartDate(DateUtils.dateRepair(educationObj.getStart_date(), "\\."));
+                        if (educationObj.getEnd_date() != null && educationObj.getEnd_date().equals("至今")) {
+                            education.setEndUntilNow(1);
+                        } else {
+                            education.setEndDate(DateUtils.dateRepair(educationObj.getEnd_date(), "\\."));
+                        }
+                    } catch (ParseException e) {
+                        LogResumeRecordRecord logResumeRecordRecord = new LogResumeRecordRecord();
+                        logResumeRecordRecord.setErrorLog("日期转换异常: " + e.getMessage());
+                        logResumeRecordRecord.setFieldValue("education: {\"startDate\": " + educationObj.getStart_date() + "\", \"endDate\":" + educationObj.getEnd_date()+"}");
+                        logResumeRecordRecord.setUserId(uid);
+                        logResumeRecordRecord.setFileName(fileName);
+                        logResumeRecordRecord.setResultData(JSONObject.toJSONString(resumeObj));
+//                        resumeDao.addRecord(logResumeRecordRecord);
+                        logger.error(e.getMessage(), e);
+                    }
+                    // 学校名称
+                    education.setCollegeName(educationObj.getEdu_college());
+                    // 专业名称
+                    education.setMajorName(educationObj.getEdu_major());
+                    if (StringUtils.isNotNullOrEmpty(educationObj.getEdu_recruit())) {
+                        education.setIsUnified(educationObj.getEdu_recruit().equals("统招") ? 1 : 2);
+                    }
+                    educationList.add(education);
+                    mapList.add(JSON.parseObject(JSON.toJSONString(education)));
+                }
+
+            }
+            System.out.println(JSON.toJSONString(educationList));
+            List<ProfileEducationRecord> result=profileUtils.mapToEducationRecords(mapList);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        /*File file=new File("C:\\Users\\xym-moseeker\\Downloads\\00229e9e-dcd5-11e7-b9b2-00163e00307d.doc");
+        DataInputStream dis=new DataInputStream(new FileInputStream(file));
+        // available stream to be read
+        int length = dis.available();
+
+        // create buffer
+        byte[] buf = new byte[length];
+
+        dis.readFully(buf);
+
+        String fileStr = new String(Base64.encodeBase64(buf), Consts.UTF_8);
+        String fileName=file.getName();
+        ResumeObj resumeObj = profileEntity.profileParser(fileName, fileStr);
+        System.out.println("profileParser resumeObj:"+JSON.toJSONString(resumeObj));*/
     }
 
 
@@ -229,19 +343,19 @@ public class ProfileServicesImplTest {
         }
     }
 
-    @Test
+//    @Test
     public void getProfileOtherTest() {
         Response response = profileService.getProfileOther("[{'positionId': 134299, 'profileId': 64}]");
         System.out.println(response);
     }
 
-    @Test
+//    @Test
     public void profileCheckTest() {
         Response response = profileService.checkProfileOther(676202,11896);
         System.out.println(response);
     }
 
-    @Test
+//    @Test
     public void profileParserTest() {
         try {
             Response response = profileService.profileParser(1707240, "77559bba-ad7d-11e7-9d40-00163e000f58.pdf", "");
@@ -251,7 +365,7 @@ public class ProfileServicesImplTest {
         }
     }
 
-    @Test
+//    @Test
     public void otherFieldCheckTest() {
         Response response = profileService.otherFieldsCheck(985, "{\"salary\": 0, \"residence\": \"天水\", \"gmat\": \"\", \"PoliticalStatus\": \"共青团员\", \"majorrank\": \"5%\", \"height\": \"184\", \"idnumber\": \"62052319920207199X\", \"gre\": \"\", \"ielts\": \"\", \"location\": \"兰州\", \"secondexpectedlocation\": \"深圳\", \"marriage\": \"未婚\", \"schooljob\": [{\"schooljob_end_until_now\": 1, \"schooljob_description_hidden\": \"主要负责横向课题\", \"schooljob_end\": null, \"schooljob_start\": \"2016-11-05\", \"schooljob_job\": \"项目负责人\"}], \"workyears\": \"1\", \"toefl\": \"\"}");
         System.out.println(response);
