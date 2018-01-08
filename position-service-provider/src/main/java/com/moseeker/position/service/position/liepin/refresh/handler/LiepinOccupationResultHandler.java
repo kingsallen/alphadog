239b46c9323153a65201a320bc080f3ffb214653
panyongbing <panyongbing@moseeker.com>
2018-01-08 14:14:12 +0800

    加上51和猎聘两个渠道

---
+++ position-service-provider/src/main/java/com/moseeker/position/service/position/liepin/refresh/handler/LiepinOccupationResultHandler.java
@@ -0,0 +1,55 @@
+package com.moseeker.position.service.position.liepin.refresh.handler;
+
+import com.alibaba.fastjson.JSONObject;
+import com.moseeker.baseorm.dao.dictdb.Dict51OccupationDao;
+import com.moseeker.baseorm.dao.dictdb.DictLiepinOccupationDao;
+import com.moseeker.position.service.position.base.refresh.handler.AbstractOccupationResultHandler;
+import com.moseeker.position.service.position.job51.refresh.handler.Job51ResultHandlerAdapter;
+import com.moseeker.position.utils.PositionRefreshUtils;
+import com.moseeker.thrift.gen.dao.struct.dictdb.Dict51jobOccupationDO;
+import com.moseeker.thrift.gen.dao.struct.dictdb.DictLiepinOccupationDO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Map;
+
+public class LiepinOccupationResultHandler extends AbstractOccupationResultHandler<DictLiepinOccupationDO> implements LiepinResultHandlerAdapter{
+    Logger logger= LoggerFactory.getLogger(LiepinOccupationResultHandler.class);
+
+    @Autowired
+    DictLiepinOccupationDao occupationDao;
+
+    @Override
+    protected DictLiepinOccupationDO buildOccupation(List<String> texts, List<String> codes, Map<String, Integer> newCode, JSONObject msg) {
+        DictLiepinOccupationDO temp=new DictLiepinOccupationDO();
+
+        temp.setOtherCode(codes.get(codes.size()-1));
+        temp.setCode(newCode.get(temp.getOtherCode()));
+        temp.setLevel((short)codes.size());
+        temp.setName(PositionRefreshUtils.lastString(texts));
+        temp.setParentId(newCode.get(PositionRefreshUtils.parentCode(codes)));
+        temp.setStatus((short)1);
+
+        return temp;
+    }
+
+    @Override
+    protected void persistent(List<DictLiepinOccupationDO> data) {
+        int delCount=occupationDao.deleteAll();
+        logger.info("veryeast delete old Occupation "+delCount);
+        occupationDao.addAllData(data);
+        logger.info("veryeast insert success");
+    }
+
+    @Override
+    protected String occupationKey() {
+        return "occupation";
+    }
+
+    @Override
+    protected List<Occupation> toList(JSONObject msg) {
+        return null;
+    }
+}
---
+++ position-service-provider/src/main/java/com/moseeker/position/service/position/liepin/refresh/handler/LiepinRegionResultHandler.java
@@ -0,0 +1,6 @@
+package com.moseeker.position.service.position.liepin.refresh.handler;
+
+import com.moseeker.position.service.position.base.refresh.handler.AbstractRegionResultHandler;
+
+public class LiepinRegionResultHandler extends AbstractRegionResultHandler implements LiepinResultHandlerAdapter {
+}
