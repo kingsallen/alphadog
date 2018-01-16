d1fb45bfe4dc608a4356c100afa271fc1a6dec5b
zztaiwll <zztaiwll@zztaiwlldeMac-mini.local>
2018-01-16 15:51:28 +0800

    去掉字符串左右两边的空格

--- common/src/main/java/com/moseeker/common/util/StringUtils.java
+++ common/src/main/java/com/moseeker/common/util/StringUtils.java
@@ -320,8 +320,12 @@ public class StringUtils {
             if(value.contains("\\")){
                 value=value.toLowerCase().replaceAll("\\\\","");
             }
+            if(StringUtils.isNotNullOrEmpty(value)){
+                value=value.trim();
+            }
         }
         return value;
     }
+
 
 }
