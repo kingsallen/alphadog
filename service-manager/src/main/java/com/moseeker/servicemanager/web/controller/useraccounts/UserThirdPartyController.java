b3cd2f87726014028a527bba3518c4cdf9e8cecb
panyongbing <panyongbing@moseeker.com>
2018-01-16 16:28:44 +0800

    加个对user_thirdparty_user查询的方法

--- service-manager/src/main/java/com/moseeker/servicemanager/web/controller/useraccounts/UserThirdPartyController.java
+++ service-manager/src/main/java/com/moseeker/servicemanager/web/controller/useraccounts/UserThirdPartyController.java
@@ -54,13 +54,12 @@ public class UserThirdPartyController {
             // GET方法 通用参数解析并赋值
             CommonQuery query = ParamUtils.initCommonQuery(request, CommonQuery.class);
             logger.info(query.toString());
-//            Response result = service.getResources(query);
+            Response result = service.get(query);
 
-//            return ResponseLogNotification.success(request, result);
+            return ResponseLogNotification.success(request, result);
         } catch (Exception e) {
             logger.info(e.getMessage(),e);
             return ResponseLogNotification.fail(request, e.getMessage());
         }
-        return "";
     }
 }
--- useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/service/impl/ThirdPartyUserService.java
+++ useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/service/impl/ThirdPartyUserService.java
@@ -2,7 +2,12 @@ package com.moseeker.useraccounts.service.impl;
 
 import com.moseeker.baseorm.dao.userdb.ThirdPartyUserDao;
 import com.moseeker.common.annotation.iface.CounterIface;
+import com.moseeker.common.constants.ConstantErrorCodeMessage;
+import com.moseeker.common.providerutils.ResponseUtils;
+import com.moseeker.common.util.query.Query;
+import com.moseeker.thrift.gen.common.struct.CommonQuery;
 import com.moseeker.thrift.gen.common.struct.Response;
+import com.moseeker.thrift.gen.dao.struct.userdb.UserThirdpartyUserDO;
 import com.moseeker.thrift.gen.useraccounts.service.ThirdPartyUserService.Iface;
 import com.moseeker.thrift.gen.useraccounts.struct.ThirdPartyUser;
 import org.apache.thrift.TException;
@@ -10,6 +15,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 用户登陆， 注册，合并等api的实现
@@ -28,5 +35,17 @@ public class ThirdPartyUserService {
 
     public Response updateUser(ThirdPartyUser user) throws TException {
         return thirdPartyUserDao.putThirdPartyUser(user);
+    }
+
+    public Response get(Query query) throws TException {
+        try {
+            List<UserThirdpartyUserDO> list=thirdPartyUserDao.getDatas(query);
+            return ResponseUtils.success(list);
+        } catch (Exception e) {
+            logger.error("getResources error", e);
+            return ResponseUtils.fail(ConstantErrorCodeMessage.PROGRAM_EXCEPTION);
+        } finally {
+            //do nothing
+        }
     }
 }
