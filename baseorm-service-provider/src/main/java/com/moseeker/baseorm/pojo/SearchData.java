4dbf330bff278d297cdb0bb130da16a874610573
zztaiwll <zztaiwll@zztaiwlldeMac-mini.local>
2018-01-22 16:28:06 +0800

    position list

---
+++ baseorm-service-provider/src/main/java/com/moseeker/baseorm/pojo/SearchData.java
@@ -0,0 +1,53 @@
+package com.moseeker.baseorm.pojo;
+
+/**
+ * Created by zztaiwll on 18/1/22.
+ */
+public class SearchData {
+
+    private String city;
+    private String occupation;
+    private String team_name;
+    private String degree_name;
+    private String custom;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getTeam_name() {
+        return team_name;
+    }
+
+    public void setTeam_name(String team_name) {
+        this.team_name = team_name;
+    }
+
+    public String getDegree_name() {
+        return degree_name;
+    }
+
+    public void setDegree_name(String degree_name) {
+        this.degree_name = degree_name;
+    }
+
+    public String getCustom() {
+        return custom;
+    }
+
+    public void setCustom(String custom) {
+        this.custom = custom;
+    }
+}
