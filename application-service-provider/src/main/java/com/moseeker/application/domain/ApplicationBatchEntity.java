3f81cedb464206ae93a868c4946f4e37144a90e1
jack frame weng <jack@wjfs-MacBook-Pro.local>
2018-01-24 20:48:35 +0800

    调整代码接口

--- application-service-provider/src/main/java/com/moseeker/application/domain/ApplicationBatchEntity.java
+++ application-service-provider/src/main/java/com/moseeker/application/domain/ApplicationBatchEntity.java
@@ -1,13 +1,16 @@
 package com.moseeker.application.domain;
 
-import com.moseeker.application.domain.component.state.*;
+import com.moseeker.application.domain.component.state.ApplicationState;
+import com.moseeker.application.domain.component.state.ApplyState;
 import com.moseeker.application.domain.pojo.Application;
 import com.moseeker.application.exception.ApplicationException;
 import com.moseeker.application.infrastructure.DaoManagement;
+import com.moseeker.baseorm.db.hrdb.tables.pojos.HrOperationRecord;
 import com.moseeker.common.exception.CommonException;
-import org.springframework.context.ApplicationContext;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * 申请实体
@@ -15,27 +18,15 @@ import java.util.List;
  */
 public class ApplicationBatchEntity {
 
-    private List<Application> applicationList;          //申请编号集合
-    private DaoManagement daoManagement;                //DAO操作
-    private volatile ApplicationState applicationState;
-    private final ApplicationContext applicationContext;
+    private volatile List<Application> applicationList;     //申请编号集合
+    private DaoManagement daoManagement;                    //DAO操作
 
-    public ApplicationBatchEntity(DaoManagement daoManagement, List<Application> applicationList, ApplicationContext applicationContext) throws CommonException {
+    public ApplicationBatchEntity(DaoManagement daoManagement, List<Application> applicationList) throws CommonException {
         if (applicationList == null || applicationList.size() == 0 || daoManagement == null) {
             throw ApplicationException.APPLICATION_ENTITY_BUILD_FAILED;
         }
         this.daoManagement = daoManagement;
         this.applicationList = applicationList;
-        this.applicationContext = applicationContext;
-
-        switch (applicationList.get(0).getStatus()) {
-            case 1: applicationState = new ApplyState(this, daoManagement);break;
-            case 3: applicationState = new HiredState(this, daoManagement); break;
-            case 6: applicationState = new CVCheckedState(this, daoManagement); break;
-            case 10: applicationState = new CVPassedState(this, daoManagement); break;
-            case 12: applicationState = new OfferedStatus(this, daoManagement); break;
-            default:
-        }
     }
 
     public List<Application> getApplicationList() {
@@ -44,12 +35,42 @@ public class ApplicationBatchEntity {
 
     /**
      * 简历被查看
+     * @param hrEntity
      */
-    public void viewed() {
-        this.applicationState = applicationState.pass();
+    public List<HrOperationRecord> viewed(HREntity hrEntity) {
+
+        ApplicationState state = new ApplyState(this, daoManagement);
+        List<Application> unViewedApplicationList = applicationList
+                .stream()
+                .filter(application -> !application.isViewed())                         //过滤已经查看过的
+                .filter(application -> validateAuthority(hrEntity, application))        //过滤没有权限的申请
+                .collect(Collectors.toList());
+        applicationList = unViewedApplicationList;
+
+        //生成HR操作申请的操作记录
+        List<HrOperationRecord> operationRecordList = applicationList
+                .stream()
+                .map(application -> {
+                    HrOperationRecord hrOperationRecord = new HrOperationRecord();
+                    hrOperationRecord.setAdminId((long) hrEntity.getId());
+                    hrOperationRecord.setCompanyId((long) hrEntity.getCompanyId());
+                    hrOperationRecord.setAppId((long) application.getId());
+                    hrOperationRecord.setOperateTplId(state.getNext().getStatus().getState());
+                    return hrOperationRecord;
+                })
+                .collect(Collectors.toList());
+        state.pass();
+        return operationRecordList;
     }
 
-    public ApplicationContext getApplicationContext() {
-        return applicationContext;
+    /**
+     * 判断HR 是否有权限操作申请记录
+     * @param hrEntity HR
+     * @param application 申请记录
+     * @return true 有权限；false 没有权限
+     */
+    private boolean validateAuthority(HREntity hrEntity, Application application) {
+        Optional<Integer> optional = application.getHrId().stream().filter(id -> id.intValue() == hrEntity.getId()).findAny();
+        return optional.isPresent();
     }
 }
--- application-service-provider/src/main/java/com/moseeker/application/domain/HREntity.java
+++ application-service-provider/src/main/java/com/moseeker/application/domain/HREntity.java
@@ -1,16 +1,13 @@
 package com.moseeker.application.domain;
 
-import com.moseeker.application.domain.component.state.ApplicationStatus;
-import com.moseeker.application.domain.pojo.Application;
-import com.moseeker.application.exception.ApplicationException;
 import com.moseeker.application.infrastructure.DaoManagement;
+import com.moseeker.application.service.event.ViewApplicationListEvent;
 import com.moseeker.baseorm.config.HRAccountType;
 import com.moseeker.baseorm.db.hrdb.tables.pojos.HrOperationRecord;
 import com.moseeker.common.exception.CommonException;
 import org.springframework.context.ApplicationContext;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -41,6 +38,10 @@ public class HREntity {
         return accountType;
     }
 
+    public int getCompanyId() {
+        return companyId;
+    }
+
     /**
      * HREntity 查阅申请
      * @param applicationBatchEntity 申请实体
@@ -51,55 +52,29 @@ public class HREntity {
                 || applicationBatchEntity.getApplicationList().size() == 0) {
             return;
         }
-        //校验是否有权限浏览申请
-        if (!validateAuthority(applicationBatchEntity.getApplicationList())) {
-            throw ApplicationException.APPLICATION_HAVE_NO_PERMISSION;
-        }
         //申请被查阅
-        applicationBatchEntity.viewed();
+        List<HrOperationRecord> operationRecordList = applicationBatchEntity.viewed(this);
 
-        //添加HR对申请的操作记录
-        List<Application> unViewedApplicationList = applicationBatchEntity.getApplicationList()
-                .stream()
-                .filter(application -> !application.isViewed())
-                .collect(Collectors.toList());
+        if (operationRecordList != null && operationRecordList.size() > 0) {
 
-        if (unViewedApplicationList != null && unViewedApplicationList.size() > 0) {
-            List<HrOperationRecord> operationRecordList = unViewedApplicationList
-                    .stream()
-                    .map(application -> {
-                        HrOperationRecord hrOperationRecord = new HrOperationRecord();
-                        hrOperationRecord.setAdminId((long) this.id);
-                        hrOperationRecord.setCompanyId((long) this.companyId);
-                        hrOperationRecord.setAppId((long) application.getId());
-                        hrOperationRecord.setOperateTplId(ApplicationStatus.CVChecked.getState());
-                        return hrOperationRecord;
-                    })
-                    .collect(Collectors.toList());
+            //持久化HR操作申请的记录
             daoManagement.getHrOperationJOOQDao().insert(operationRecordList);
+
+            //发布HR查看申请事件
+            publishEvent(operationRecordList
+                    .stream()
+                    .map(record -> record.getAppId().intValue())
+                    .collect(Collectors.toList()));
         }
     }
 
     /**
-     * 检验HR是否有权限浏览这些申请
-     * @param applicationEntityList 申请集合
-     * @return true 有权限浏览；false 没有权限浏览申请
+     * 发布HR查看申请的事件
+     * @param applicationIdList 查看的申请编号
      */
-    private boolean validateAuthority(List<Application> applicationEntityList) {
-
-        Optional<Application> optional = applicationEntityList.stream().filter(applicationEntity -> {
-            if (applicationEntity.getHrId() != null && applicationEntity.getHrId().size() > 0) {
-                Optional<Integer> optionId = applicationEntity.getHrId().stream().filter(id -> id == this.getId()).findAny();
-                if (optionId.isPresent()) {
-                    return false;
-                } else {
-                    return true;
-                }
-            } else {
-                return true;
-            }
-        })
-        .findAny();
-        return !optional.isPresent();
+    private void publishEvent(List<Integer> applicationIdList) {
+        ViewApplicationListEvent viewApplicationListEvent
+                = new ViewApplicationListEvent(applicationIdList);
+        applicationContext.publishEvent(viewApplicationListEvent); //发布查看申请事件
     }
 }
