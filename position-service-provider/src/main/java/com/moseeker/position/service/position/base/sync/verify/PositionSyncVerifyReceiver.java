f4597f32b088bdd5f83571c8a519c8cf5542859b
panyongbing <panyongbing@moseeker.com>
2018-01-30 15:51:37 +0800

    注释

--- position-service-provider/src/main/java/com/moseeker/position/service/position/base/sync/verify/PositionSyncVerifyReceiver.java
+++ position-service-provider/src/main/java/com/moseeker/position/service/position/base/sync/verify/PositionSyncVerifyReceiver.java
@@ -2,6 +2,10 @@ package com.moseeker.position.service.position.base.sync.verify;
 
 import com.moseeker.thrift.gen.common.struct.BIZException;
 
+/**
+ * 接受职位同步验证消息
+ * @param <P>
+ */
 public interface PositionSyncVerifyReceiver<P> {
     void receive(P param) throws BIZException;
 }
