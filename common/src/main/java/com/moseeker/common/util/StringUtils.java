3149de14766a035a6f9e0a4dcbb1174fbd0b0018
zztaiwll <zztaiwll@zztaiwlldeMac-mini.local>
2018-02-08 17:47:22 +0800

    Stringutils filter

--- common/src/main/java/com/moseeker/common/util/StringUtils.java
+++ common/src/main/java/com/moseeker/common/util/StringUtils.java
@@ -386,93 +386,96 @@ public class StringUtils {
     public static String filterStringForSearch(String value){
         if(StringUtils.isNotNullOrEmpty(value)){
             if(value.contains("/")){
-                value=value.replaceAll("/"," ");
+                value=value.replaceAll("/","");
             }
             if(value.contains("OR")){
-                value=value.replaceAll("OR"," ");
+                value=value.replaceAll("OR","");
             }
             if(value.contains("AND")){
-                value=value.replaceAll("AND"," ");
+                value=value.replaceAll("AND","");
             }
 
             if(value.contains("(")){
-                value=value.replaceAll("\\("," ");
+                value=value.replaceAll("\\(","");
             }
             if(value.contains(")")){
-                value=value.replaceAll("\\)"," ");
+                value=value.replaceAll("\\)","");
             }
             if(value.contains("+")){
-                value=value.replaceAll("\\+"," ");
+                value=value.replaceAll("\\+","");
             }
             if(value.contains("\\")){
-                value=value.replaceAll("\\\\"," ");
+                value=value.replaceAll("\\\\","");
             }
             if(value.contains("（")){
-                value=value.replaceAll("（"," ");
+                value=value.replaceAll("（","");
             }
             if(value.contains("）")){
-                value=value.replaceAll("）"," ");
+                value=value.replaceAll("）","");
             }
             if(value.contains("-")){
-                value=value.replaceAll("-"," ");
+                value=value.replaceAll("-","");
             }
             if(value.contains("&")){
-                value=value.replaceAll("&"," ");
+                value=value.replaceAll("&","");
             }
             if(value.contains("+")){
-                value=value.replaceAll("\\+"," ");
+                value=value.replaceAll("\\+","");
             }
             if(value.contains("-")){
-                value=value.replaceAll("-"," ");
+                value=value.replaceAll("-","");
             }
 
             if(value.contains("|")){
-                value=value.replaceAll("|"," ");
+                value=value.replaceAll("|","");
             }
             if(value.contains("!")){
-                value=value.replaceAll("!"," ");
+                value=value.replaceAll("!","");
             }
             if(value.contains("{")){
-                value=value.replaceAll("\\{"," ");
+                value=value.replaceAll("\\{","");
             }
             if(value.contains("}")) {
-                value = value.replaceAll("\\}", " ");
+                value = value.replaceAll("\\}", "");
             }
             if(value.contains("^")) {
-                value = value.replaceAll("\\^", " ");
+                value = value.replaceAll("\\^", "");
             }
             if(value.contains("\"")) {
-                value = value.replaceAll("\"", " ");
+                value = value.replaceAll("\"", "");
             }
             if(value.contains("~")) {
-                value = value.replaceAll("~", " ");
+                value = value.replaceAll("~", "");
             }
             if(value.contains("*")) {
-                value = value.replaceAll("\\*", " ");
+                value = value.replaceAll("\\*", "");
             }
             if(value.contains("?")) {
-                value = value.replaceAll("\\?", " ");
+                value = value.replaceAll("\\?", "");
             }
             if(value.contains(":")) {
-                value = value.replaceAll(":", " ");
+                value = value.replaceAll(":", "");
             }
             if(value.contains("'")) {
-                value = value.replaceAll("'", " ");
+                value = value.replaceAll("'", "");
             }
             if(value.contains("@")) {
-                value = value.replaceAll("@", " ");
+                value = value.replaceAll("@", "");
             }
             if(value.contains("%")) {
-                value = value.replaceAll("%", " ");
+                value = value.replaceAll("%", "");
             }
             if(value.contains("$")) {
-                value = value.replaceAll("\\$", " ");
+                value = value.replaceAll("\\$", "");
             }
             if(value.contains("#")) {
-                value = value.replaceAll("#", " ");
+                value = value.replaceAll("#", "");
             }
             if(value.contains("=")) {
-                value = value.replaceAll("=", " ");
+                value = value.replaceAll("=", "");
+            }
+            if(value.contains(" ")) {
+                value = value.replaceAll(" ", "");
             }
             if(StringUtils.isNotNullOrEmpty(value)){
                 value=value.trim();
