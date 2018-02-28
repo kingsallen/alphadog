b30e5814dc9b676b0cfa358be2a7eb5f54e6a79d
刘旭辉 <liuxuhui@moseeker.com>
2018-02-28 12:03:19 +0800

    修改logger

--- mq-service-provider/src/main/java/com/moseeker/mq/service/impl/ResumeDeliveryService.java
+++ mq-service-provider/src/main/java/com/moseeker/mq/service/impl/ResumeDeliveryService.java
@@ -425,7 +425,6 @@ public class ResumeDeliveryService {
             String appid = handlerMiniappId();
             response = msgHttp.handleHrTemplate(accountDO, positionDO, hrChatDO, templateMessageDO, userUserDO, workExp, lastWorkName , hrWxUserDo.getOpenid(), url, appid);
         }
-        logger.info("sendMessageAndEmail sendTemplateMessageToHr:openId {}; accesstoken", hrWxUserDo.getOpenid());
         logger.info("sendMessageAndEmail sendTemplateMessageToHr:{}", response);
         return  response;
     }
