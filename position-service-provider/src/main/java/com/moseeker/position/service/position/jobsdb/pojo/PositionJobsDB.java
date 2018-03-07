05b491daf6ceb9896baa562e7e0f18857abb24c6
panyongbing <panyongbing@moseeker.com>
2018-03-07 13:28:18 +0800

    JobsDB渠道职位同步

--- position-service-provider/src/main/java/com/moseeker/position/service/position/jobsdb/pojo/PositionJobsDB.java
+++ position-service-provider/src/main/java/com/moseeker/position/service/position/jobsdb/pojo/PositionJobsDB.java
@@ -6,5 +6,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PositionJobsDB {
+    /*private String title;
 
+    private String details;
+
+    private String email;
+
+    private String summary1;
+
+    private String summary2;
+
+    private String summary3;
+
+    private List<String> functions1;
+
+    private List<String> functions2;
+
+    private List<String> functions3;
+
+    private String*/
 }
