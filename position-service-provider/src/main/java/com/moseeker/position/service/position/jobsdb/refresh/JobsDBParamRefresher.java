69450ce2c3b409a140a8d9a4ce7c51c4167486a4
panyongbing <panyongbing@moseeker.com>
2018-03-14 12:19:48 +0800

    jobsdb environ需要传一些参数才能获取到

--- position-service-provider/src/main/java/com/moseeker/position/service/position/jobsdb/refresh/JobsDBParamRefresher.java
+++ position-service-provider/src/main/java/com/moseeker/position/service/position/jobsdb/refresh/JobsDBParamRefresher.java
@@ -1,5 +1,6 @@
 package com.moseeker.position.service.position.jobsdb.refresh;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.moseeker.common.constants.ChannelType;
 import com.moseeker.position.service.position.base.refresh.AbstractRabbitMQParamRefresher;
@@ -18,9 +19,25 @@ public class JobsDBParamRefresher extends AbstractRabbitMQParamRefresher {
     @Autowired
     private List<JobsDBResultHandlerAdapter> refreshList;
 
+    //因为JobsDB environ的参数在发布职位第二页，所以需要给他传一部分值，让他填写完第一页，再跳转到第二页
+    //实话，我不知道为什么这一步也要基础服务来做，本来这个environ对接传账号密码，感觉就已经不合理了
+    private static final String POSITION_INFO =
+            "{" +
+            "  'job_title': 'kantoku'," +
+            "  'job_details': 'kantoku'," +
+            "  'email': 'daqi@moseeker.com'," +
+            "  'summary_points': [" +
+            "    'first'," +
+            "    'second'," +
+            "    'third'" +
+            "  ]" +
+            "}";
+
     @Override
     public void addSendParam(JSONObject jsonSend) {
+        JSONObject positionInfo= JSON.parseObject(POSITION_INFO);
 
+        jsonSend.put("position_info",positionInfo);
     }
 
     @Override
@@ -33,6 +50,8 @@ public class JobsDBParamRefresher extends AbstractRabbitMQParamRefresher {
     public void addUserParam(JSONObject jsonSend) {
         jsonSend.put("user_name",getConfig("jobsdb.username"));
         jsonSend.put("password",getConfig("jobsdb.password"));
+
+
     }
 
     @Override
