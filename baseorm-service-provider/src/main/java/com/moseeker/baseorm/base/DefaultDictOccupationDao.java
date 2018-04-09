ab0a76a798b99feefd0b1284abd707d999bf2b3f
panyongbing <panyongbing@moseeker.com>
2018-04-09 10:15:26 +0800

    因为jooq生成的table.field对应的大小写未知，可能全大写(STATUS)，可能小写(status) 所以需要找到字段对应的jooq字段名称

--- baseorm-service-provider/src/main/java/com/moseeker/baseorm/base/DefaultDictOccupationDao.java
+++ baseorm-service-provider/src/main/java/com/moseeker/baseorm/base/DefaultDictOccupationDao.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.moseeker.common.constants.ChannelType;
 import com.moseeker.common.util.query.Condition;
 import com.moseeker.common.util.query.ValueOp;
+import org.jooq.Field;
 import org.jooq.UpdatableRecord;
 import org.jooq.impl.TableImpl;
 
@@ -17,11 +18,11 @@ public abstract class DefaultDictOccupationDao<S, R extends UpdatableRecord<R>> 
         super(table, sClass);
     }
 
-    private static final String CODE = "CODE";
-    private static final String PARENT_ID = "PARENT_ID";
-    private static final String LEVEL = "LEVEL";
-    private static final String CODE_OTHER = "CODE_OTHER";
-    private static final String STATUS = "STATUS";
+    private static final String CODE = "code";
+    private static final String PARENT_ID = "parent_id";
+    private static final String LEVEL = "level";
+    private static final String CODE_OTHER = "code_other";
+    private static final String STATUS = "status";
 
     @Override
     protected Condition statusCondition() {
@@ -31,9 +32,9 @@ public abstract class DefaultDictOccupationDao<S, R extends UpdatableRecord<R>> 
     @Override
     protected Map<String, Object> queryEQParam(JSONObject obj) {
         Map<String, Object> paramMap = new HashMap<>();
-        paramMap.put(CODE, obj.getInteger("code"));
-        paramMap.put(PARENT_ID, obj.getInteger("parent_id"));
-        paramMap.put(LEVEL, obj.getInteger("level"));
+        paramMap.put(fieldIgnoreCase(CODE), obj.getInteger(CODE));
+        paramMap.put(fieldIgnoreCase(PARENT_ID), obj.getInteger(PARENT_ID));
+        paramMap.put(fieldIgnoreCase(LEVEL), obj.getInteger(LEVEL));
         return paramMap;
     }
 
@@ -44,18 +45,33 @@ public abstract class DefaultDictOccupationDao<S, R extends UpdatableRecord<R>> 
 
     @Override
     protected Condition conditionToSearchFather(S s) {
-        return new Condition(CODE, getParentId(s));
+        return new Condition(fieldIgnoreCase(CODE), getParentId(s));
     }
 
     @Override
     public int deleteAll() {
-        Condition condition = new Condition(CODE, 0, ValueOp.NEQ);
+        Condition condition = new Condition(fieldIgnoreCase(CODE), 0, ValueOp.GE);
         return delete(condition);
     }
 
     @Override
     protected String otherCodeName() {
-        return CODE_OTHER;
+        return fieldIgnoreCase(CODE_OTHER);
+    }
+
+    /**
+     * 因为jooq生成的table.field对应的大小写未知，可能全大写(STATUS)，可能小写(status)
+     * 所以需要找到字段对应的jooq字段名称
+     * @param fildName
+     * @return
+     */
+    private String fieldIgnoreCase(String fildName){
+        for(Field field:table.fields()){
+            if(field.getName().equalsIgnoreCase(fildName)){
+                return field.getName();
+            }
+        }
+        throw new NullPointerException();
     }
 
     private int getParentId(S s) {
