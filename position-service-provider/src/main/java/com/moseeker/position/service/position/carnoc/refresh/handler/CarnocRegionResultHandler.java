b95d1e061f095ff58db008107283593b36094c47
panyongbing <panyongbing@moseeker.com>
2018-04-09 17:08:49 +0800

    民航environ还要加上地区处理策略

---
+++ position-service-provider/src/main/java/com/moseeker/position/service/position/carnoc/refresh/handler/CarnocRegionResultHandler.java
@@ -0,0 +1,8 @@
+package com.moseeker.position.service.position.carnoc.refresh.handler;
+
+import com.moseeker.position.service.position.base.refresh.handler.AbstractRegionResultHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CarnocRegionResultHandler extends AbstractRegionResultHandler implements CarnocResultHandlerAdapter {
+}
