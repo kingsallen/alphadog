acec63a39864f27251196154add718d0f7a11105
zztaiwll <zztaiwll@zztaiwlldeMac-mini.local>
2018-04-16 18:10:42 +0800

    修改曾任职为和公司的标签查询

--- common-service/src/main/java/com/moseeker/entity/biz/CompanyFilterTagValidation.java
+++ common-service/src/main/java/com/moseeker/entity/biz/CompanyFilterTagValidation.java
@@ -412,7 +412,7 @@ public class CompanyFilterTagValidation {
             }
 
         }
-        int resultYear=(int)Math.ceil(((double)end-(double)start)/(3600*24*30*12*1000));
+        int resultYear=(int)Math.ceil(((double)end-(double)start)/((double)3600*24*30*12*1000));
 
         if(StringUtils.isNotNullOrEmpty(workyears)){
             String array[]=workyears.split(",");
@@ -436,7 +436,7 @@ public class CompanyFilterTagValidation {
                     min=10;
                     max=100;
                 }
-                if(min<resultYear&&max>resultYear){
+                if(min<=resultYear&&max>resultYear){
                     return true;
                 }
             }
