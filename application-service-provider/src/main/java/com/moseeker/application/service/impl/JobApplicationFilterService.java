3482884bfa345e11435e5fbab4db14fe9b613c06
刘旭辉 <liuxuhui@moseeker.com>
2018-04-16 11:50:18 +0800

    申请操作处理

--- application-service-provider/src/main/java/com/moseeker/application/service/impl/JobApplicationFilterService.java
+++ application-service-provider/src/main/java/com/moseeker/application/service/impl/JobApplicationFilterService.java
@@ -11,6 +11,8 @@ import com.moseeker.baseorm.dao.talentpooldb.TalentpoolProfileFilterDao;
 import com.moseeker.baseorm.dao.talentpooldb.TalentpoolProfileFilterExcuteDao;
 import com.moseeker.baseorm.db.jobdb.tables.pojos.JobPositionProfileFilter;
 import com.moseeker.baseorm.db.jobdb.tables.records.JobPositionRecord;
+import com.moseeker.baseorm.db.logdb.tables.daos.LogTalentpoolProfileFilterLogDao;
+import com.moseeker.baseorm.db.logdb.tables.pojos.LogTalentpoolProfileFilterLog;
 import com.moseeker.baseorm.db.logdb.tables.records.LogTalentpoolProfileFilterLogRecord;
 import com.moseeker.baseorm.db.talentpooldb.tables.pojos.TalentpoolExecute;
 import com.moseeker.baseorm.db.talentpooldb.tables.pojos.TalentpoolProfileFilter;
@@ -18,9 +20,12 @@ import com.moseeker.baseorm.db.talentpooldb.tables.pojos.TalentpoolProfileFilter
 import com.moseeker.common.util.StringUtils;
 import com.moseeker.entity.biz.CompanyFilterTagValidation;
 import com.moseeker.rpccenter.client.ServiceManager;
+import com.moseeker.thrift.gen.apps.profilebs.service.ProfileBS;
 import com.moseeker.thrift.gen.common.struct.Response;
+import com.moseeker.thrift.gen.company.service.TalentpoolServices;
 import com.moseeker.thrift.gen.dao.struct.hrdb.HrCompanyConfDO;
 import com.moseeker.thrift.gen.dao.struct.hrdb.HrCompanyDO;
+import com.moseeker.thrift.gen.dao.struct.jobdb.JobPositionDO;
 import com.moseeker.thrift.gen.mq.struct.MessageEmailStruct;
 import com.moseeker.thrift.gen.profile.service.WholeProfileServices;
 import java.util.ArrayList;
@@ -53,6 +58,9 @@ public class JobApplicationFilterService {
 
     @Autowired
     private CompanyFilterTagValidation tagValidation;
+    @Autowired
+    private LogTalentpoolProfileFilterLogDao logDao;
+
 
     @Autowired
     private TalentpoolProfileFilterExcuteDao talentpoolProfileFilterExcuteDao;
@@ -62,6 +70,8 @@ public class JobApplicationFilterService {
 
 
     WholeProfileServices.Iface profileService = ServiceManager.SERVICEMANAGER.getService(WholeProfileServices.Iface.class);
+    TalentpoolServices.Iface talentpoolService = ServiceManager.SERVICEMANAGER.getService(TalentpoolServices.Iface.class);
+    ProfileBS.Iface bsService = ServiceManager.SERVICEMANAGER.getService(ProfileBS.Iface.class);
 
     public void handerApplicationFilter(MessageEmailStruct filterInfoStruct) throws TException {
         JobPositionRecord positionDO = jobPositionDao.getPositionById(filterInfoStruct.getPosition_id());
@@ -86,10 +96,10 @@ public class JobApplicationFilterService {
                         boolean passApplicationExecute = false;
                         Map<String, Object> profiles = JSON.parseObject(res.getData());
                         if(talentpoolProfileFilterMap != null && talentpoolProfileFilterMap.size()>0){
-                            passTalentpoolExecute = forTalentpoolProfileFilter(filterExecute2, talentpoolProfileFilterMap, profiles, filterInfoStruct, filterPassMap, companyDO.getId(), passTalentpoolExecute, 2);
-                            passTalentpoolExecute = forTalentpoolProfileFilter(filterExecute1, talentpoolProfileFilterMap, profiles, filterInfoStruct, filterPassMap, companyDO.getId(), passTalentpoolExecute, 1);
-                            passApplicationExecute = forTalentpoolProfileFilter(filterExecute4, talentpoolProfileFilterMap, profiles, filterInfoStruct, filterPassMap, companyDO.getId(), passApplicationExecute, 4);
-                            passApplicationExecute = forTalentpoolProfileFilter(filterExecute3, talentpoolProfileFilterMap, profiles, filterInfoStruct, filterPassMap, companyDO.getId(), passApplicationExecute, 3);
+                            passTalentpoolExecute = forTalentpoolProfileFilter(filterExecute2, talentpoolProfileFilterMap, profiles, filterInfoStruct, filterPassMap, positionDO, passTalentpoolExecute,2);
+                            passTalentpoolExecute = forTalentpoolProfileFilter(filterExecute1, talentpoolProfileFilterMap, profiles, filterInfoStruct, filterPassMap, positionDO, passTalentpoolExecute,1);
+                            passApplicationExecute = forTalentpoolProfileFilter(filterExecute4, talentpoolProfileFilterMap, profiles, filterInfoStruct, filterPassMap, positionDO, passApplicationExecute,4);
+                            passApplicationExecute = forTalentpoolProfileFilter(filterExecute3, talentpoolProfileFilterMap, profiles, filterInfoStruct, filterPassMap, positionDO, passApplicationExecute,3);
                         }
                     }
                 }
@@ -97,21 +107,34 @@ public class JobApplicationFilterService {
         }
     }
 
+
     //循环验证各个筛选项
     private boolean forTalentpoolProfileFilter(List<Integer> filterIdList, Map<Integer,TalentpoolProfileFilter>  talentpoolProfileFilterMap, Map<String, Object> profiles,
-        MessageEmailStruct filterInfoStruct,  Map<Integer, Boolean> filterPassMap, int company_id, boolean bool, int type) throws TException {
+                                               MessageEmailStruct filterInfoStruct, Map<Integer, Boolean> filterPassMap, JobPositionRecord position, boolean bool, int type) throws TException {
         if(filterIdList!=null && filterIdList.size()>0 && !bool) {
             for (Integer filter_id : filterIdList) {
                 if (talentpoolProfileFilterMap.get(filter_id) != null) {
                     boolean isflag = false;
                     if(filterPassMap.get(filter_id)== null) {
-                        isflag = validateFile(talentpoolProfileFilterMap.get(filter_id), filterInfoStruct, profiles, company_id);
+                        isflag = validateFile(talentpoolProfileFilterMap.get(filter_id), filterInfoStruct, profiles, position.getCompanyId());
                     }else{
                         isflag = filterPassMap.get(filter_id);
                     }
                     filterPassMap.put(filter_id, isflag);
                     bool = isflag;
+                    LogTalentpoolProfileFilterLog logRecord = new LogTalentpoolProfileFilterLog();
+                    logRecord.setCompanyId(position.getCompanyId());
+                    logRecord.setFilterId(filter_id);
+                    logRecord.setHrId(position.getPublisher());
+                    logRecord.setUserId(filterInfoStruct.getApplier_id());
+                    int i = 0;
+                    if(bool){
+                        i = 1;
+                    }
+                    logRecord.setResult(i);
+                    logDao.insert(logRecord);
                     if (isflag){
+                        filterExecuteAction(filterInfoStruct.getApplier_id(), position, filterInfoStruct.getApplication_id(), type);
                         break;
                     }
                 }
@@ -158,5 +181,21 @@ public class JobApplicationFilterService {
             }
         }
         return  false;
+    }
+
+    private void filterExecuteAction(int user_id, JobPositionRecord position, int application_id, int type) throws TException {
+        List<Integer> userIds = new ArrayList<>();
+        userIds.add(user_id);
+        List<Integer> applicaitionIds = new ArrayList<>();
+        applicaitionIds.add(application_id);
+        if(type == 1){
+            talentpoolService.batchAddTalent(position.getPublisher(), userIds, position.getCompanyId());
+        }else if(type == 2){
+            talentpoolService.batchAddPublicTalent(position.getPublisher(), position.getCompanyId(), userIds);
+        }else if(type == 3){
+            bsService.profileProcess(position.getCompanyId(), 7, applicaitionIds, position.getPublisher());
+        }else if(type == 4){
+            bsService.profileProcess(position.getCompanyId(), 13, applicaitionIds, position.getPublisher());
+        }
     }
 }
