07aada96a70cb2b3f000dc57bd93f69bb9e5eacd
刘旭辉 <liuxuhui@moseeker.com>
2018-04-20 16:37:53 +0800

    修改删选规则来源校验

--- application-service-provider/src/main/java/com/moseeker/application/service/impl/JobApplicationFilterService.java
+++ application-service-provider/src/main/java/com/moseeker/application/service/impl/JobApplicationFilterService.java
@@ -164,6 +164,7 @@ public class JobApplicationFilterService {
         String[] array = origins.split(",");
         if(array==null||array.length==0){
             return false;
+
         }
         for(String item : array){
             if(StringUtils.isNotNullOrEmpty(item)&&item.length()<8){
