78c8d31f07f7965a30329407a1ae06ae09a6f59a
panyongbing <panyongbing@moseeker.com>
2018-04-23 10:08:59 +0800

    民航的city映射用文字

--- position-service-provider/src/main/java/com/moseeker/position/service/position/carnoc/refresh/handler/CarnocRegionResultHandler.java
+++ position-service-provider/src/main/java/com/moseeker/position/service/position/carnoc/refresh/handler/CarnocRegionResultHandler.java
@@ -1,8 +1,14 @@
 package com.moseeker.position.service.position.carnoc.refresh.handler;
 
 import com.moseeker.position.service.position.base.refresh.handler.AbstractRegionResultHandler;
+import com.moseeker.thrift.gen.dao.struct.dictdb.DictCityMapDO;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CarnocRegionResultHandler extends AbstractRegionResultHandler implements CarnocResultHandlerAdapter {
+
+    @Override
+    protected void setCodeOther(DictCityMapDO cityMap, Region thirdPartyRegion) {
+        cityMap.setCodeOther(thirdPartyRegion.textToString());
+    }
 }
