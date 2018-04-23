2eec3101c9a5b5d65f868f62a5c5cb2d74f20d8b
panyongbing <panyongbing@moseeker.com>
2018-04-23 14:40:32 +0800

    民航职能处理类继承默认职能处理类

--- dict-service-provider/src/main/java/com/moseeker/dict/service/impl/occupation/CarnocOccupationHandler.java
+++ dict-service-provider/src/main/java/com/moseeker/dict/service/impl/occupation/CarnocOccupationHandler.java
@@ -1,12 +1,11 @@
 package com.moseeker.dict.service.impl.occupation;
 
 import com.moseeker.common.constants.ChannelType;
-import com.moseeker.dict.service.base.AbstractOccupationHandler;
 import com.moseeker.thrift.gen.dao.struct.dictdb.DictCarnocOccupationDO;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CarnocOccupationHandler extends AbstractOccupationHandler<DictCarnocOccupationDO>{
+public class CarnocOccupationHandler extends DefaultOccupationHandler<DictCarnocOccupationDO>{
     @Override
     public ChannelType getChannelType() {
         return ChannelType.CARNOC;
