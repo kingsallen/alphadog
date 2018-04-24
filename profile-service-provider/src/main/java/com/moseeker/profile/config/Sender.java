080c49005d35b4a98acd9049cd208c81338df3fb
zztaiwll <zztaiwll@zztaiwlldeMac-mini.local>
2018-04-24 15:02:48 +0800

    修改配置文件

--- profile-service-provider/src/main/java/com/moseeker/profile/config/Sender.java
+++ profile-service-provider/src/main/java/com/moseeker/profile/config/Sender.java
@@ -1,11 +1,9 @@
 package com.moseeker.profile.config;
 
-import com.alibaba.fastjson.JSONObject;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.MessageBuilder;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,15 +11,15 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class Sender {
-    @Autowired
-    private AmqpTemplate amqpTemplate;
-
-    public void  send(String message) {
-        MessageProperties msp = new MessageProperties();
-        msp.setDelay(5000); // 延迟5s发送
-        amqpTemplate.send("profile_company_tag_recom_exchange", "profilecompanytagrecom.#", MessageBuilder.withBody(message.getBytes()).andProperties(msp).build());
-        System.out.println("send success...");
-    }
+//    @Autowired
+//    private AmqpTemplate amqpTemplate;
+//
+//    public void  send(String message) {
+//        MessageProperties msp = new MessageProperties();
+//        msp.setDelay(5000); // 延迟5s发送
+//        amqpTemplate.send("profile_company_tag_recom_exchange", "profilecompanytagrecom.#", MessageBuilder.withBody(message.getBytes()).andProperties(msp).build());
+//        System.out.println("send success...");
+//    }
 
 
 }
