64587bc6534c58aa3ed13027788b2f578dde74e4
刘旭辉 <liuxuhui@moseeker.com>
2018-04-25 17:42:07 +0800

    修改批量发送接口

--- mq-service-provider/src/main/java/com/moseeker/mq/service/email/MandrillMailListConsumer.java
+++ mq-service-provider/src/main/java/com/moseeker/mq/service/email/MandrillMailListConsumer.java
@@ -138,7 +138,7 @@ public class MandrillMailListConsumer {
                 message.setTrackClicks(true);
                 message.setTrackOpens(true);
                 message.setViewContentLink(true);
-
+                logger.info("sendMailList :{}",message);
                 MandrillMessageStatus[] messageStatus = mandrillApi.messages().sendTemplate(mandrillEmailListStruct.getTemplateName(),
                         null,message, false);
                 LogEmailSendrecordDO emailrecord = new LogEmailSendrecordDO();
