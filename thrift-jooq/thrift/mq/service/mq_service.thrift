44e358757135bee6292833cd7e577be9902337b8
刘旭辉 <liuxuhui@moseeker.com>
2018-04-25 17:20:21 +0800

    修改批量发送接口

--- thrift-jooq/thrift/mq/service/mq_service.thrift
+++ thrift-jooq/thrift/mq/service/mq_service.thrift
@@ -16,6 +16,8 @@ service MqService {
 
    common_struct.Response sendMandrilEmail(1: mq_struct.MandrillEmailStruct mandrillEmailStruct);
 
+   void sendMandrilEmailList(1: mq_struct.MandrillEmailListStruct mandrillEmailStruct);
+
    common_struct.Response sendSMS(1: mq_struct.SmsType smsType, 2: string mobile, 3: map<string, string> data, 4: string sys, 5: string ip);
 
    common_struct.Response sendMessageAndEmail(1: i32 application_id);
