b4db3485cb0aefba4650856062619b18c09755cd
panyongbing <panyongbing@moseeker.com>
2018-04-26 15:59:56 +0800

    继承默认职能类

--- dict-service-provider/src/main/java/com/moseeker/dict/service/impl/occupation/ZhilianOccupationHandler.java
+++ dict-service-provider/src/main/java/com/moseeker/dict/service/impl/occupation/ZhilianOccupationHandler.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
-public class ZhilianOccupationHandler extends AbstractOccupationHandler<DictZhilianOccupationDO> {
+public class ZhilianOccupationHandler extends DefaultOccupationHandler<DictZhilianOccupationDO> {
 
     @Override
     public ChannelType getChannelType() {
