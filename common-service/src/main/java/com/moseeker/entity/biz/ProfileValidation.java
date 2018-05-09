e405bc9bddeef0a2815ca15f51a237966c0c5282
刘旭辉 <liuxuhui@moseeker.com>
2018-05-09 14:10:19 +0800

    修改简历职位描述长度限制

--- common-service/src/main/java/com/moseeker/entity/biz/ProfileValidation.java
+++ common-service/src/main/java/com/moseeker/entity/biz/ProfileValidation.java
@@ -247,7 +247,7 @@ public class ProfileValidation {
 		if(StringUtils.isNullOrEmpty(workExp.getJob())) {
 			vm.addFailedElement("职位名称", "未填写职位名称");
 		}
-        if(workExp.getDescription()!=null && workExp.getDescription().length()>5000) {
+        if(workExp.getDescription()!=null && workExp.getDescription().length()>10000) {
             vm.addFailedElement("职位描述", "超过最长字数限制");
         }
 		if(StringUtils.isNullOrEmpty(workExp.getStart_date())) {
@@ -278,7 +278,7 @@ public class ProfileValidation {
 		if(StringUtils.isNullOrEmpty(workExp.getJob())) {
 			vm.addFailedElement("职位名称", "未填写职位名称");
 		}
-        if(workExp.getDescription()!=null && workExp.getDescription().length()>5000) {
+        if(workExp.getDescription()!=null && workExp.getDescription().length()>10000) {
             vm.addFailedElement("职位描述", "超过最长字数限制");
         }
 		if(workExp.getStart() == null) {
@@ -307,7 +307,7 @@ public class ProfileValidation {
 		if(StringUtils.isNullOrEmpty(workExp.getJob())) {
 			vm.addFailedElement("职位名称", "未填写职位名称");
 		}
-        if(workExp.getDescription()!=null && workExp.getDescription().length()>5000) {
+        if(workExp.getDescription()!=null && workExp.getDescription().length()>10000) {
             vm.addFailedElement("职位描述", "超过最长字数限制");
         }
 		if(workExp.getStart() == null) {
