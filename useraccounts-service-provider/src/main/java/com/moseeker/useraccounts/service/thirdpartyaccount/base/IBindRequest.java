5b0b782032f2e565bac68e39d3a73600a810b37b
panyongbing <panyongbing@moseeker.com>
2018-05-25 09:50:44 +0800

    加个注释

--- useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/service/thirdpartyaccount/base/IBindRequest.java
+++ useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/service/thirdpartyaccount/base/IBindRequest.java
@@ -7,5 +7,12 @@ import java.util.Map;
 
 public interface IBindRequest extends IChannelType{
 
+    /**
+     * 发送绑定请求，默认是ChaosHandler，即爬虫
+     * @param hrThirdPartyAccount   账号数据
+     * @param extras    额外参数
+     * @return  绑定完成后的结果
+     * @throws Exception
+     */
     HrThirdPartyAccountDO bind(HrThirdPartyAccountDO hrThirdPartyAccount, Map<String, String> extras) throws Exception;
 }
