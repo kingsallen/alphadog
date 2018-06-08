4e08e7f2411a253f1d71b1951afcafc97bf3f956
zztaiwll <zztaiwll@zztaiwlldeMac-mini.local>
2018-06-08 12:04:21 +0800

    修改企业标签

--- company-service-provider/src/main/java/com/moseeker/company/service/impl/CompanyTagService.java
+++ company-service-provider/src/main/java/com/moseeker/company/service/impl/CompanyTagService.java
@@ -66,12 +66,12 @@ public class CompanyTagService {
                     }
                     params.put("size","0");
                     int total=service.queryCompanyTagUserIdListCount(params);
-                    int totalPage=(int)Math.ceil((double)total/1000.0);
+                    int totalPage=(int)Math.ceil((double)total/100.0);
                     if(type == 1){
                         talentpoolCompanyTagUserDao.deleteByTag(tagIdList);
                     }
                     for(int i=1;i<=totalPage;i++){
-                        this.handlerUserIdList(tagIdList,type,map,i,1000);
+                        this.handlerUserIdList(tagIdList,type,map,i,100);
                     }
                 }
             }
