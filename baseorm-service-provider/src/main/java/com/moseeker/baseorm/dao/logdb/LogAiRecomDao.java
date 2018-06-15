281a2bb8562788b9cfa56fb66f607db75fee79a9
zztaiwll <zztaiwll@zztaiwlldeMac-mini.local>
2018-06-15 17:23:30 +0800

    测试ai

--- baseorm-service-provider/src/main/java/com/moseeker/baseorm/dao/logdb/LogAiRecomDao.java
+++ baseorm-service-provider/src/main/java/com/moseeker/baseorm/dao/logdb/LogAiRecomDao.java
@@ -4,8 +4,13 @@ import com.moseeker.baseorm.crud.JooqCrudImpl;
 import com.moseeker.baseorm.db.logdb.tables.pojos.LogAiRecom;
 import com.moseeker.baseorm.db.logdb.tables.records.LogAiRecomRecord;
 import static com.moseeker.baseorm.db.logdb.tables.LogAiRecom.LOG_AI_RECOM;
+import static org.jooq.impl.DSL.using;
+
 import org.jooq.impl.TableImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zztaiwll on 18/5/22.
@@ -19,5 +24,21 @@ public class LogAiRecomDao extends JooqCrudImpl<LogAiRecom, LogAiRecomRecord> {
 
     public LogAiRecomDao(TableImpl<LogAiRecomRecord> table, Class<LogAiRecom> logAiRecomClass) {
         super(table, logAiRecomClass);
+    }
+
+    public List<Integer> batchAdd(List<LogAiRecomRecord> list){
+       create.execute("set names utf8mb4");
+       create.attach(list);
+       create.batchInsert(list).execute();
+       List<Integer> result=new ArrayList<>();
+       for(LogAiRecomRecord recomRecord:list){
+           result.add(recomRecord.getId());
+       }
+       return result;
+    }
+    public int addrecord(LogAiRecomRecord recomRecord){
+        create.attach(recomRecord);
+        recomRecord.insert();
+        return recomRecord.getId();
     }
 }
