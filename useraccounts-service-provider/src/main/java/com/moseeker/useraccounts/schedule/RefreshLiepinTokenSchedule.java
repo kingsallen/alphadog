ba245e1b5bd9ee986933c7bb728d63f703fdbf68
cjmGod <oscarjm@163.com>
2018-06-23 13:29:17 +0800

    token测试通过，暂时将定时任务停止

--- useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/schedule/RefreshLiepinTokenSchedule.java
+++ useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/schedule/RefreshLiepinTokenSchedule.java
@@ -50,7 +50,8 @@ public class RefreshLiepinTokenSchedule {
     static List<String> emailList = new ArrayList<>();
 
     // {秒数} {分钟} {小时} {日期} {月份} {星期} {年份(可为空)}
-    @Scheduled(fixedDelay = 120000)
+//    @Scheduled(fixedDelay = 120000)
+    @Scheduled(cron="0 0 0 1,15 * ?")
     public void refreshLiepinToken() {
         try {
             List<HrThirdPartyAccountDO> successRequest = new ArrayList<>();
