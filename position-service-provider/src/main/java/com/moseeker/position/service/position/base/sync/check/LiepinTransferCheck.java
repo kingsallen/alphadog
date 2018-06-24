b530f6594552743e0e15b6036ee33b948d3219fb
cjmGod <oscarjm@163.com>
2018-06-24 14:27:47 +0800

    福利特色检验判断条件修改

--- position-service-provider/src/main/java/com/moseeker/position/service/position/base/sync/check/LiepinTransferCheck.java
+++ position-service-provider/src/main/java/com/moseeker/position/service/position/base/sync/check/LiepinTransferCheck.java
@@ -64,7 +64,7 @@ public class LiepinTransferCheck extends AbstractTransferCheck<ThirdPartyPositio
             // 必须设置福利特色
             if(thirdPartyPosition.getFeature() == null || thirdPartyPosition.getFeature().size() < 1){
                 errorMsg.add(FEATURE_NOT_EMPTY);
-            }else if(thirdPartyPosition.getFeature() != null || thirdPartyPosition.getFeature().size() > 1){
+            }else if(thirdPartyPosition.getFeature() != null && thirdPartyPosition.getFeature().size() > 1){
                 int index = 0;
                 for(String singleFeature : thirdPartyPosition.getFeature()){
                     if(StringUtils.isNullOrEmpty(singleFeature)){
