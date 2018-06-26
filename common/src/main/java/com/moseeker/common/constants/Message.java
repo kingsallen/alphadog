a9d9bc340c9625b94baf36fb93b9a5dbc750163a
刘旭辉 <liuxuhui@moseeker.com>
2018-06-26 18:53:45 +0800

    修改邮件内容

---
+++ common/src/main/java/com/moseeker/common/constants/Message.java
@@ -0,0 +1,31 @@
+package com.moseeker.common.constants;
+
+/**
+ * Created by moseeker on 2018/6/26.
+ */
+public class Message {
+    private String key;
+    private String value;
+
+    public Message(String key, Object value) {
+        this.key = key;
+        if(value != null)
+            this.value = (String)value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
