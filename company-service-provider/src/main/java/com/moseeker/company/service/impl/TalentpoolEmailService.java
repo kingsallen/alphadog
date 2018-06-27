3be9ce1cb76dd45c31a692a4826de5fe6bab83ce
zztaiwll <zztaiwll@zztaiwlldeMac-mini.local>
2018-06-27 17:17:50 +0800

    修改模板名称

--- company-service-provider/src/main/java/com/moseeker/company/service/impl/TalentpoolEmailService.java
+++ company-service-provider/src/main/java/com/moseeker/company/service/impl/TalentpoolEmailService.java
@@ -1425,7 +1425,7 @@ public class TalentpoolEmailService {
         result.setMergeVars(resumeInfoList);
         result.setFromName(abbr+"人才招聘团队");
         result.setFromEmail("info@moseeker.net");
-        result.setTemplateName("forwards-resume");
+        result.setTemplateName("forward-resume");
         return result;
     }
      private TalentEmailForwardsResumeInfo convertInfo1(TalentEmailForwardsResumeInfo info){
