b9472d3723fe99213a1689d0a3998f970f6bcb71
刘旭辉 <liuxuhui@moseeker.com>
2018-07-03 12:46:46 +0800

    修改模板消息文案

--- mq-service-provider/src/main/java/com/moseeker/mq/service/impl/DeliveryEmailProducer.java
+++ mq-service-provider/src/main/java/com/moseeker/mq/service/impl/DeliveryEmailProducer.java
@@ -380,9 +380,15 @@ public class DeliveryEmailProducer {
                 intentions.setJob(positionName.substring(0,positionName.length()-1));
             }
             intentions.setWorkStatus((String) intention.getOrDefault("workstate_name",""));
-            List<String> industriesList = (List<String>) intention.getOrDefault("industries", null);
+            List<Map<String, Object>> industriesList = (List<Map<String, Object>>) intention.getOrDefault("industries", null);
             if(!StringUtils.isEmptyList(industriesList)){
-                intentions.setIndustry(String.join(",", industriesList));
+                StringBuffer industrieName= new StringBuffer();
+                for(Map<String, Object> industries : industriesList){
+                    if(industries.get("industry_name")!=null){
+                        industrieName.append(industries.get("industry_name")+",");
+                    }
+                }
+                intentions.setIndustry(industrieName.substring(0,industrieName.length()-1));
             }
 
         }
