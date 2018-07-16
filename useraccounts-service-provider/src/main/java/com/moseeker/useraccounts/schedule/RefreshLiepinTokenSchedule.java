7ac317f8b19ff39d5eda33e7e8a212bfd1a294c7
cjmGod <oscarjm@163.com>
2018-07-16 18:20:06 +0800

    'bugfix'

--- useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/schedule/RefreshLiepinTokenSchedule.java
+++ useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/schedule/RefreshLiepinTokenSchedule.java
@@ -52,8 +52,7 @@ public class RefreshLiepinTokenSchedule {
      * @author  cjm
      * @date  2018/7/9
      */
-//    @Scheduled(cron="0 0 0 1,15 * ?")
-    @Scheduled(cron="0 0,5,10,15,20,25,30,35,40,45,50,55 * * * ? ")
+    @Scheduled(cron="0 0 0 1,15 * ?")
     public void refreshLiepinToken() {
         long check= redisClient.incrIfNotExist(AppId.APPID_ALPHADOG.getValue(), KeyIdentifier.LIEPIN_TOKEN_REFRESH.toString(), "");
         if (check>1) {
