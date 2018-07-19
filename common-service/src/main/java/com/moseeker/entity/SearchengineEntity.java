a82133c9d64c406e6067dc68144f6ae1a5a9255d
jack frame weng <jack@wjfs-MacBook-Pro.local>
2018-07-19 20:36:24 +0800

    修复排名规则

--- common-service/src/main/java/com/moseeker/entity/SearchengineEntity.java
+++ common-service/src/main/java/com/moseeker/entity/SearchengineEntity.java
@@ -578,26 +578,28 @@ public class SearchengineEntity {
 
     private int getSort(TransportClient client, int employeeId, int award, long lastUpdateTime, String timeSpan,
                         QueryBuilder companyIdListQueryBuild) {
-        QueryBuilder defaultQuery = QueryBuilders.matchAllQuery();
-        QueryBuilder query = QueryBuilders.boolQuery().must(defaultQuery);
+        if (award > 0) {
+            QueryBuilder defaultQuery = QueryBuilders.matchAllQuery();
+            QueryBuilder query = QueryBuilders.boolQuery().must(defaultQuery);
 
-        QueryBuilder awardQuery = QueryBuilders.rangeQuery("awards." + timeSpan + ".award")
-                .gt(award);
-        ((BoolQueryBuilder) query).must(awardQuery);
+            QueryBuilder awardQuery = QueryBuilders.rangeQuery("awards." + timeSpan + ".award")
+                    .gt(award);
+            ((BoolQueryBuilder) query).must(awardQuery);
 
-        ((BoolQueryBuilder) query).must(companyIdListQueryBuild);
+            ((BoolQueryBuilder) query).must(companyIdListQueryBuild);
 
-        QueryBuilder exceptCurrentEmployeeQuery = QueryBuilders
-                .termQuery("id", employeeId);
-        ((BoolQueryBuilder) query).mustNot(exceptCurrentEmployeeQuery);
+            QueryBuilder exceptCurrentEmployeeQuery = QueryBuilders
+                    .termQuery("id", employeeId);
+            ((BoolQueryBuilder) query).mustNot(exceptCurrentEmployeeQuery);
 
-        try {
-            SearchResponse sortResponse = client.prepareSearch("awards").setTypes("award")
-                    .setQuery(query).setSize(0).execute().get();
-            return (int)sortResponse.getHits().getTotalHits();
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-            return 0;
+            try {
+                SearchResponse sortResponse = client.prepareSearch("awards").setTypes("award")
+                        .setQuery(query).setSize(0).execute().get();
+                return (int)sortResponse.getHits().getTotalHits() + 1;
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+            }
         }
+        return 0;
     }
 }
