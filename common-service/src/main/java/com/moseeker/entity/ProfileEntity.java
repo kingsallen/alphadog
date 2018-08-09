be4cccb2c084e7a292ab977f9c7d004513bc6dce
jack frame weng <jack@wjfs-MacBook-Pro.local>
2018-08-09 17:11:28 +0800

    修复逻辑错误

--- common-service/src/main/java/com/moseeker/entity/ProfileEntity.java
+++ common-service/src/main/java/com/moseeker/entity/ProfileEntity.java
@@ -656,6 +656,7 @@ public class ProfileEntity {
 
 
             logger.info("mergeProfile userId:{}", userUserRecord.getId());
+            profilePojo.setUserRecord(userUserRecord);
             storeProfile(profilePojo);
 
             return userUserRecord;
