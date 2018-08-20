7c083036f22a10889a4d1687ef136dd83c6866c6
jack frame weng <jack@wjfs-MacBook-Pro.local>
2018-08-20 20:33:07 +0800

    修复集团公司 员工间可以相互点赞

--- useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/domain/UserEmployeeEntity.java
+++ useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/domain/UserEmployeeEntity.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.concurrent.Future;
 
 /**
@@ -53,7 +54,8 @@ public class UserEmployeeEntity {
             throw UserAccountException.AWARD_EMPLOYEE_ELEGAL;
         }
 
-        if (receiver.getCompanyId() != sender.getCompanyId() || receiver.getCompanyId() == 0) {
+        List<Integer> companyIdList = employeeEntity.getCompanyIds(sender.getCompanyId());
+        if (!companyIdList.contains(receiver.getCompanyId())) {
             throw UserAccountException.AWARD_EMPLOYEE_ELEGAL;
         }
         return new UpVoteData(sender, receiver);
