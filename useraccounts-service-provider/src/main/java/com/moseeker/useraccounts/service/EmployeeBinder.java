23a1fbc559ba75f51f48a54bdeea388ff20ea451
panyongbing <panyongbing@moseeker.com>
2018-08-29 11:19:54 +0800

    员工认证，补填信息入库问题

--- useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/service/EmployeeBinder.java
+++ useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/service/EmployeeBinder.java
@@ -193,6 +193,10 @@ public abstract class EmployeeBinder {
                     if (org.apache.commons.lang.StringUtils.isBlank(userEmployee.getCname())) {
                         userEmployee.setCname(useremployee.getCname());
                     }
+                    if (org.apache.commons.lang.StringUtils.isBlank(userEmployee.getCustomFieldValues())
+                            || "[]".equals(userEmployee.getCustomFieldValues())) {
+                        userEmployee.setCustomFieldValues(useremployee.getCustomFieldValues());
+                    }
                     userEmployee.setActivation(EmployeeActiveState.Actived.getState());
                     log.info("userEmployee update record");
                     if (useremployee.getAuthMethod() == 1 && userEmployee.getBindingTime() == null) {
