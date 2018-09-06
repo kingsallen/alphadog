957860c18b9537b847244b9c647a885fedb32bac
刘旭辉 <liuxuhui@moseeker.com>
2018-09-06 10:11:32 +0800

    TRM

--- thrift-jooq/thrift/company/struct/talentpool_struct.thrift
+++ thrift-jooq/thrift/company/struct/talentpool_struct.thrift
@@ -31,7 +31,9 @@ struct TalentpoolCompanyTagDO {
    21: optional Timestamp create_time,
    22: optional Timestamp update_time,
    23: optional i32 disable=1,
-   24: optional string origins=""
+   24: optional string origins="",
+   25: optional list<string> keywords,
+   26: optional i32 contain_any_key
 }
 
  struct ActionForm {
