3e97b5bf95bf2380acb3483015b0e0c09793d29a
panyongbing <panyongbing@moseeker.com>
2018-09-11 18:33:15 +0800

    恢复代码

--- useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/service/impl/employee/FollowStateSynchronizationTool.java
+++ useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/service/impl/employee/FollowStateSynchronizationTool.java
@@ -26,7 +26,7 @@ public class FollowStateSynchronizationTool {
     @Autowired
     private EmployeeEntity employeeEntity;
 
-//    @RabbitListener(queues = "#{followWechatQueue.name}", containerFactory = "rabbitListenerContainerFactoryAutoAck")
+    @RabbitListener(queues = "#{followWechatQueue.name}", containerFactory = "rabbitListenerContainerFactoryAutoAck")
     @RabbitHandler
     public void recoverEmployee(Message message, Channel channel) {
         String msgBody = "{}";
@@ -43,7 +43,7 @@ public class FollowStateSynchronizationTool {
         }
     }
 
-//    @RabbitListener(queues = "#{unFollowWechatQueue.name}", containerFactory = "rabbitListenerContainerFactoryAutoAck")
+    @RabbitListener(queues = "#{unFollowWechatQueue.name}", containerFactory = "rabbitListenerContainerFactoryAutoAck")
     @RabbitHandler
     public void cancelEmployee(Message message) {
         String msgBody = "{}";
