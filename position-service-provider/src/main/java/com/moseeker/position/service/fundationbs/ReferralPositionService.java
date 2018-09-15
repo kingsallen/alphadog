6611dbabc17f0dfab27b9299d754de8cf59efef9
yin.yang <yin.yang@centalent.cn>
2018-09-15 14:54:28 +0800

    cc

--- position-service-provider/src/main/java/com/moseeker/position/service/fundationbs/ReferralPositionService.java
+++ position-service-provider/src/main/java/com/moseeker/position/service/fundationbs/ReferralPositionService.java
@@ -50,7 +50,7 @@ public class ReferralPositionService {
 
     private ThreadPool tp = ThreadPool.Instance;
 
-    private static  final int RECORDNUM = 1000;
+    private static  final int RECORDNUM = 2000;
 
     @CounterIface
     @Transactional
