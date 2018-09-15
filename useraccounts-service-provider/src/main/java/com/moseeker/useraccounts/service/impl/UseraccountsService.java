ab7c7449dea937ff8894771a241b05a36ae43e22
jack frame weng <jack@wjfs-MacBook-Pro.local>
2018-09-15 14:25:27 +0800

    修复国际号码的情况

--- useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/service/impl/UseraccountsService.java
+++ useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/service/impl/UseraccountsService.java
@@ -1226,7 +1226,7 @@ public class UseraccountsService {
         }
 
         //修改手机号码
-        if (userUserDO.getUsername() == null || !FormCheck.isMobile(userUserDO.getUsername().trim())) {
+        if (userUserDO.getUsername() == null || !FormCheck.isNumber(userUserDO.getUsername().trim())) {
             ValidateUtil validateUtil = new ValidateUtil();
             validateUtil.addRequiredStringValidate("手机号码", claimForm.getMobile());
             validateUtil.addRequiredStringValidate("验证码", claimForm.getVerifyCode());
