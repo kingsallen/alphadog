aaf5edb16ec7d5704661275ce99c18414882b40d
jack frame weng <jack@wjfs-MacBook-Pro.local>
2018-09-17 23:00:31 +0800

    修改提示信息

--- useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/exception/UserAccountException.java
+++ useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/exception/UserAccountException.java
@@ -41,7 +41,7 @@ public class UserAccountException extends CommonException {
     public static final UserAccountException ERMPLOYEE_REFERRAL_TYPE_NOT_EXIST = new UserAccountException(42032, "职位信息不存在!");
     public static final UserAccountException ERMPLOYEE_REFERRAL_LOG_NOT_EXIST = new UserAccountException(42033, "推荐记录不存在!");
     public static final UserAccountException ERMPLOYEE_REFERRAL_USER_NOT_EXIST = new UserAccountException(42034, "用户信息不存在!");
-    public static final UserAccountException ERMPLOYEE_REFERRAL_USER_NOT_WRITE = new UserAccountException(42035, "用户信息错误!");
+    public static final UserAccountException ERMPLOYEE_REFERRAL_USER_NOT_WRITE = new UserAccountException(42035, "姓名和推荐候选人不一致!");
     public static final UserAccountException ERMPLOYEE_REFERRAL_ALREADY_CLAIMED = new UserAccountException(42036, "已经被认领!");
     public static final UserAccountException ERMPLOYEE_REFERRAL_EMPLOYEE_CLAIM_FAILED = new UserAccountException(42037, "推荐人无法自己认领!");
 
