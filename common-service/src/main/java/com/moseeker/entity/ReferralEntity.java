25244144d013a66a8f457c0a34c8e728e9c88e29
jack frame weng <jack@wjfs-MacBook-Pro.local>
2018-09-17 19:08:01 +0800

    修复推荐记录点击时间问题

--- common-service/src/main/java/com/moseeker/entity/ReferralEntity.java
+++ common-service/src/main/java/com/moseeker/entity/ReferralEntity.java
@@ -138,10 +138,12 @@ public class ReferralEntity {
 
     public void logReferralOperation(int position, int applicationId, int depth, List<String> referralReasons,
                                      String mobile, UserEmployeeDO employeeDO, int presenteeUserId, byte gender, String email) {
+        Timestamp now = new Timestamp(System.currentTimeMillis());
         CandidateRecomRecordRecord recomRecordRecord = new CandidateRecomRecordRecord();
         recomRecordRecord.setPositionId(position);
         recomRecordRecord.setAppId(applicationId);
-        recomRecordRecord.setRecomTime(new Timestamp(System.currentTimeMillis()));
+        recomRecordRecord.setRecomTime(now);
+        recomRecordRecord.setClickTime(now);
         recomRecordRecord.setDepth(depth);
         recomRecordRecord.setRecomReason(referralReasons.stream().collect(Collectors.joining(",")));
         recomRecordRecord.setMobile(mobile);
