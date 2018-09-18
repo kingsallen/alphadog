dfd17067004902cf58763b779a35b1a12b35471c
jack frame weng <jack@wjfs-MacBook-Pro.local>
2018-09-18 11:59:58 +0800

    修复重复认领

--- useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/exception/UserAccountException.java
+++ useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/exception/UserAccountException.java
@@ -44,6 +44,7 @@ public class UserAccountException extends CommonException {
     public static final UserAccountException ERMPLOYEE_REFERRAL_USER_NOT_WRITE = new UserAccountException(42035, "姓名和推荐候选人不一致!");
     public static final UserAccountException ERMPLOYEE_REFERRAL_ALREADY_CLAIMED = new UserAccountException(42036, "已经被认领!");
     public static final UserAccountException ERMPLOYEE_REFERRAL_EMPLOYEE_CLAIM_FAILED = new UserAccountException(42037, "推荐人无法自己认领!");
+    public static final UserAccountException ERMPLOYEE_REFERRAL_EMPLOYEE_REPEAT_CLAIM = new UserAccountException(42038, "重复认领!");
 
     public static final UserAccountException INVALID_SMS_CODE = new UserAccountException(10011, "无效验证码！");
 
