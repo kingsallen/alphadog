7ff7c90963b4ada37ec809d8fa29210e708e92b2
jack frame weng <jack@wjfs-MacBook-Pro.local>
2018-09-29 17:53:18 +0800

    添加日志

--- app-bs-service-provider/src/main/java/com/moseeker/apps/service/ProfileProcessBS.java
+++ app-bs-service-provider/src/main/java/com/moseeker/apps/service/ProfileProcessBS.java
@@ -808,6 +808,7 @@ public class ProfileProcessBS {
     private void publishStateChange(JobApplication jobApplication,
                                     Map<Integer, StateInfo> stateMap, DateTime operationTime) {
         StateInfo stateInfo = stateMap.get(jobApplication.getId());
+        logger.info("publishStateChange stateMap:{}", JSON.toJSONString(stateMap));
         logger.info("publishStateChange applicationId:{}, stateInfo:{}", jobApplication.getId(), JSON.toJSONString(stateInfo));
         byte move = 0;
         if (stateInfo.getRecruitOrder() != 13 && stateInfo.getPreRecruitOrder() < stateInfo.getRecruitOrder()) {
