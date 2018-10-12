e9ef3d69de82b5dbde7ca0d51c2d6537949a9560
jack frame weng <jack@wjfs-MacBook-Pro.local>
2018-10-12 16:29:00 +0800

    修复账号合并  被合并的unionid没有被指定为新的unionid

--- useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/service/BindOnAccountService.java
+++ useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/service/BindOnAccountService.java
@@ -188,6 +188,7 @@ public abstract class BindOnAccountService {
 	 *            信息来源
 	 */
 	protected void consummateUserAccount(UserUserRecord userMobile, UserUserRecord userUnionid) {
+		userMobile.setUnionid(userUnionid.getUnionid());
 		/* 完善用户名称 */
 		if (StringUtils.isNullOrEmpty(userMobile.getName()) && StringUtils.isNotNullOrEmpty(userUnionid.getName())) {
 			userMobile.setName(userUnionid.getName());
