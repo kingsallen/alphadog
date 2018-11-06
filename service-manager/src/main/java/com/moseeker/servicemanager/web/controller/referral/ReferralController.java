b2578c0f37cf2eaa6a5e40ac953d27328aaddbf5
cjm <oscarjm@163.com>
2018-11-06 17:56:38 +0800

    出参为下划线

--- service-manager/src/main/java/com/moseeker/servicemanager/web/controller/referral/ReferralController.java
+++ service-manager/src/main/java/com/moseeker/servicemanager/web/controller/referral/ReferralController.java
@@ -498,7 +498,7 @@ public class ReferralController {
         String validateResult = validateUtil.validate();
         if (StringUtils.isBlank(validateResult)) {
             String claimResults = userService.batchClaimReferralCard(claimForm.getUser(), claimForm.getName(), claimForm.getMobile(), claimForm.getVcode(), claimForm.getReferralRecordIds());
-            return Result.success(JSONArray.toJSON(claimResults)).toJson();
+            return Result.success(JSONArray.parseArray(claimResults)).toJson();
         } else {
             return Result.validateFailed(validateResult).toJson();
         }
