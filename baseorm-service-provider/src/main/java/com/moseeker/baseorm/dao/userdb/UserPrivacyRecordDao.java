7d2a1372af24397c66c517d16fb47fe11509f796
李成飞 <lichengfei@moseeker.com>
2018-11-08 11:08:23 +0800

    SaaS 12.5隐私协议是否阅读

---
+++ baseorm-service-provider/src/main/java/com/moseeker/baseorm/dao/userdb/UserPrivacyRecordDao.java
@@ -0,0 +1,10 @@
+package com.moseeker.baseorm.dao.userdb;
+
+import com.moseeker.baseorm.crud.JooqCrudImpl;
+import com.moseeker.baseorm.db.userdb.tables.records.UserPrivacyRecordRecord;
+
+/**
+ * Created by lee on 2018/11/7.
+ */
+public class UserPrivacyRecordDao implements JooqCrudImpl<UserPrivacyRecord, UserPrivacyRecordRecord> {
+}
