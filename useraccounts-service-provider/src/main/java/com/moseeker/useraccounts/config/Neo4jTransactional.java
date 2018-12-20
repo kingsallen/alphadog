3890d5f45d1592b846dd55c39ebc7cc9e6361a8d
刘旭辉 <liuxuhui@moseeker.com>
2018-12-20 14:45:24 +0800

    thrift

--- useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/config/Neo4jTransactional.java
+++ useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/config/Neo4jTransactional.java
@@ -1,9 +1,10 @@
-package com.moseeker.useraccounts.constant;
+package com.moseeker.useraccounts.config;
 
 import java.lang.annotation.*;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
+ * 用于neo4j的事务管理
  * Created by moseeker on 2018/12/20.
  */
 @Target({ElementType.TYPE, ElementType.METHOD})
