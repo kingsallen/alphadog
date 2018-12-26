3188c3de7b895dfdf09b59ef274866ba0818153b
panyongbing <panyongbing@moseeker.com>
2018-12-26 16:37:42 +0800

    捕捉异常

--- useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/thrift/UserProviderThriftServiceImpl.java
+++ useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/thrift/UserProviderThriftServiceImpl.java
@@ -1,6 +1,6 @@
 package com.moseeker.useraccounts.thrift;
 
-import com.moseeker.thrift.gen.common.struct.BIZException;
+import com.moseeker.common.providerutils.ExceptionUtils;
 import com.moseeker.thrift.gen.dao.struct.userdb.UserUserDO;
 import com.moseeker.thrift.gen.useraccounts.service.UserProviderService;
 import com.moseeker.useraccounts.service.impl.UserProviderServiceImpl;
@@ -9,18 +9,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserProviderThriftServiceImpl implements UserProviderService.Iface{
+public class UserProviderThriftServiceImpl implements UserProviderService.Iface {
 
     @Autowired
     private UserProviderServiceImpl userProviderService;
 
     @Override
     public UserUserDO getCompanyUser(int appid, String phone, int companyId) throws TException {
-        return userProviderService.getCompanyUser(appid,phone,companyId);
+        try {
+            return userProviderService.getCompanyUser(appid, phone, companyId);
+        } catch (Exception e) {
+            throw ExceptionUtils.convertException(e);
+        }
     }
 
     @Override
     public UserUserDO storeChatBotUser(String profilePojo, int reference, int companyId, int source, int appid) throws TException {
-        return userProviderService.storeChatBotUser(profilePojo,reference,companyId, source, appid);
+        try {
+            return userProviderService.storeChatBotUser(profilePojo, reference, companyId, source, appid);
+        } catch (Exception e) {
+            throw ExceptionUtils.convertException(e);
+        }
     }
 }
