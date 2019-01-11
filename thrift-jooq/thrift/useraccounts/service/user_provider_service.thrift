e42e19376ce9498ffc1b8cec2bd5dca58a740b98
panyongbing <panyongbing@moseeker.com>
2019-01-11 10:29:32 +0800

    去掉storeChatBotUser接口

--- thrift-jooq/thrift/useraccounts/service/user_provider_service.thrift
+++ thrift-jooq/thrift/useraccounts/service/user_provider_service.thrift
@@ -11,5 +11,4 @@ service UserProviderService {
 
     // 获取新版本内容
     user_user_struct.UserUserDO getCompanyUser(1: i32 appid,2: string phone,3: i32 companyId) throws (1: common_struct.BIZException e);
-    user_user_struct.UserUserDO storeChatBotUser(1: string profilePojo,2: i32 reference,3: i32 companyId,4: i32 source,5: i32 appid) throws (1: common_struct.BIZException e);
 }
