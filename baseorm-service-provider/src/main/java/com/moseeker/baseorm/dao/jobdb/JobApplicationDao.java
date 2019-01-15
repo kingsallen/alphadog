f35412759669a4fbc93937f26c28e8bfa64c18af
yin.yang <yin.yang@centalent.cn>
2019-01-15 20:33:09 +0800

    打log

--- baseorm-service-provider/src/main/java/com/moseeker/baseorm/dao/jobdb/JobApplicationDao.java
+++ baseorm-service-provider/src/main/java/com/moseeker/baseorm/dao/jobdb/JobApplicationDao.java
@@ -151,8 +151,7 @@ public class JobApplicationDao extends JooqCrudImpl<JobApplicationDO, JobApplica
 		return list;
 	}
 
-    /**
-     * insertIfNotExist 判断是否已存在
+    /** insertIfNotExist 判断是否已存在
      * @param record
      * @return
      */
@@ -161,6 +160,7 @@ public class JobApplicationDao extends JooqCrudImpl<JobApplicationDO, JobApplica
 		// 为防止高并发下的重复申请，先在redis中检验是否申请过，如果没有再去执行sql语句
 		long redisFlag = checkRedisApplication(record);
 		int result = 0;
+		logger.info("addIfNotExisits redisFlag {}", redisFlag);
 		if(redisFlag == 1){
 			List<Field<?>> changedFieldList = Arrays.stream(record.fields()).filter(f -> record.changed(f)).collect(Collectors.toList());
 			String insertSql = " insert into jobdb.job_application ".concat(changedFieldList.stream().map(m -> m.getName()).collect(Collectors.joining(",", " (", ") ")))
@@ -171,7 +171,7 @@ public class JobApplicationDao extends JooqCrudImpl<JobApplicationDO, JobApplica
 					.concat(JobApplication.JOB_APPLICATION.APPLIER_ID.getName()).concat(" = ").concat(record.getApplierId().toString()).concat(" and ")
 					.concat(JobApplication.JOB_APPLICATION.POSITION_ID.getName()).concat(" = ").concat(record.getPositionId().toString())
 					.concat(" ) ");
-			//logger.info("addIfNotExisits job_application sql: {}", insertSql);
+			logger.info("addIfNotExisits job_application sql: {}", insertSql);
 			result = create.execute(insertSql, changedFieldList.stream().map(m -> record.getValue(m)).collect(Collectors.toList()).toArray());
 		}
 
