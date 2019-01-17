8c4af3d1776b56e9fe3ad80ac8b1619fef14886d
cjmgod <cuijiaming@git.moseeker.com>
2019-01-17 16:44:08 +0800

    加日志

--- useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/kafka/KafkaSender.java
+++ useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/kafka/KafkaSender.java
@@ -1,5 +1,7 @@
 package com.moseeker.useraccounts.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -7,10 +9,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class KafkaSender {
 
+    Logger logger = LoggerFactory.getLogger(KafkaSender.class);
+
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String topic, String data){
+        logger.info("topic:{}, data:{}", topic, data);
         kafkaTemplate.send(topic, data);
     }
 
