0ef60290307de93ca93fbac813df248f5b01537c
刘旭辉 <liuxuhui@moseeker.com>
2019-02-15 18:01:56 +0800

    修改hr操作记录

--- position-service-provider/src/main/java/com/moseeker/position/service/fundationbs/ReferralPositionService.java
+++ position-service-provider/src/main/java/com/moseeker/position/service/fundationbs/ReferralPositionService.java
@@ -452,11 +452,11 @@ public class ReferralPositionService {
             if (StringUtils.isNotNullOrEmpty(result)) {
                 Map<String, Object> map = JSON.parseObject(result, Map.class);
                 if (map.get("pids") != null) {
-                    String pids = (String) map.get("pids");
-                    List<String> pidList = StringUtils.stringToList(pids, ",");
+                    JSONArray pids = (JSONArray) map.get("pids");
+                    List<Integer> pidList = pids.toJavaList(Integer.class);
                     logger.info("fetchPositionMatchByUserId pidList:{}",JSONObject.toJSONString(pidList));
                     if (!StringUtils.isEmptyList(pidList)) {
-                        List<Integer> idList = pidList.subList(0, 3).stream().map(m ->Integer.valueOf(m)).collect(Collectors.toList());
+                        List<Integer> idList = pidList.subList(0, 3);
                         logger.info("fetchPositionMatchByUserId idList:{}",JSONObject.toJSONString(pidList));
                         List<JobPosition> positionList = positionEntity.getPositionInfoByIdList(idList);
                         List<JobPositionCityDO> positionCityList = positionCityDao.getPositionCityBypids(idList);
