a81e4dfa426867e3dec88639bbc4112918d74a43
cjmgod <cuijiaming@git.moseeker.com>
2019-02-19 14:27:18 +0800

    merge

--- common-service/src/main/java/com/moseeker/entity/ReferralEntity.java
+++ common-service/src/main/java/com/moseeker/entity/ReferralEntity.java
@@ -250,7 +250,7 @@ public class ReferralEntity {
     }
 
     public void logReferralOperation(int positionId, int applicationId,  List<String> referralReasons,String mobile,
-                                     UserEmployeeDO employeeDO, int presenteeUserId, byte shipType, String referralText) {
+                                     int postUserId, int presenteeUserId, byte shipType, String referralText) {
         ReferralRecomEvaluationRecord evaluationRecord = new ReferralRecomEvaluationRecord();
         evaluationRecord.setAppId(applicationId);
         evaluationRecord.setRecomReasonTag(referralReasons.stream().collect(Collectors.joining(",")));
@@ -258,7 +258,7 @@ public class ReferralEntity {
         evaluationRecord.setRecomReasonText(referralText);
         evaluationRecord.setMobile(mobile);
         evaluationRecord.setPresenteeUserId(presenteeUserId);
-        evaluationRecord.setPostUserId(employeeDO.getSysuserId());
+        evaluationRecord.setPostUserId(postUserId);
         evaluationRecord.setPositionId(positionId);
         recomEvaluationDao.insertIfNotExist(evaluationRecord);
     }
