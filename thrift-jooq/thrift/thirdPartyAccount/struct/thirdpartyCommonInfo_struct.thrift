50fb9c054a96aaa8883dd4328fe0869b4d78022a
panyongbing <panyongbing@moseeker.com>
2019-03-06 14:18:51 +0800

    龙湖插入需求

---
+++ thrift-jooq/thrift/thirdPartyAccount/struct/thirdpartyCommonInfo_struct.thrift
@@ -0,0 +1,14 @@
+namespace java com.moseeker.thrift.gen.thirdpart.struct
+
+
+struct ThirdPartyCommonInfo {
+    1:i32 id
+    2:i32 company_id
+    3:i32 user_id
+    4:string content
+    5:string type
+    6:string user_type
+    7:i32 status
+    8:string create_time
+    9:string update_time
+}
