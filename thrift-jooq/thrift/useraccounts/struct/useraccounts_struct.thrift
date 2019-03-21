b6c457f683a182e38822f630f8a5586b05f458d1
zztaiwll <zztaiwll@zztaiwlldeMac-mini-2.local>
2019-03-21 15:20:29 +0800

    ss

--- thrift-jooq/thrift/useraccounts/struct/useraccounts_struct.thrift
+++ thrift-jooq/thrift/useraccounts/struct/useraccounts_struct.thrift
@@ -165,7 +165,9 @@ struct SearchCondition {
    30: i32 position_status,//职位状态
    31: string position_key_word,//职位关键字
    32: string past_position_key_word,//曾任职位的关键字
-   33: string past_company_key_word// 曾任公司的关键字
+   33: string past_company_key_word,// 曾任公司的关键字
+   34: String start_submit_time,//开始时间
+   35: string end_submit_time//结束时间
 
 }
 
