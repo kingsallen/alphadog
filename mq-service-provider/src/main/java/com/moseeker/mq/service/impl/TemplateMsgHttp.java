d918b1cf4b3cc8a230377dc2d8f2583423b5800d
Feng.Shi <feng.shi@gongbangbang.com>
2019-04-10 16:52:18 +0800

    //修改消息模板

--- mq-service-provider/src/main/java/com/moseeker/mq/service/impl/TemplateMsgHttp.java
+++ mq-service-provider/src/main/java/com/moseeker/mq/service/impl/TemplateMsgHttp.java
@@ -538,7 +538,7 @@ public class TemplateMsgHttp {
 
         MessageTplDataCol keywords3 = new MessageTplDataCol();
         keywords3.setColor("#171717");
-        keywords3.setValue("红包充值  成功~！！！");
+        keywords3.setValue("红包充值");
         colMap.put("keyword3", keywords3);
 
        MessageTplDataCol keywords4 = new MessageTplDataCol();
@@ -547,10 +547,10 @@ public class TemplateMsgHttp {
         colMap.put("keyword4", keywords4);
 
 
-        MessageTplDataCol remarkJson = new MessageTplDataCol();
+        /*MessageTplDataCol remarkJson = new MessageTplDataCol();
         remarkJson.setColor("#171717");
        remarkJson.setValue("求推荐时间 ---》》："+time);
-      colMap.put("remark", remarkJson);
+      colMap.put("remark", remarkJson);*/
 
         Map<String, Object> applierTemplate = new HashMap<>();
         applierTemplate.put("data", colMap);
