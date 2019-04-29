c87e6c5cdb9c38b8068641aaa9f91d1c7d10950d
李成飞 <lichengfei@moseeker.com>
2019-04-29 18:02:08 +0800

    猎头简历上传优化

--- profile-service-provider/src/main/java/com/moseeker/profile/service/impl/resumefileupload/UserProfileParser.java
+++ profile-service-provider/src/main/java/com/moseeker/profile/service/impl/resumefileupload/UserProfileParser.java
@@ -72,12 +72,12 @@ public class UserProfileParser extends AbstractResumeFileParser {
             int status = OfficeUtils.Word2Pdf(fileNameData.getFileAbsoluteName(),
                     fileNameData.getFileAbsoluteName().replace(fileNameData.getFileName(), pdfName));
             logger.info("........." + pdfName + " --------------pdf file ---> status = " + status);
-            if(status == 1) {
+//            if(status == 1) {
                 fileNameData.setFileAbsoluteName(fileNameData.getFileAbsoluteName().replace(fileNameData.getFileName(), pdfName));
                 fileNameData.setFileName(pdfName);
                 fileNameData.setOriginName(fileNameData.getOriginName().replace(".docx", Constant.WORD_PDF)
                         .replace(".doc", Constant.WORD_PDF));
-            }
+//            }
         }
     }
 
