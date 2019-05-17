ccb7dd8b5d817cf17696943e0e62dc553eac0970
zhangzeteng@moseeker.com <zhangzeteng@moseeker.com>
2019-05-17 10:16:19 +0800

    add

--- searchengine-service-provider/src/main/java/com/moseeker/searchengine/service/impl/keywordSearch/searchkeyword/NameSearchBuilder.java
+++ searchengine-service-provider/src/main/java/com/moseeker/searchengine/service/impl/keywordSearch/searchkeyword/NameSearchBuilder.java
@@ -1,7 +1,9 @@
 package com.moseeker.searchengine.service.impl.keywordSearch.searchkeyword;
 
+import com.moseeker.common.util.StringUtils;
 import com.moseeker.searchengine.domain.KeywordSearchParams;
 import com.moseeker.searchengine.util.SearchUtil;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 
@@ -12,9 +14,16 @@ public class NameSearchBuilder implements KeywordSearch {
         QueryBuilder defaultquery = QueryBuilders.matchAllQuery();
         QueryBuilder query = QueryBuilders.boolQuery().must(defaultquery);
         SearchUtil searchUtil=new SearchUtil();
-        String fieldName="user.profiles.basic.name";
-        searchUtil.queryMatchPrefixSingle(fieldName,keyword,query);
-        searchUtil.convertSearchNameScript(keyword,query);
+//        String fieldName="user.profiles.basic.user_name_new";
+//        searchUtil.queryMatchPrefixSingle(fieldName,keyword,query);
+//        searchUtil.convertSearchNameScript(keyword,query);
+        String []array=keyword.split(",");
+        for(String key:array){
+            QueryBuilder keyand = QueryBuilders.wildcardQuery("user.profiles.basic.user_name_new","*"+key+"*");
+            ((BoolQueryBuilder) query).should(keyand);
+        }
+        ((BoolQueryBuilder) query).minimumNumberShouldMatch(1);
+
         return query;
     }
 }
