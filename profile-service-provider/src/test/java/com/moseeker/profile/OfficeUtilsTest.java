774748a8c41a6a0255a4ecaecacc9085318c214a
wulei <wulei@moseeker.com>
2019-06-10 14:44:45 +0800

    提交测试文件

--- profile-service-provider/src/test/java/com/moseeker/profile/OfficeUtilsTest.java
+++ profile-service-provider/src/test/java/com/moseeker/profile/OfficeUtilsTest.java
@@ -3,6 +3,8 @@ package com.moseeker.profile;
 import com.moseeker.profile.utils.OfficeUtils;
 import org.junit.Test;
 
+import java.io.File;
+
 /**
  * @Description TODO
  * @Author Rays
@@ -13,6 +15,13 @@ public class OfficeUtilsTest {
     @Test
     public void test00(){
         OfficeUtils.Word2Pdf("/Users/wulei/Documents/工作文档/杂乱/黄成杰简历.docx","/Users/wulei/Documents/工作文档/杂乱/黄成杰简历.pdf");
+    }
+
+    @Test
+    public void test01(){
+        boolean errorCompare = new File("/Users/wulei/Documents/工作文档/杂乱/黄成杰简历.docx").length()<
+                new File("/Users/wulei/Documents/工作文档/杂乱/黄成杰简历.pdf").length();
+        System.out.println(errorCompare);
     }
 
 }
