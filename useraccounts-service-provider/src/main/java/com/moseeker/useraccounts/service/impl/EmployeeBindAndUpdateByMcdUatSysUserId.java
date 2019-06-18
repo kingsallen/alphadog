9126c96b6e4589a084ac9428212ef6fa8ee48a2b
jack frame weng <wjf2255@gmail.com>
2019-06-18 17:35:46 +0800

    修复员工认证错误

--- useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/service/impl/EmployeeBindAndUpdateByMcdUatSysUserId.java
+++ useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/service/impl/EmployeeBindAndUpdateByMcdUatSysUserId.java
@@ -1,5 +1,6 @@
 package com.moseeker.useraccounts.service.impl;
 
+import com.alibaba.fastjson.JSONArray;
 import com.moseeker.baseorm.constant.EmployeeActiveState;
 import com.moseeker.baseorm.db.userdb.tables.records.UserEmployeeRecord;
 import com.moseeker.common.constants.Constant;
@@ -86,7 +87,11 @@ public class EmployeeBindAndUpdateByMcdUatSysUserId extends EmployeeBinder {
         userEmployeeDO.setBindingTime(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
         userEmployeeDO.setCustomField(org.apache.commons.lang.StringUtils
                 .defaultIfBlank(bindingParams.getCustomField(), userEmployeeDO.getCustomField()));
-
+        if (bindingParams.getCustomFieldValues() != null && bindingParams.getCustomFieldValues().size() > 0) {
+            JSONArray jsonArray = new JSONArray();
+            jsonArray.add(bindingParams.getCustomFieldValues());
+            userEmployeeDO.setCustomFieldValues(jsonArray.toJSONString());
+        }
         return userEmployeeDO;
     }
 
