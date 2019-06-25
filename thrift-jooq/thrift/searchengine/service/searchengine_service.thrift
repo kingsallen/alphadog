e617c45a82e00f61b89d414290b763ff9d04e6a6
qiancheng <qiancheng@moseeker.com>
2019-06-25 15:54:51 +0800

    健康检查

--- thrift-jooq/thrift/searchengine/service/searchengine_service.thrift
+++ thrift-jooq/thrift/searchengine/service/searchengine_service.thrift
@@ -7,6 +7,9 @@ namespace java com.moseeker.thrift.gen.searchengine.service
 
 service SearchengineServices {
 
+
+    bool healthCheck();
+
     common_struct.Response query(1: string keywords,2:string cities,3:string industries,4:string occupations,5:string scale,
     6:string employment_type,7:string candidate_source,8:string experience,9:string degree,10:string salary,11:string company_name,
     12:i32 page_from,13: i32 page_size,14:string child_company_name,15:string department,16:bool order_by_priority,
