d6d30a8ad940f36193027f7b6082e50e919b239b
jack frame weng <wjf2255@gmail.com>
2019-06-26 17:31:16 +0800

    删除日志

--- service-manager/src/main/java/com/moseeker/servicemanager/web/controller/useraccounts/UserHrAccountController.java
+++ service-manager/src/main/java/com/moseeker/servicemanager/web/controller/useraccounts/UserHrAccountController.java
@@ -898,7 +898,6 @@ public class UserHrAccountController {
             String timespan = params.getString("timespan", "");
             String selectedIids = params.getString("selectedIds");
 
-            logger.info("UserHrAccountController employeeList params:{}", JSONObject.toJSONString(params));
             UserEmployeeVOPageVO userEmployeeVOPageVO = userHrAccountService.getEmployees(keyWord, companyId, filter,
                     order, asc, pageNumber, pageSize, email_isvalid,balanceType, timespan, selectedIids);
             return ResponseLogNotification.success(request,
