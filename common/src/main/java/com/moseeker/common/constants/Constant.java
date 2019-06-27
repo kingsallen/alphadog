4f5e0306c8636d11c6370cb782b37f6231388786
jack frame weng <wjf2255@gmail.com>
2019-06-27 14:27:45 +0800

    添加等待超时

--- common/src/main/java/com/moseeker/common/constants/Constant.java
+++ common/src/main/java/com/moseeker/common/constants/Constant.java
@@ -49,6 +49,7 @@ public final class Constant {
 
     public static final int FIVE_THOUSAND = 5000;
     public static final int ONE = 1;
+    public static final int FIVE_HUNDRED = 300;
 
     // status ok状态
     public static final int OK = 0;
