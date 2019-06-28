c30c0109aec240df7be4b29e3e8c2c289853d875
jack frame weng <wjf2255@gmail.com>
2019-06-28 10:45:45 +0800

    添加日志

--- useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/service/impl/employee/BatchValidate.java
+++ useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/service/impl/employee/BatchValidate.java
@@ -692,10 +692,12 @@ public class BatchValidate {
                                 department.put("department", "");
                             } else {
                                 Integer optionId = Integer.valueOf(departmentValueOptional.get().get(departmentOptional.get().getId().toString()));
+                                logger.info("BatchValidate convertToListDisplay department optionId:{}", optionId);
                                 Optional<EmployeeOptionValue> optionValueOptional = employeeOptionValues
                                         .parallelStream()
                                         .filter(employeeOptionValue -> employeeOptionValue.getId().equals(optionId))
                                         .findAny();
+                                logger.info("BatchValidate convertToListDisplay department optionValueOptional:{}", optionValueOptional.isPresent());
                                 if (optionValueOptional.isPresent()) {
                                     department.put("department", optionValueOptional.get().getName());
                                 } else {
