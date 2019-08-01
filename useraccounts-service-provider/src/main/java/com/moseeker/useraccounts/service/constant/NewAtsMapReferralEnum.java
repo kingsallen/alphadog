875a1a29153afb5b6f03751a7e4ea027a323e2b1
miaodongchang <miaodongchang@moseeker.com>
2019-08-01 11:04:58 +0800

    新旧流程匹配规则修改

--- useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/service/constant/NewAtsMapReferralEnum.java
+++ useraccounts-service-provider/src/main/java/com/moseeker/useraccounts/service/constant/NewAtsMapReferralEnum.java
@@ -17,14 +17,17 @@ import static com.moseeker.useraccounts.service.constant.ReferralProgressEnum.*;
 public enum NewAtsMapReferralEnum {
     /**
      * 老流程     1 提交简历 10 通过初筛 12 通过面试 3 成功入职 4 拒绝 6 hr查看简历 15 员工上传简历 16 联系内推推荐投递简历 （1，6，15，16是简历投递阶段）
-     * 新流程 类别 1 是筛选类 2 是面食类型 3 是服务类型 4 offer类型 5其他类型
+     * 新流程 类别 0初筛 1初筛型 2面试型 3服务类型 4offer型 5通用型 6待入职
      * new-old : 1-1,2-10,4-12,5-3(目前待定)
      *
      */
-    APPLYED(1, 1),
-    FILTERED(2, 10),
-    INTERVIEWED(4, 12),
-    ENTRY(5, 3);
+    APPLYED(0, 1),
+    FILTERED(1, 10),
+    INTERVIEWED(2, 12),
+    SERVER(3, 12),
+    OFFER(4, 12),
+    COMMON(5, 12),
+    ENTRY(6, 3);
 
     int newPhaseType;
     int oldStep;
