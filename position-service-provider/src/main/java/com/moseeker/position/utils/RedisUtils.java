0e10a8ac592471bb607656f9d8ad4c7c46c5c2b5
zhangxiang <zhangxiang@moseeker.com>
2019-10-25 14:11:56 +0800

    优化职位锁，优化日志输出

--- position-service-provider/src/main/java/com/moseeker/position/utils/RedisUtils.java
+++ position-service-provider/src/main/java/com/moseeker/position/utils/RedisUtils.java
@@ -5,13 +5,13 @@ import com.moseeker.common.constants.Constant;
 
 public class RedisUtils {
 
-    public static void lock(RedisClient redisClient, String key1, String key2, int lockSeconds, int retry) {
+    public static void lock(RedisClient redisClient, String key_identifier, String key, int lockSeconds, int retry) {
         if (retry == 0) {
             retry = Integer.MAX_VALUE;
         }
         int failCount = 0;
         while (failCount < retry) {
-            if (lock(redisClient, key1, key2, lockSeconds)) {
+            if (lock(redisClient, key_identifier, key, lockSeconds)) {
                 return;
             } else {
                 failCount++;
@@ -25,20 +25,20 @@ public class RedisUtils {
         throw new RedisLockException("to many try");
     }
 
-    public static boolean lock(RedisClient redisClient, String key1, String key2, int lockSeconds) {
-        long check = redisClient.incrIfNotExist(Constant.APPID_ALPHADOG, key1, key2);
+    public static boolean lock(RedisClient redisClient, String key_identifier, String key, int lockSeconds) {
+        long check = redisClient.incrIfNotExist(Constant.APPID_ALPHADOG, key_identifier, key);
         if (check > 1) {
             //同步中
             return true;
         }
-        redisClient.expire(Constant.APPID_ALPHADOG, key1, key2, lockSeconds);
+        redisClient.expire(Constant.APPID_ALPHADOG, key_identifier, key, lockSeconds);
         return false;
     }
 
     /**
      * 删除锁
      */
-    public static void unLock(RedisClient redisClient, String key1, String key2) {
-        redisClient.del(Constant.APPID_ALPHADOG, key1, key2);
+    public static void unLock(RedisClient redisClient, String key_identifier, String key) {
+        redisClient.del(Constant.APPID_ALPHADOG, key_identifier, key);
     }
 }
